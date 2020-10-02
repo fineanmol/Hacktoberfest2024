69c87964923331884df295540cbe2edf70e5ed1b
Milos Vujinic <milos.vujinic2@gmail.com>
2020-10-02 19:52:13 +0200

    Added myself as contributor and created a new script

---
+++ scripts/chuck-norris-joke-generator.js
@@ -0,0 +1,15 @@
+const API_URL = 'https://api.chucknorris.io/jokes/random'
+
+/**
+ * To run this you will need node installed
+ * Run by executing `node chuck-norris-jokes.js`
+ * @author milos5593
+ */
+async function GET_JOKES() {
+    const response = await fetch(API_URL)
+    const { value } = await response.json()
+
+    console.log(value)
+}
+
+GET_JOKES()

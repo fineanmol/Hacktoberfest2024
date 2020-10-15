47bb7b30e38ccaee2f80426a43746ceff44ceb4e
qdtroemner <31808836+qdtroemner@users.noreply.github.com>
2020-10-15 14:54:29 -0400

    Create factorial.cpp

---
+++ Program's_Contributed_By_Contributors/factorial.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int factorial(int n) {
+	if(n == 0) {
+		return 1;
+	}
+	if(n > 0) {
+		return n * factorial(n - 1);
+	} else if(n < 0) {
+		return n * factorial(n + 1);
+	}
+
+	return 0;
+}
+
+int main() {
+  std::cout << factorial(3);
+}

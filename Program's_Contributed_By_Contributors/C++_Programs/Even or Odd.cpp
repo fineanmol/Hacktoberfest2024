318c89f87ce521c164d17f0ccaf11b0248ab52ee
srravigupt <56300120+srravigupt@users.noreply.github.com>
2021-10-01 16:45:54 +0530

    Create Even or Odd.cpp

---
+++ Program's_Contributed_By_Contributors/C++_Programs/Even or Odd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+
+  cout << "Enter an integer: ";
+  cin >> n;
+
+  if ( n % 2 == 0)
+    cout << n << " is even.";
+  else
+    cout << n << " is odd.";
+
+  return 0;
+}

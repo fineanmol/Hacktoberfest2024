d94dbc2c38f062de91bace6b16e85a57dae2448d
Anmol55555 <61626454+Anmol55555@users.noreply.github.com>
2021-10-02 03:39:45 +0530

    IfElse Code

---
+++ Program's_Contributed_By_Contributors/C++_Programs/IfElse.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a = 5;
+  if(a == 5)
+  {
+      cout<<"Present in the if part"<<endl;
+  }
+  else
+  {
+      cout<<"Present in the else part"<<endl;
+  }
+  
+    
+}

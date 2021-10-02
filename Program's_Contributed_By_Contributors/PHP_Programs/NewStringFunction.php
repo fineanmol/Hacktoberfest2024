778389b52b517c51a7c5e9229e1df51accdcc35f
fikriks <fikriks130@gmail.com>
2021-10-02 13:51:22 +0700

    added learn php

---
+++ Program's_Contributed_By_Contributors/PHP_Programs/NewStringFunction.php
@@ -0,0 +1,10 @@
+<?php
+
+// contains - mengandung kata kata yang kita cari
+var_dump(str_contains("Saya ingin Belanja Online di Shopee", "Shopee"));
+
+//  start with - mengandung kata kata yang kita cari di awal kata / kalimat
+var_dump(str_starts_with("PHP 8 adalah versi terbaru saat ini dari PHP", "PHP"));
+
+//  end with - mengandung kata kata yang kita cari di akhir kata / kalimat
+var_dump(str_ends_with("Hari Jumat", "Jumat"));

c6f5703cbf625d9193d85041be0acbbc7ffeb4ac
susang <susangmree@gmail.com>
2021-10-02 11:15:08 +0545

    added email_validation method

--- Program's_Contributed_By_Contributors/Python_Programs/regex_snippets.py
+++ Program's_Contributed_By_Contributors/Python_Programs/regex_snippets.py
@@ -19,6 +19,13 @@ class regex:
         else:
             return False  # weak password
 
+    # check if the email is valid
+    def email_validation(self, email):
+        if re.fullmatch(r"/[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/igm", email):
+            return True  # valid email
+        else:
+            return False  # invalid email
+
 
 reg = regex()
 

b98754dfaf144fbae5bfc2a1b31e0d7d25749df2
gigabite-pro <rajmachawal19@gmail.com>
2021-10-04 18:41:03 +0530

    added my name and made back-to-top more aesthetic

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,45 +1,57 @@
-window.onscroll = scrollFunc;
-var scrollButton = document.getElementById('backToTop')
+// window.onscroll = scrollFunc;
+// var scrollButton = document.getElementById('backToTop')
 
-function scrollFunc() {
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-        scrollButton.style.display = "block"
+// function scrollFunc() {
+//     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+//         scrollButton.style.display = "block"
+//     } else {
+//         scrollButton.style.display = "none"
+//     }
+// }
+// scrollButton.onclick = function() {
+//     scrollButton.classList.add('startscrolling')
+//     scrollTo(document.documentElement, 0, 1250, function() {
+//         scrollButton.classList.remove('startscrolling')
+//     });
+// }
+// scrollButton.onmousedown = function() {
+//     scrollButton.classList.add("mousedown")
+// }
+// scrollButton.onmouseup = function(){
+//     scrollButton.classList.remove("mousedown")
+// }
+// function scrollTo(element, to, duration, callback) {
+//     var start = element.scrollTop,
+//         change = (to - start),
+//         currentTime = 0,
+//         increment = 20
+//     var animateScroll = function() {
+//         currentTime += increment
+//         var val = Math.easeInOutQuad(currentTime, start, change, duration)
+//         element.scrollTop = val
+//         if (currentTime < duration) {
+//             setTimeout(animateScroll,increment)
+//         } else if(callback && typeof callback === 'function'){
+//             callback()
+//         }
+//     }
+//     animateScroll()
+// }
+// Math.easeInOutQuad = function(t, b, c, d) {
+//     t /= d/2
+//     if (t < 1) return (c/2*t*t) + b
+//     t--
+//     return -c/2 * (t*(t-2) - 1) + b
+// }
+
+window.onload = function(){
+    const toTop = document.querySelector("#to-top");
+
+    window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("activetop");
     } else {
-        scrollButton.style.display = "none"
+        toTop.classList.remove("activetop");
     }
-}
-scrollButton.onclick = function() {
-    scrollButton.classList.add('startscrolling')
-    scrollTo(document.documentElement, 0, 1250, function() {
-        scrollButton.classList.remove('startscrolling')
-    });
-}
-scrollButton.onmousedown = function() {
-    scrollButton.classList.add("mousedown")
-}
-scrollButton.onmouseup = function(){
-    scrollButton.classList.remove("mousedown")
-}
-function scrollTo(element, to, duration, callback) {
-    var start = element.scrollTop,
-        change = (to - start),
-        currentTime = 0,
-        increment = 20
-    var animateScroll = function() {
-        currentTime += increment
-        var val = Math.easeInOutQuad(currentTime, start, change, duration)
-        element.scrollTop = val
-        if (currentTime < duration) {
-            setTimeout(animateScroll,increment)
-        } else if(callback && typeof callback === 'function'){
-            callback()
-        }
-    }
-    animateScroll()
-}
-Math.easeInOutQuad = function(t, b, c, d) {
-    t /= d/2
-    if (t < 1) return (c/2*t*t) + b
-    t--
-    return -c/2 * (t*(t-2) - 1) + b
+    })
 }

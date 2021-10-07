550a899dfa7b6aac0ef3659372bca5ade3f3572d
zgosnell <zgosnell88@gmail.com>
2021-10-07 17:28:14 -0400

    added etch-a-sketch js to PROGRAMS

---
+++ Program's_Contributed_By_Contributors/JavaScript_Programs/etch-a-sketch.js
@@ -0,0 +1,69 @@
+// Select the elements on the page - canvas, shake button
+const canvas = document.querySelector('#etch-a-sketch');
+const ctx = canvas.getContext('2d');
+const shakebutton = document.querySelector('.shake');
+const MOVE_AMOUNT = 10;
+
+// Setup our canvas for drawing
+// make a variable called height and width from the same properties on our canvas.
+const { width, height } = canvas;
+
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+// create random x and y starting points on the canvas
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = MOVE_AMOUNT;
+
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+ctx.beginPath(); // start the drawing
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
+ctx.stroke();
+
+function draw({key}) {
+    hue += 5;
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    switch (key) {
+        case 'ArrowUp':
+            y -= MOVE_AMOUNT;
+            break;
+        case 'ArrowRight':
+            x += MOVE_AMOUNT;
+            break;
+        case 'ArrowDown':
+            y += MOVE_AMOUNT;
+            break;
+        case 'ArrowLeft':
+            x -= MOVE_AMOUNT;
+            break;
+        default:
+            break;
+    }
+    ctx.lineTo(x, y);
+    ctx.stroke();
+}
+
+function handleKey(e) {
+    if(e.key.includes('Arrow')) {
+        e.preventDefault();
+        draw({key: e.key});
+    }
+}
+
+window.addEventListener('keydown', handleKey);
+
+function clearCanvas() {
+    canvas.classList.add('shake');
+    ctx.clearRect(0, 0, width, height);
+    canvas.addEventListener('animationend', function() {
+        canvas.classList.remove('shake');
+    }, { once: true });
+}
+
+shakebutton.addEventListener('click', clearCanvas);
+

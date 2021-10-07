1b18f64e3884f5d3cf2be3ba1937f29e8e38e3a4
varshaig1999 <62078868+varshaig1999@users.noreply.github.com>
2021-10-07 11:43:52 +0530

    Created CycleDetection.cpp

---
+++ Program's_Contributed_By_Contributors/C++ Programs/CycleDetectionBFS.cpp
@@ -0,0 +1,67 @@
+// Detecting cycle in the Directed Graph using BFS
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+	bool isCyclic(int N, vector<int> adj[]) {
+        queue<int> q; 
+	    vector<int> indegree(N, 0); 
+	    for(int i = 0;i<N;i++) {
+	        for(auto it: adj[i]) {
+	            indegree[it]++; 
+	        }
+	    }
+
+	    for(int i = 0;i<N;i++) {
+	        if(indegree[i] == 0) {
+	            q.push(i); 
+	        }
+	    }
+	    int cnt = 0;
+	    while(!q.empty()) {
+	        int node = q.front(); 
+	        q.pop(); 
+	        cnt++; 
+	        for(auto it : adj[node]) {
+	            indegree[it]--;
+	            if(indegree[it] == 0) {
+	                q.push(it); 
+	            }
+	        }
+	    }
+	    if(cnt == N) return false; 
+	    return true; 
+	}
+};
+
+
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+    	int V, E;
+    	cin >> V >> E;
+
+    	vector<int> adj[V];
+
+    	for(int i = 0; i < E; i++)
+    	{
+    		int u, v;
+    		cin >> u >> v;
+    		adj[u].push_back(v);
+    	}
+
+    	Solution obj;
+    	cout << obj.isCyclic(V, adj) << "\n";
+    }
+
+    return 0;
+}

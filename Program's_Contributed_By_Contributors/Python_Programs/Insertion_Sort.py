ba95bf78e8ab4a70196df73de808f009a30631bf
smriti2411 <23240.smriti@gmail.com>
2021-10-16 23:34:38 +0530

    Update File

--- Program's_Contributed_By_Contributors/Python_Programs/Insertion_Sort.py
+++ Program's_Contributed_By_Contributors/Python_Programs/Insertion_Sort.py
@@ -1,20 +1,23 @@
-# Insertion sort
-def insertion_Sort(arr):
-  for i in range(1, len(arr)):
-    key = arr[i]
-    j = i - 1
-     # Compare key with each element on the left of it until an element smaller than it is found.
-    while j >= 0 and key < arr[j]:
-      arr[j + 1] = arr[j]
-      j = j - 1
-    #Place Key element just after the smaller than it.
-    arr[j + 1] = key
-
+# Recursive Insertion sort 
+def insertionSort(arr,n):
+   # base case
+   if n<=1:
+      return
+   # Recursive Call
+   insertionSort(arr,n-1)
+   key = arr[n-1]
+   j = n-2
+   # Compare key with each element on the left of it until an element smaller than it is found
+   while (j>=0 and arr[j]>key):
+      arr[j+1] = arr[j]
+      j = j-1
+   # Place key at after the element just smaller than it.
+   arr[j+1]=key
 a=[]
 n=int(input("Enter number of elements: "))
 for i in range(0,n):
     b=int(input("Enter element: "))
     a.append(b)
-insertion_Sort(a)
+insertionSort(a,n)
 print('Sorted Array:')
 print(a)

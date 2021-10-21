f122044045a81cbb628652130561e2a66996e143
souvik0306 <souvikdatta123@gmail.com>
2021-10-21 13:25:49 +0530

    Sorted

--- Object Detection/Detection_over_video.py
+++ Object Detection/Detection_over_video.py
@@ -3,15 +3,16 @@ import numpy as np
 import os
 import sys
 
-config_file = (r'D:\HACK\Hacktoberfest2021\Object Detection\Coco\ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt')
-frozen_model = (r'D:\HACK\Hacktoberfest2021\Object Detection\Coco\frozen_inference_graph.pb')
+
 out = cv.VideoWriter('Result.mp4', cv.VideoWriter_fourcc(*'m', 'p', '4', 'v'), 20, (700, 500))
 
+config_file = (r'D:\Hacktoberfest2021\Object Detection\ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt')
+frozen_model = (r'D:\Hacktoberfest2021\Object Detection\frozen_inference_graph.pb')
 
 model = cv.dnn_DetectionModel(frozen_model, config_file)
 
 classLabels = []
-file_name = (r'D:\HACK\Hacktoberfest2021\Object Detection\Labels.txt')
+file_name = (r'D:\Hacktoberfest2021\Object Detection\Labels.txt')
 with open(file_name, 'rt') as fpt:
     classLabels = fpt.read().rstrip('\n').split('\n')
 
@@ -33,9 +34,9 @@ while True:
         for ClassInd, conf, boxes in zip(ClassIndex.flatten(), confidence.flatten(), bbox):
             if (ClassInd <= 80):
          
-                cv.rectangle(img, boxes, (255, 0, 0), 2)
+                cv.rectangle(img, boxes, (0, 255, 0), 2)
                 cv.putText(img, classLabels[int(ClassInd - 1)], (boxes[0] + 10, boxes[1] + 40), cv.FONT_HERSHEY_PLAIN,
-                           3, color=(255, 0, 0), thickness=3)
+                           3, color=(0, 255, 0), thickness=3)
 
         out.write(img)
         cv.imshow('Video', img)

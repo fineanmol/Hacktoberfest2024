63a2d935eeeb46fe290698616c7cdf994f383088
nightowldevelopers <nightowldevelopers@gmail.com>
2021-10-23 23:42:07 +0530

    merge

--- Program's_Contributed_By_Contributors/HTML Designs/stay motivated/script.js
+++
@@ -1,7 +0,0 @@
-fetch('https://type.fit/api/quotes')
-    .then(data => data.json())
-    .then(quoteData => {
-            var ran = Math.floor(Math.random() * 10);
-            document.getElementById('quoteElement').innerHTML=quoteData[ran].text+"<br>"+quoteData[ran].author;
-        
-    })

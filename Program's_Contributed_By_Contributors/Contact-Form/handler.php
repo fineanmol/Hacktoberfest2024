5eaf28982c1a745bd74a1dfde21bd3254d3b5007
murshed <fahimshaper@gmail.com>
2021-10-24 14:16:47 -0700

    added contact form with amazing style

---
+++ Program's_Contributed_By_Contributors/Contact-Form/handler.php
@@ -0,0 +1,26 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+/*
+Tested working with PHP5.4 and above (including PHP 7 )
+
+ */
+require_once './vendor/autoload.php';
+
+use FormGuide\Handlx\FormHandler;
+
+
+$pp = new FormHandler(); 
+
+$validator = $pp->getValidator();
+$validator->fields(['name','email'])->areRequired()->maxLength(50);
+$validator->field('email')->isEmail();
+$validator->field('message')->maxLength(6000);
+
+
+
+
+$pp->sendEmailTo('someone@gmail.com'); // ← Your email here
+
+echo $pp->process($_POST);
---
+++ Program's_Contributed_By_Contributors/Contact-Form/vendor/FormGuide/PHPFormValidator/src/ValidatorMap.php
@@ -0,0 +1,19 @@
+<?php
+return  
+    [
+        'isRequired'=>'required',
+        'areRequired'=>'required',
+        'isEmail'=>'email',
+        'areEmails'=>'email',
+        'maxLength'=>'maxlen',
+        'maxLen'=>'maxlen',
+        'minLength'=>'minlen',
+        'minLen'=>'minlen',
+        'isAlphabetic'=>'alphabetic',
+        'areAlphabetic'=>'alphabetic',
+        'Alphabetic'=>'alphabetic',
+        'isAlphaNumeric'=>'alphanumeric',
+        'areAlphaNumeric'=>'alphanumeric',
+        'AlphaNumeric'=>'alphanumeric'
+    ];
+

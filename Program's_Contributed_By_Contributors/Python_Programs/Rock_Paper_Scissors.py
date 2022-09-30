e791a543bc37440ed20fbf0ca03d41d380c1fcdb
Srishty Mangutte <68679980+Srish-ty@users.noreply.github.com>
2022-10-01 03:53:14 +0530

    Update Rock_Paper_Scissors.py

--- Program's_Contributed_By_Contributors/Python_Programs/Rock_Paper_Scissors.py
+++ Program's_Contributed_By_Contributors/Python_Programs/Rock_Paper_Scissors.py
@@ -6,29 +6,29 @@ import os
 import re
 os.system('cls' if os.name=='nt' else 'clear')
 while (1 < 2):
-    print "\n"
-    print "Rock, Paper, Scissors - Shoot!"
-    userChoice = raw_input("Choose your weapon [R]ock], [P]aper, or [S]cissors: ")
+    print ("\n")
+    print ("Rock, Paper, Scissors - Shoot!")
+    userChoice = input("Choose your weapon [R]ock], [P]aper, or [S]cissors: ")
     if not re.match("[SsRrPp]", userChoice):
-        print "Please choose a letter:"
-        print "[R]ock, [S]cissors or [P]aper."
+        print ("Please choose a letter:")
+        print ("[R]ock, [S]cissors or [P]aper.")
         continue
-    // Echo the user's choice                                                                                                                                                                                                                                                                                                                                        
-    print "You chose: " + userChoice
+    # Echo the user's choice                                                                                                                                                                                                                                                                                                                                        
+    print ("You chose: " + userChoice)
     choices = ['R', 'P', 'S']
     opponenetChoice = random.choice(choices)
-    print "I chose: " + opponenetChoice
+    print ("I chose: " + opponenetChoice)
     if opponenetChoice == str.upper(userChoice):
-        print "Tie! "
+        print ("Tie! ")
     #if opponenetChoice == str("R") and str.upper(userChoice) == "P"
     elif opponenetChoice == 'R' and userChoice.upper() == 'S':      
-        print "Scissors beats rock, I win! "
+        print ("Scissors beats rock, I win! ")
         continue
     elif opponenetChoice == 'S' and userChoice.upper() == 'P':      
-        print "Scissors beats paper! I win! "
+        print ("Scissors beats paper! I win! ")
         continue
     elif opponenetChoice == 'P' and userChoice.upper() == 'R':      
-        print "Paper beat rock, I win! "
+        print ("Paper beat rock, I win! ")
         continue
     else:       
-        print "You win!"
+        print("You win!")

2173cec8a704897a03c3b866180a2fe124a047b9
Ahzam Akhtar <ahzam8186@gmail.com>
2022-10-01 03:23:34 +0530

    program added to sort 0,1,2 in an array

--- Program's_Contributed_By_Contributors/Python_Programs/sort012.py
+++ Program's_Contributed_By_Contributors/Python_Programs/sort012.py
@@ -1,18 +1,18 @@
-def sort012(arr):
+def sort012(array):
   low=0
-  high=len(arr)-1
+  high=len(array)-1
   mid=0
   while mid<=high:
-    if arr[mid]==0:
-      arr[low],arr[mid]=arr[mid],arr[low]
+    if array[mid]==0:
+      array[low],array[mid]=array[mid],array[low]
       low=low+1
       mid=mid+1
-    elif arr[mid]==1:
+    elif array[mid]==1:
       mid=mid+1
     else:
-      arr[mid],arr[high]=arr[high],arr[mid]
+      array[mid],array[high]=array[high],array[mid]
       high=high-1
-  return arr
+  return array
 
 arr=[0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
 ans = sort012(arr)

80d2af712c5dfa762d867c2733f0a869f596ce00
Dhruvsingh1234 <71575448+Dhruvsingh1234@users.noreply.github.com>
2022-10-01 05:03:26 +0530

    Update scroll.js
    es6 syntax add....

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,31 +1,31 @@
 window.onscroll = scrollFunc;
 var scrollButton = document.getElementById('backToTop')
 
-function scrollFunc() {
+const scrollFunc = ()=>{
     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
         scrollButton.style.display = "block"
     } else {
         scrollButton.style.display = "none"
     }
 }
-scrollButton.onclick = function() {
+scrollButton.onclick = ()=>{
     scrollButton.classList.add('startscrolling')
     scrollTo(document.documentElement, 0, 1250, function() {
         scrollButton.classList.remove('startscrolling')
     });
 }
-scrollButton.onmousedown = function() {
+scrollButton.onmousedown = ()=>{
     scrollButton.classList.add("mousedown")
 }
-scrollButton.onmouseup = function(){
+scrollButton.onmouseup = ()=>{
     scrollButton.classList.remove("mousedown")
 }
-function scrollTo(element, to, duration, callback) {
+const scrollTo = (element, to, duration, callback)>{
     var start = element.scrollTop,
         change = (to - start),
         currentTime = 0,
         increment = 20
-    var animateScroll = function() {
+    var animateScroll = ()=>{
         currentTime += increment
         var val = Math.easeInOutQuad(currentTime, start, change, duration)
         element.scrollTop = val
@@ -37,7 +37,7 @@ function scrollTo(element, to, duration, callback) {
     }
     animateScroll()
 }
-Math.easeInOutQuad = function(t, b, c, d) {
+Math.easeInOutQuad = (t, b, c, d)=>{
     t /= d/2
     if (t < 1) return (c/2*t*t) + b
     t--

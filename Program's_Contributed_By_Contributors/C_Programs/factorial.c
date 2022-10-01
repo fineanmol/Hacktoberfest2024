75cdc8ecf19338e2799ae02bd9c7c3faf60a5082
atmozki <dennisjojo052001@gmail.com>
2022-10-01 23:57:21 +0530

    Added C program folder

---
+++ Program's_Contributed_By_Contributors/C_Programs/factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    unsigned long long fact = 1;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    // shows error if the user enters a negative integer
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else {
+        for (i = 1; i <= n; ++i) {
+            fact *= i;
+        }
+        printf("Factorial of %d = %llu", n, fact);
+    }
+
+    return 0;
+}

75310914a25b3bf3bc06e8b0cbce99dd2d86ea9c
Saptarsi Saha <saptarshi.saha0@gmail.com>
2022-10-01 17:28:31 +0530

    Create 97 DecodeWays.cpp

---
+++ Program's_Contributed_By_Contributors/C++/97 DecodeWays.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+        for (int i = 2; i <= n; i++) {
+            int first = Integer.valueOf(s.substring(i - 1, i));
+            int second = Integer.valueOf(s.substring(i - 2, i));
+            if (first >= 1 && first <= 9) {
+               dp[i] += dp[i-1];  
+            }
+            if (second >= 10 && second <= 26) {
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+}

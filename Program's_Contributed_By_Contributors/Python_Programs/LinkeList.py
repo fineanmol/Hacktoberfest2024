2ad56a81ca8a5d48bb004a2e020acd9496520adf
Dhananjay Porwal <51775507+DhananjayPorwal@users.noreply.github.com>
2022-10-01 22:25:28 +0530

    Create LinkeList.py

---
+++ Program's_Contributed_By_Contributors/Python_Programs/LinkeList.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, data):
+       self.data = data
+       self.next = None
+ 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.last_node = None
+ 
+    def append(self, data):
+        if self.last_node is None:
+            self.head = Node(data)
+            self.last_node = self.head
+        else:
+            self.last_node.next = Node(data)
+            self.last_node = self.last_node.next
+ 
+    def display(self):
+        current = self.head
+        while current is not None:
+            print(current.data, end = ' ')
+            current = current.next
+ 
+a_llist = LinkedList()
+n = int(input('How many elements would you like to add? '))
+for i in range(n):
+    data = int(input('Enter data item: '))
+    a_llist.append(data)
+print('The linked list: ', end = '')
+a_llist.display()

052c4c13e6f14b318ef82459adfa732684994f89
Mayur~ <=>
2022-10-02 20:42:49 +0530

    Add a webapp that shows all Pokemon Cards and Decks

---
+++ frontend projects/Pokemon TCG Collection/src/components/Loader.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Loader extends Component {
+    render() {
+        return (
+
+<div class="center-on-page">
+  
+  <div class="pokeball">
+    <div class="pokeball__button"></div>
+  </div>
+  
+</div>
+            
+
+
+        )
+    }
+}

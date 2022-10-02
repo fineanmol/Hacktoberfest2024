167a66207b081318dcf73a9b0b6c5e8e8da7c0c4
unknown <shreyas.kamath27@gmail.com>
2022-10-02 22:55:24 +0530

    Leetcode-344-Reverse a string

---
+++ Program's_Contributed_By_Contributors/C++/Reversing_String.cpp
@@ -0,0 +1,30 @@
+// To Reverse a string using iterators
+
+#include <iostream>
+using namespace std;
+
+
+string reverseString(string s,int k){
+    int i=0,j,temp;
+    j=k-1;
+
+    while(i<j){              //Iterate and swap until you reach i is not greater than j//   
+        temp=s[i];
+        s[i]=s[j];
+        s[j]=temp;
+        i++;
+        j--;
+    }
+    return s;
+    
+    
+}
+
+int main(){
+    string s="Good Morning";
+    int n=s.length();
+    string c=reverseString(s,n);
+
+    cout<<c;
+}
+

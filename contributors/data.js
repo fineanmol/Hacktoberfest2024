033609a6fad3e9147c3c4f870f3022c13de86153
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2022-10-02 10:11:41 +0530

    Update data.js

--- contributors/data.js
+++ contributors/data.js
@@ -1864,5 +1864,10 @@ export const contributors = [
       "id":365,
       "fullname": "Prajwal Kulkarni",
       "username": "https://github.com/prajwalkulkarni"
+    },
+    {
+      "id":366,
+      "fullname": "Tanmoy Sengupta",
+      "username": "https://github.com/TSG405"
     }
 ]

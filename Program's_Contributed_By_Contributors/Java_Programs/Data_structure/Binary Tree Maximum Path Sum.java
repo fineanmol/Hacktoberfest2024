334e74709a28c3576f52244f075b8adb25ea66d6
code-sparsh <85060248+code-sparsh@users.noreply.github.com>
2022-10-02 15:38:07 +0530

    Create Binary Tree Maximum Path Sum.java
    Added a Solution to one of the famous questions of Binary Tree

---
+++ Program's_Contributed_By_Contributors/Java_Programs/Data_structure/Binary Tree Maximum Path Sum.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public int maxSum = Integer.MIN_VALUE;
+    
+        public static int maxPathDown(TreeNode node, int maxValue[]) {
+        if (node == null) return 0;
+        int left = Math.max(0, maxPathDown(node.left, maxValue));
+        int right = Math.max(0, maxPathDown(node.right, maxValue));
+        maxValue[0] = Math.max(maxValue[0], left + right + node.val);
+        return Math.max(left, right) + node.val;
+    }
+    
+    public static int maxPathSum(TreeNode root) {
+        int maxValue[] = new int[1];
+        maxValue[0] = Integer.MIN_VALUE;
+        maxPathDown(root, maxValue);
+        return maxValue[0];
+    }
+
+
+}

6373fcb85f6eb00ce6293b5649d3862d166a1af7
unknown <pratikmamdapure@gmail.com>
2022-10-03 23:29:07 +0530

    added name to contributors list

--- contributors/data.js
+++ contributors/data.js
@@ -1864,5 +1864,10 @@ export const contributors = [
       "id":365,
       "fullname": "Prajwal Kulkarni",
       "username": "https://github.com/prajwalkulkarni"
+    },
+    {
+        "id": 366,
+        "fullname": "Pratik Mamdapure",
+        "username": "https://github.com/Pratikmamdapure"
     }
 ]

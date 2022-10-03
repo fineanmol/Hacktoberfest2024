1a7d9d099e2aab00b495700abee3a1753073471a
Bruno-Vasconcellos-Betella <57138664+Bruno-Vasconcellos-Betella@users.noreply.github.com>
2022-10-03 10:12:46 -0300

    add new functions

---
+++ Program's_Contributed_By_Contributors/JavaScript_Programs/generals-functions.js
@@ -0,0 +1,12 @@
+function onlyNumbers(str){
+    return str.replace(/\D/g, "");
+}
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+

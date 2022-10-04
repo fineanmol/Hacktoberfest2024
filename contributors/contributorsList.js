ecf26cae8d956271613af9b58d192fa7e3671095
Abhishek Pandey <abhipandey0314@gmail.com>
2022-10-04 12:56:14 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1890,4 +1890,9 @@ contributors = [
     fullname: "Vikhrama SPJ",
     username: "https://github.com/MangEs23/",
   },
+   {
+    id: 373,
+    fullname: "Abhishek Pandey",
+    username: "https://github.com/vedsom",
+  },
 ];

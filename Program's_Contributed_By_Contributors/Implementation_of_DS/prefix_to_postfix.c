ffb0a886a43b0e789cd0a3e22594c35017bef4e6
Archit Gupta <89090903+i-archit-gupta@users.noreply.github.com>
2022-10-04 23:15:15 +0530

    Created Stack Program

---
+++ Program's_Contributed_By_Contributors/Implementation_of_DS/prefix_to_postfix.c
@@ -0,0 +1,52 @@
+// PREFIX to POSTFIX expression conversion using stack.
+// PROGRAM BY "https://github.com/i-archit-gupta"
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAX 20
+char str[MAX], stack[MAX];
+int top = -1;
+void push(char c)
+{
+    stack[++top] = c;
+}
+char pop()
+{
+    return stack[top--];
+}
+void pre_post()
+{
+    int n, i, j = 0;
+    char c[20];
+    char a, b, op;
+    printf("Enter the prefix expression\n");
+    gets(str);
+    n = strlen(str);
+    for (i = 0; i < MAX; i++)
+        stack[i] = '\0';
+    printf("Postfix expression is:\t");
+    for (i = 0; i < n; i++)
+    {
+        if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/')
+        {
+            push(str[i]);
+        }
+        else
+        {
+            c[j++] = str[i];
+            while ((top != -1) && (stack[top] == '@'))
+            {
+                a = pop();
+                c[j++] = pop();
+            }
+            push('@');
+        }
+    }
+    c[j] = '\0';
+    printf("%s", c);
+}
+void main()
+{
+    pre_post();
+}

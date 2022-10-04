e64f1c3ada4801b57d388c4f9eadbdd4eb48a407
Funikz <aurorateam2504@gmail.com>
2022-10-04 15:33:56 +0700

    Add Funikz to contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1890,4 +1890,9 @@ contributors = [
     fullname: "Vikhrama SPJ",
     username: "https://github.com/MangEs23/",
   },
+  {
+    id: 373,
+    fullname: "Funikz",
+    username: "https://github.com/Funikz/",
+  },
 ];

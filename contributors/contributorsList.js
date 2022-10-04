2320d38c445c23806054ad6dadf296a5ae404217
tanht027 <tanht2707@gmail.com>
2022-10-04 09:39:12 +0700

    Add tanht to Contributors list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1890,4 +1890,9 @@ contributors = [
     fullname: "Vikhrama SPJ",
     username: "https://github.com/MangEs23/",
   },
+  {
+    id: 373,
+    fullname: "Huynh Thanh Tan",
+    username: "https://github.com/Tanht027/",
+  },
 ];

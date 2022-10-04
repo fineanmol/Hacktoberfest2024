8c882c2f8c65e081ec32b53fdf2f2be7c1b5fb0b
Rahul_Roy_21 <royrahul0921@gmail.com>
2022-10-05 02:49:36 +0530

    Added IceCreamApp-AsyncJS

---
+++ Program's_Contributed_By_Contributors/JavaScript_Programs/iceCreamApp/student_list.js
@@ -0,0 +1,13 @@
+const listOfStudents = {
+  Nandkishar: [94.5, "Chocolate"],
+  Manushi: [88, "Vanilla"],
+  Sumit: [98, "Vanilla"],
+  Raman: [100, "Butter-Scotch"],
+  Kushal: [92, "Chocolate"],
+  Virat: [93, "Vanilla"],
+  Naman: [91, "Strawberry"],
+  Harman: [88.5, "Chocolate"],
+  Adarsh: [90.5, "Chocolate"],
+};
+
+module.exports = { listOfStudents };
---
+++ Program's_Contributed_By_Contributors/JavaScript_Programs/iceCreamApp/utils/stock.js
@@ -0,0 +1,7 @@
+const ICECREAM_STOCK = {
+  vanilla: 1,
+  chocolate: 2,
+  strawberry: 3,
+};
+
+module.exports = { ICECREAM_STOCK };

1eefd86af4fcf1b3dc78a92e4b23805f2f12daf0
Eman Munir <104212613+emanmunir@users.noreply.github.com>
2022-10-04 23:38:59 +0500

    Update contributers
    I added my name too

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1830,4 +1830,9 @@ contributors = [
     fullname: "Anusha Gupta",
     username: "https://github.com/anushagupta1312",
   }
+  {
+    id:378,
+    fullname: "Eman Munir",
+    username: "https://github.com/emanmunir",
+  }
 ];

47896d7fb557f2e91c3cd986e96d137039ac92c6
Abhishek Kumar <kumarabhishek.kumar97@gmail.com>
2022-10-04 12:55:17 +0530

    - update contribution list.

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1890,4 +1890,9 @@ contributors = [
     fullname: "Vikhrama SPJ",
     username: "https://github.com/MangEs23/",
   },
+  {
+    id: 374,
+    fullname: "Abhishek Kumar",
+    username: "https://github.com/aabbhishek",
+  },
 ];

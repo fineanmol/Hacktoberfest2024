26706d66a11207924140824511bd055712c9c21e
rushilpatra <70966802+rushilpatra@users.noreply.github.com>
2022-10-05 00:16:12 +0530

    Added my name

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1830,4 +1830,9 @@ contributors = [
     fullname: "Anusha Gupta",
     username: "https://github.com/anushagupta1312",
   }
+  {
+    id:378,
+    fullname: "Rushil Patra",
+    username: "https://github.com/rushilpatra",
+  }
 ];

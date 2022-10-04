0e5cdb830026ad3a9e778919fb41034a534ad8b3
aven99 <vnarvekar8@gmail.com>
2022-10-04 17:21:39 +0530

    added snake game py

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1890,4 +1890,9 @@ contributors = [
     fullname: "Vikhrama SPJ",
     username: "https://github.com/MangEs23/",
   },
+  {
+    id: 373,
+    fullname: "Varad Narvekar",
+    username: "https://github.com/aven99",
+  },
 ];

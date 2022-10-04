c7493be172f4c8c20f6160648cc8fac002e4d8c7
Saumya Chaudhary <chaudharysaumya25@gmail.com>
2022-10-04 18:56:56 +0530

    First Contribution

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1890,4 +1890,9 @@ contributors = [
     fullname: "Vikhrama SPJ",
     username: "https://github.com/MangEs23/",
   },
+  {
+    id:373,
+    fullname: "Saumya Chaudhary",
+    username: "https://github.com/Saumya9425",
+  },
 ];

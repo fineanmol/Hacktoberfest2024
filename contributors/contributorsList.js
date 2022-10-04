697d94370764605c229cb7ed1c2b9fb83643e407
akshay-Kalekar <akalekar2003@gmail.com>
2022-10-04 15:47:43 +0530

    added details

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1890,4 +1890,9 @@ contributors = [
     fullname: "Vikhrama SPJ",
     username: "https://github.com/MangEs23/",
   },
+  {
+    id: 373,
+    fullname: "Akshay Kalekar",
+    username: "https://github.com/akshay-Kalekar",
+  },
 ];

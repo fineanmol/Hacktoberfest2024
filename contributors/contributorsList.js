db7ff4fffa6f4c4428f12389f832e9c82679e79f
Thiago 'Yaakov 'Dantas Soares <thiagosoares93@hotmail.com>
2022-10-03 21:21:46 -0300

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1890,4 +1890,9 @@ contributors = [
     fullname: "Vikhrama SPJ",
     username: "https://github.com/MangEs23/",
   },
+   {
+    id: 373,
+    fullname: "Thiago Dantas",
+    username: "https://github.com/YaakovDantas/",
+  },
 ];

3433cece41702f6643a21f7a01356296409c5370
Raluca Ginga <gingaraluca@gmail.com>
2022-10-04 14:48:01 +0300

    added own name + username

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1890,4 +1890,9 @@ contributors = [
     fullname: "Vikhrama SPJ",
     username: "https://github.com/MangEs23/",
   },
+  {
+	id: 373,
+	fullname: "Andreea Ginga",
+	username: "https://github.com/ralucaginga/",
+  }
 ];

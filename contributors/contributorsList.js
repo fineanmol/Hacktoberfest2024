41922773861e911ecfae4924e8ba6faabdfce0a5
Aira Jena <113255984+0xaira@users.noreply.github.com>
2022-10-05 10:58:51 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1830,4 +1830,9 @@ contributors = [
     fullname: "Anusha Gupta",
     username: "https://github.com/anushagupta1312",
   }
+   {
+    id: 388,
+    fullname: "Aira Jena",
+    username: "https://github.com/0xaira",
+  }
 ];

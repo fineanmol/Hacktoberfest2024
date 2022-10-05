cfc570b52de4bf82aa58a516cd87f0ad5e968a8e
candrawibowo350@gmail.com <candrawibowo@gmail.com>
2022-10-05 10:08:36 +0700

    Ma First Contribution

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1830,4 +1830,9 @@ contributors = [
     fullname: "Anusha Gupta",
     username: "https://github.com/anushagupta1312",
   }
+  {
+    id:378,
+    fullname: "Candra W",
+    username: "https://github.com/candraw",
+  }
 ];

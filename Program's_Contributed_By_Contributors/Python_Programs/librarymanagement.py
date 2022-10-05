a237d0d0ebe09341d8bba3e4f3249f895ed44c7a
Imran Nazir <imrnnzr1@gmail.com>
2022-10-05 11:42:13 +0530

    added library management system

---
+++ Program's_Contributed_By_Contributors/Python_Programs/librarymanagement.py
@@ -0,0 +1,59 @@
+class Library:
+    def __init__(self, listOfBooks):
+        self.books = listOfBooks
+
+    def displayAvailableBooks(self):
+        print("Books present in this library are: ")
+        for book in self.books: 
+            print(" *" + book)
+    
+    def borrowBook(self, bookName):
+        if bookName in self.books:
+            print(f"You have been issued {bookName}. Please keep it safe and return it within 30 days")
+            self.books.remove(bookName)
+            return True
+        else:
+            print("Sorry, This book is either not available or has already been issued to someone else. Please wait until the book is available")
+            return False
+
+    def returnBook(self, bookName):
+        self.books.append(bookName)
+        print("Thanks for returning this book! Hope you enjoyed reading it. Have a great day ahead!")
+
+class Student: 
+    def requestBook(self):
+        self.book = input("Enter the name of the book you want to borrow: ")
+        return self.book
+
+    def returnBook(self):
+        self.book = input("Enter the name of the book you want to return: ")
+        return self.book
+         
+
+if __name__ == "__main__":
+    centraLibrary = Library(["Algorithms", "Django", "Clrs", "Python Notes"])
+    student = Student()
+    # centraLibrary.displayAvailableBooks()
+    while(True):
+        welcomeMsg = '''\n ====== Welcome to Central Library ======
+        Please choose an option:
+        1. List all the books
+        2. Request a book
+        3. Add/Return a book
+        4. Exit the Library
+        '''
+        print(welcomeMsg)
+        a = int(input("Enter a choice: "))
+        if a == 1:
+            centraLibrary.displayAvailableBooks()
+        elif a == 2:
+            centraLibrary.borrowBook(student.requestBook())
+        elif a == 3:
+            centraLibrary.returnBook(student.returnBook())
+        elif a == 4:
+            print("Thanks for choosing Central Library. Have a great day ahead!")
+            exit()
+        else:
+            print("Invalid Choice!")
+
+        

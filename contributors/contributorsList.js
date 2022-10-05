4fa5fc4509f839bf627dee7d5db9ff8e07a9e33d
Alessandro <alessandrogreco@autistici.org>
2022-10-05 17:20:06 +0200

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1840,4 +1840,9 @@ contributors = [
     fullname: "Sabin Thapa",
     username: "https://github.com/sabin-thapa",
   },
+  {
+    id:368,
+    fullname: "Alessandro Greco",
+    username: "https://github.com/aleff-github",
+  },
 ];

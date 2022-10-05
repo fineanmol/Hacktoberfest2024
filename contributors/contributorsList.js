942520820fff347b4657e25c6dea4d202c5fc814
DamianT7 <DamianT7@live.com>
2022-10-05 14:11:33 +0200

    Updated contributors list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1840,4 +1840,9 @@ contributors = [
     fullname: "Sabin Thapa",
     username: "https://github.com/sabin-thapa",
   },
+  {
+    id:368,
+    fullname: "Damian Teske",
+    username: "https://github.com/DamianT7",
+  },
 ];

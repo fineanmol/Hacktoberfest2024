3c66ce144bd15ced3d32f2f45ad4220a77444587
Sheetal Patel <sheetal2001p@gmail.com>
2022-10-05 16:16:04 +0530

    modified contributors.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1840,4 +1840,9 @@ contributors = [
     fullname: "Sabin Thapa",
     username: "https://github.com/sabin-thapa",
   },
+  {
+    id:368,
+    fullname: "Sheetal Patel",
+    username: "https://github.com/sheetal2001p",
+  },
 ];

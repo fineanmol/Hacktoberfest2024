25562134e99de076be69e7f676b654c36345671b
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-10-06 00:30:44 +0530

    [fix]: remove unnecessary code

--- Program's_Contributed_By_Contributors/JavaScript_Programs/bubble-sort-in-js.js
+++ Program's_Contributed_By_Contributors/JavaScript_Programs/bubble-sort-in-js.js
@@ -5,11 +5,8 @@ const bubbleSort = (arr) => {
 				[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
 			}
 		}
-		console.log(i);
-		console.log(arr);
 	}
-
 	return arr;
 };
 
-console.log(bubbleSort([6, 5, 4, 3, 2, 1])); //231,213
+console.log(bubbleSort([6, 5, 4, 3, 2, 1]));

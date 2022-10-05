634021a397ed8b7d1eec4458262b08be3676135e
Shruti Gupta <105665662+Shrutigupta03@users.noreply.github.com>
2022-10-05 15:36:29 +0530

    Update Guessthenumber.py

--- Program's_Contributed_By_Contributors/Python_Programs/Guessthenumber.py
+++ Program's_Contributed_By_Contributors/Python_Programs/Guessthenumber.py
@@ -1,23 +1,22 @@
 import random
-input('==================PRESS ENTER TO START THE GAME==================',end='\n\n\n\n\n')
+input('==================PRESS ENTER TO START THE GAME==================')
+print('\n\n\n')
 
 
-
-
-
-print('    |          |||||||||||   |       |  ||||||||  |||||||||       |||||||||   =================')
-print('    |          |             |       |  \\         \\             \\                 ===============           ===============')
-print('    |          |     /||||   |       |   ||||||     \\              \\                                       ======================')
-print('    |          |_____|___|   |      /    //          \\\\            \\\\=      ---KHUSHANK                           ===================')
-print('    |          |_________    ||||||/    ||||||||  |||||||||    ||||||||||               ==================================   ', end='\n\n')
+print('    |          |||||||||||   |       |  ||||||||  |||||||||  |||||||||   ===============')
+print('    |          |             |       |  \\          \\          \\         ===============')
+print('    |          |     /||||   |       |   ||||||     \\\\         \\\\        ===============')
+print('    |          |_____|___|   |       |   //           \\\\\\\      \\\\\\\    ===============')
+print('    |          |||||||||||    \|||||/   ||||||||  |||||||||  |||||||||   ===============')
+print('\n\n')
 
 points = 0
 while True:
-	Guess = int(input('Guess a number between 1 and 60 :', end='\r'))
+	Guess = int(input('\rGuess a number between 1 and 60 :'))
 	num = int(random.random()*61)
 	points+=abs(Guess - num)
-	print(f'Actual number is {num}. You are off by {abs(Guess-num)}\nyour current score is {points}'end='\r')
-	ask = input('do you want to play more(y/n): ', end='\r')
+	print(f'\rActual number is {num}. You are off by {abs(Guess-num)}\nyour current score is {points}')
+	ask = input('\rdo you want to play more(y/n): ')
 	if ask == 'n':break
 
 #HIGH SCORE
@@ -29,6 +28,7 @@ HIGH.close()
 highscore = max(int(highscore[0]), points)
 
 HIGH = open("HIGH SCORE.txt", "w")
+
 HIGH.write(highscore)
 HIGH.close()
 print(f'Your score is: {points}\nHIGH SCORE: {highscore}')

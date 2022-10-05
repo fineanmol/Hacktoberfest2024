02941c080ffbd2ad396f79bff16a3dddebff2880
Arindam Ghosh <arindamghosh854@gmail.com>
2022-10-05 10:26:10 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1830,4 +1830,9 @@ contributors = [
     fullname: "Anusha Gupta",
     username: "https://github.com/anushagupta1312",
   }
+  {
+    id:378,
+    fullname: "Arindam Ghosh",
+    username: "https://github.com/pappughosh",
+  }
 ];

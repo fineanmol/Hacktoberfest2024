2e7f3f6b939f95ebf6069e9513f4720b6ffd9abe
Khori Hutama <khori.qq@gmail.com>
2022-10-05 17:01:50 +0700

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1840,4 +1840,9 @@ contributors = [
     fullname: "Sabin Thapa",
     username: "https://github.com/sabin-thapa",
   },
+  {
+    id:368,
+    fullname: "Khori Hutama",
+    username: "https://github.com/khorihutama",
+  },
 ];

44d45986ccf72e8b882b06f7ffebc25d951a2ff5
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-10-06 00:29:24 +0530

    add bubble sort program in javascript

---
+++ Program's_Contributed_By_Contributors/JavaScript_Programs/bubble-sort-in-js.js
@@ -0,0 +1,15 @@
+const bubbleSort = (arr) => {
+	for (let i = 0; i < arr.length - 1; i++) {
+		for (let j = 0; j < arr.length - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+			}
+		}
+		console.log(i);
+		console.log(arr);
+	}
+
+	return arr;
+};
+
+console.log(bubbleSort([6, 5, 4, 3, 2, 1])); //231,213

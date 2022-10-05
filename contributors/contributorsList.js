7f44504f3585f6b772abce1ed16e1753d12246de
atmaniali <atmaniali97@gmail.com>
2022-10-05 10:47:49 +0100

    add my name to this file

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1840,4 +1840,9 @@ contributors = [
     fullname: "Sabin Thapa",
     username: "https://github.com/sabin-thapa",
   },
+  {
+    id:368,
+    fullname: "Atmani Ali",
+    username: "https://github.com/atmaniali",
+  },
 ];

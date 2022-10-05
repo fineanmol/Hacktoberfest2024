3f5a86a829d1523fdc0b1357abef4d21a9de57df
Sandeep  Patel <96694484+Sandeep-1507@users.noreply.github.com>
2022-10-05 21:47:51 +0530

    updated contributors.js
    Made my first contribution in the Hacktoberfest here.

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1840,4 +1840,9 @@ contributors = [
     fullname: "Sabin Thapa",
     username: "https://github.com/sabin-thapa",
   },
+  {
+    id:368,
+    fullname: "Sandeep Patel",
+    username: "https://github.com/Sandeep-1507",
+  },
 ];

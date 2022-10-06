5fc66e5a26f717f59c43cac7320faf4d246134d4
siddak <siddakbagga@gmail.com>
2022-10-06 11:10:46 +0530

    Contribution 368

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1840,4 +1840,9 @@ contributors = [
     fullname: "Sabin Thapa",
     username: "https://github.com/sabin-thapa",
   },
+  {
+    id:368,
+    fullname: "Siddak Bagga",
+    username: "https://github.com/siddakbagga13",
+  },
 ];

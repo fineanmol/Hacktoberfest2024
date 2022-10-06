0700e3c35efb04d856ec79d6d1a68c0547963d50
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-10-06 17:38:52 +0530

    Relevant message

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1939,6 +1939,10 @@ contributors = [
     id:385,
     fullname: "Philip Mello",
     username: "https://github.com/PhilipMello"
-  }
-
+  },
+{
+id:386,
+fullname:"Priyanshu Tiwari",
+username:"https://github.com/Priyanshu360-cpu"
+}
 ];

44cc84c59514262d10a9dcecd2fcfc013b246be0
Eduardo Freitas de Carvalho <61642210+edufcarvalho@users.noreply.github.com>
2022-10-06 18:29:51 -0300

    Fix typo

--- Program's_Contributed_By_Contributors/Haskell/fibonacci_series.hs
+++ Program's_Contributed_By_Contributors/Haskell/fibonacci_series.hs
@@ -2,6 +2,6 @@ fibonacci :: [Int]
 fibonacci = 0 : 1 : zipWith (+) fibonacci (tail fibonacci)
 
 main = do
-  putStrLn "Enter the number os elements:"
+  putStrLn "Enter the number of elements:"
   input <- getLine
   print (take (read input :: Int) fibonacci)

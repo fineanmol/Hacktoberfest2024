a12758fed07df0608e3e7513bcb61faf3e1e1d3f
hugofolloni <hugofollogua07@gmail.com>
2022-10-05 23:09:50 -0300

    Adding my name

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1939,6 +1939,11 @@ contributors = [
     id:385,
     fullname: "Philip Mello",
     username: "https://github.com/PhilipMello"
+  },
+  {
+    id:386,
+    fullname: "Hugo Folloni",
+    username: "https://github.com/hugofolloni"
   }
 
 ];

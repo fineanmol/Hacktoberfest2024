f727f666bfd93dd57db72cb2731a3284539fbbf1
Archis <92965519+archisvaze@users.noreply.github.com>
2022-10-06 07:11:16 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1939,6 +1939,11 @@ contributors = [
     id:385,
     fullname: "Philip Mello",
     username: "https://github.com/PhilipMello"
+  },
+   {
+    id:386,
+    fullname: "Archis Vaze",
+    username: "https://github.com/archisvaze"
   }
 
 ];

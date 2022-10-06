d608720f67ae52edb26f7d7de1d4e932144ff792
Ritik Kumar Gupta <ritikgupta1424@gmail.com>
2022-10-06 11:30:02 +0530

    This is my first pull request.Thanks for making me a part of it.

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1939,6 +1939,11 @@ contributors = [
     id:385,
     fullname: "Philip Mello",
     username: "https://github.com/PhilipMello"
+  },
+  {
+    id:386,
+    fullname: "Ritik Kumar Gupta",
+    username: "https://github.com/ritik-rkg"
   }
 
 ];

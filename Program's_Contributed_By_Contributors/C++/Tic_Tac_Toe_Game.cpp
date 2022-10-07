1ee819151841bdb75f6392b130b3b5fd5557d19a
evans129 <104578248+evans129@users.noreply.github.com>
2022-10-07 11:18:31 +0530

    Improved TicTacToe game graphics in C++

--- Program's_Contributed_By_Contributors/C++/Tic_Tac_Toe_Game.cpp
+++ Program's_Contributed_By_Contributors/C++/Tic_Tac_Toe_Game.cpp
@@ -1,123 +1,139 @@
-//Importing the inbuild libraries in CPP
-#include <iostream>
-#include <stdlib.h>
+#include<bits/stdc++.h>
 using namespace std;
-//Array for the board
-char board[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
-//Variable Declaration
-int choice;
-int row,column;
-char turn = 'X';
-bool draw = false;
-
-//Function to show the current status of the gaming board
-
-void display_board(){
-
-    //Rander Game Board LAYOUT
-
-    cout<<"PLAYER - 1 [X]t PLAYER - 2 [O]nn";
-    cout<<"tt     |     |     n";
-    cout<<"tt  "<<board[0][0]<<"  | "<<board[0][1]<<"  |  "<<board[0][2]<<" n";
-    cout<<"tt_____|_____|_____n";
-    cout<<"tt     |     |     n";
-    cout<<"tt  "<<board[1][0]<<"  | "<<board[1][1]<<"  |  "<<board[1][2]<<" n";
-    cout<<"tt_____|_____|_____n";
-    cout<<"tt     |     |     n";
-    cout<<"tt  "<<board[2][0]<<"  | "<<board[2][1]<<"  |  "<<board[2][2]<<" n";
-    cout<<"tt     |     |     n";
-}
-
-//Function to get the player input and update the board
-
-void player_turn(){
-    if(turn == 'X'){
-        cout<<"ntPlayer - 1 [X] turn : ";
-    }
-    else if(turn == 'O'){
-        cout<<"ntPlayer - 2 [O] turn : ";
-    }
-    //Taking input from user
-    //updating the board according to choice and reassigning the turn Start
-
-    cin>> choice;
-
-    //switch case to get which row and column will be update
-
-    switch(choice){
-        case 1: row=0; column=0; break;
-        case 2: row=0; column=1; break;
-        case 3: row=0; column=2; break;
-        case 4: row=1; column=0; break;
-        case 5: row=1; column=1; break;
-        case 6: row=1; column=2; break;
-        case 7: row=2; column=0; break;
-        case 8: row=2; column=1; break;
-        case 9: row=2; column=2; break;
-        default:
-            cout<<"Invalid Move";
-    }
-
-    if(turn == 'X' && board[row][column] != 'X' && board[row][column] != 'O'){
-        //updating the position for 'X' symbol if
-        //it is not already occupied
-        board[row][column] = 'X';
-        turn = 'O';
-    }else if(turn == 'O' && board[row][column] != 'X' && board[row][column] != 'O'){
-        //updating the position for 'O' symbol if
-        //it is not already occupied
-        board[row][column] = 'O';
-        turn = 'X';
-    }else {
-        //if input position already filled
-        cout<<"Box already filled!n Please choose another!!nn";
-        player_turn();
-    }
-    /* Ends */
-    display_board();
-}
-
-//Function to get the game status e.g. GAME WON, GAME DRAW GAME IN CONTINUE MODE
-
-bool gameover(){
-    //checking the win for Simple Rows and Simple Column
-    for(int i=0; i<3; i++)
-    if(board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[0][i] == board[1][i] && board[0][i] == board[2][i])
-    return false;
-
-    //checking the win for both diagonal
-
-    if(board[0][0] == board[1][1] && board[0][0] == board[2][2] || board[0][2] == board[1][1] && board[0][2] == board[2][0])
-    return false;
-
-    //Checking the game is in continue mode or not
-    for(int i=0; i<3; i++)
-    for(int j=0; j<3; j++)
-    if(board[i][j] != 'X' && board[i][j] != 'O')
+bool winchecker(char arr[3][3])
+{
+if((arr[0][0]== 'X' && arr[1][1]=='X' && arr[2][2]=='X') || (arr[0][2]=='X' && arr[1][1]=='X' && arr[2][0]=='X') || (arr[0][0]=='X' && arr[0][1]=='X' && arr[0][2]=='X') || (arr[1][0]=='X' && arr[1][1]=='X' && arr[1][2]=='X') || (arr[2][0]=='X' && arr[2][1]=='X' && arr[2][2]=='X') || (arr[0][0]=='X' && arr[1][0]=='X' && arr[2][0]=='X') || (arr[0][1]=='X' && arr[1][1]=='X' && arr[2][1]=='X') || (arr[0][2]=='X' && arr[1][2]=='X' && arr[2][2]=='X') || (arr[0][0]=='O' && arr[1][1]=='O' && arr[2][2]=='O') || (arr[0][2]=='O' && arr[1][1]=='O' && arr[2][0]=='O') || (arr[0][0]=='O' && arr[0][1]=='O' && arr[0][2]=='O') || (arr[1][0]=='O' && arr[1][1]=='O' && arr[1][2]=='O') || (arr[2][0]=='O' && arr[2][1]=='O' && arr[2][2]=='O') || (arr[0][0]=='O' && arr[1][0]=='O' && arr[2][0]=='O') || (arr[0][1]=='O' && arr[1][1]=='O' && arr[2][1]=='O') || (arr[0][2]=='O' && arr[1][2]=='O' && arr[2][2]=='O'))
     return true;
-
-    //Checking the if game already draw
-    draw = true;
+    else
     return false;
 }
-
-//Program Main Method
+void display_board(char arr[3][3])
+{
+for(int i=0;i<3;i++)
+{
+for(int j=0;j<3;j++)
+{
+ cout<<" "<<arr[i][j]<<" "<<"|";
+}
+cout<<endl;
+}
+}
 
 int main()
 {
-    cout<<"tttT I C K -- T A C -- T O E -- G A M Ettt";
-    cout<<"nttttFOR 2 PLAYERSnttt";
-    while(gameover()){
-        display_board();
-        player_turn();
-        gameover();
+ system("color 3F");
+ char arr[3][3],usymbol,csymbol;
+ int i,j,counter=0;
+ srand(time(0));
+for(int i=0;i<3;i++)
+{
+
+    for(int j=0;j<3;j++)
+    arr[i][j]='-';
+}
+ int r=rand();
+ cout<<"Welcome to TIC TAC TOE GAME!\n";
+ display_board(arr);
+ cout<<"Choose your symbol(X or O) ";
+ cin>>usymbol;
+ if(usymbol!='X' && usymbol!='O')
+ {
+    cout<<"Enter a valid symbol!!\n";
+ cin>>usymbol;
+ }
+ csymbol=(usymbol=='X'?'O':'X');
+ int turn=r%2;
+ while(counter!=9)
+{
+if(turn==0)
+{
+    while(1)
+    {
+    cout<<"Your chance\n";
+    cout<<"Enter the grid no to fill ";
+    cin>>i>>j;
+    if(arr[i][j]!='-')
+        cout<<"invalid move!!Enter again\n";
+    else
+    {
+    arr[i][j]=usymbol;
+    display_board(arr);
+    if(winchecker(arr))
+    {
+        cout<<"Congratulations! You Win!!\n";
+        return 0;
     }
-    if(turn == 'X' && draw == false){
-        cout<<"nnCongratulations!Player with 'X' has won the game";
+    turn=1;
+    break;
     }
-    else if(turn == 'O' && draw == false){
-        cout<<"nnCongratulations!Player with 'O' has won the game";
+}
+}
+else
+{
+    cout<<"computer's turn\n";
+while(1)
+{
+    if(r%9==0 && arr[0][0]=='-')
+    {
+    arr[0][0]=csymbol;
+    break;
+    }
+    else if(r%9==1 && arr[0][1]=='-')
+    {
+    arr[0][1]=csymbol;
+    break;
+}
+    else if(r%9==2 && arr[0][2]=='-')
+    {
+    arr[0][2]=csymbol;
+    break;
+    }
+    else if(r%9==3 && arr[1][0]=='-')
+    {
+    arr[1][0]=csymbol;
+    break;
+}
+    else if(r%9==4 && arr[1][1]=='-')
+    {
+    arr[1][1]=csymbol;
+    break;
+    }
+    else if(r%9==5 && arr[1][2]=='-')
+    {
+    arr[1][2]=csymbol;
+    break;
+    }
+    else if(r%9==6 && arr[2][0]=='-')
+    {
+    arr[2][0]=csymbol;
+    break;
+    }
+    else if(r%9==7 && arr[2][1]=='-')
+    {
+    arr[2][1]=csymbol;
+    break;
+    }
+    else if(r%9==8 && arr[2][2]=='-')
+    {
+    arr[2][2]=csymbol;
+    break;
     }
     else
-    cout<<"nnGAME DRAW!!!nn";
-} 
+    {
+   r=(r+1)%9;
+    }
+}
+display_board(arr);
+if(winchecker(arr))
+{
+cout<<"You Lost!! Better luck next time\n";
+return 0;
+}
+turn=0;
+}
+counter++;
+}
+if(counter==9)
+cout<<"Draw Match!!Well Played!\n";
+return 0;
+}

689d295c6cae2c665dad7c11676392b69d2b67d6
Rakshit Gupta <114903166+rakshitgupta23@users.noreply.github.com>
2022-10-08 01:38:55 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1977,4 +1977,9 @@ contributors = [
     fullname: "Benjamin Norman",
     username: "https://github.com/BenjaminN117",
   },
+  {
+    id: 393,
+    fullname: "Rakshit Gupta",
+    username: "https://github.com/rakshitgupta23",
+  },
 ];

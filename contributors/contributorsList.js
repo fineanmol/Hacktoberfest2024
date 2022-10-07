29fb429df962290de0d8fc7f66e1e346800db350
1Alisha <alishagupta314@gmail.com>
2022-10-07 09:45:28 +0530

    Name added

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1949,6 +1949,11 @@ contributors = [
     id:387,
     fullname: "Navneet Raj",
     username: "https://github.com/2nav"
+  },
+  {
+    id:388,
+    fullname: "Alisha Gupta",
+    username: "https://github.com/1Alisha"
   }
 
 ];

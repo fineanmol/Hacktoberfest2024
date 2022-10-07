14fe9b0ccf974f7b86eb955603d1d113fc0189e7
NANDKISHOR R <rachulr7@gmail.com>
2022-10-07 22:46:45 +0530

    updated program's_contribute
    Added a contribution in c programing for insertion sort and added my name in contributors list.

---
+++ Program's_Contributed_By_Contributors/C++/insertion.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, j, temp;
+    int arr[60];
+    printf("Enter number of elements\n");
+    scanf("%d", &n);
+    printf("Enter %d integers\n", n);
+    for (i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (i = 1; i < n; i++) 
+    {
+        j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) 
+        {
+            temp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = temp;
+            j--;
+        }
+    }
+    printf("Sorted list in ascending order:\n");
+    for (i = 0; i < n; i++) 
+    {
+        printf("%d\t", arr[i]);
+    }
+    return 0;
+}

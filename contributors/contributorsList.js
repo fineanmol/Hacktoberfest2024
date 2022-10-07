d236c157a0de24bd31123c8415d7c9a8422d0e65
suraj keshari <84243221+surajkeshari@users.noreply.github.com>
2022-10-07 09:53:54 +0530

    updates contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1954,6 +1954,11 @@ contributors = [
     id: 388,
     fullname: "Abhishek Anand",
     username: "https://github.com/picturepatch07"
+  },
+  {
+    id: 389,
+    fullname: "suraj keshari",
+    username: "https://github.com/surajkeshari"
   }
 
 ];

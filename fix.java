25b4ab21a73fb64143f5fe75ce845f2a8ea0c323
harshvijay <72139720+harshvijay63@users.noreply.github.com>
2022-10-07 09:25:52 +0530

    fixed_cube

---
+++ fix.java
@@ -0,0 +1,15 @@
+public class fix {
+
+  
+static void cube()  
+{  
+int x=7*7*7;  
+System.out.println("Cube of 7 is: "+x);    
+}  
+public static void main(String args[])  
+{  
+  
+cube();    
+}  
+} 
+

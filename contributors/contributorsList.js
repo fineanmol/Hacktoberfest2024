dea8cab9a73aa6931a43d776d88badd322473f89
Avin Divakara <67309607+AVDiv@users.noreply.github.com>
2022-10-07 21:37:36 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1977,4 +1977,9 @@ contributors = [
     fullname: "Benjamin Norman",
     username: "https://github.com/BenjaminN117",
   },
+  {
+    id: 393,
+    fullname: "Avin Divakara",
+    username: "https://github.com/AVDiv",
+  },
 ];

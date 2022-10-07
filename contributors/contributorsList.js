bf9eb6444dd4c1e24b7c6f936b19ce69ca6c949f
DanielB <bradtmoeller@gmail.com>
2022-10-07 19:08:08 +0200

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1977,4 +1977,9 @@ contributors = [
     fullname: "Benjamin Norman",
     username: "https://github.com/BenjaminN117",
   },
+   {
+    id: 392,
+    fullname: "Dan Bradt",
+    username: "https://github.com/denschiro"
+  }
 ];

bbacbd21cc0ec5cfafbb33e9681c8a0ec4756eec
Agis R Herdiana <agis.rahma.herdiana@gmail.com>
2022-10-07 10:18:06 +0700

    added my name

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1949,6 +1949,11 @@ contributors = [
     id:387,
     fullname: "Navneet Raj",
     username: "https://github.com/2nav"
+  },
+  {
+    id:388,
+    fullname: "Agis R Herdiana",
+    username: "https://github.com/agisrh"
   }
 
 ];

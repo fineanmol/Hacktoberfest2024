0bf9c80459a5dfa047aa2c54f2bf977b729687af
himanshu30-max <banswalhimanshu693@gmail.com>
2022-10-07 13:26:53 +0530

    Peak index in a mountain array code implemented

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1949,6 +1949,11 @@ contributors = [
     id:387,
     fullname: "Navneet Raj",
     username: "https://github.com/2nav"
+  },
+  {
+    id:388,
+    fullname: "Himanshu Banswal",
+    username: "https://github.com/himanshu30-max"
   }
 
 ];

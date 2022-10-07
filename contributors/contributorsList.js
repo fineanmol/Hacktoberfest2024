a15c367b4e4b9fe04b3eb26f23cb1378a693414c
Tejas Singh <notnormalcoder@gmail.com>
2022-10-07 06:57:48 +0530

    Program for Heap Sort using C++

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1954,6 +1954,11 @@ contributors = [
     id: 388,
     fullname: "Abhishek Anand",
     username: "https://github.com/picturepatch07"
+  },
+  {
+    id: 389,
+    fullname: "Tejas Singh",
+    username: "https://github.com/ec-026"
   }
-
+  
 ];

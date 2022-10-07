575069a418b8c1d0e98424c78faf25d7ec50017b
MyNamesTim <99489258+MyNamesTim@users.noreply.github.com>
2022-10-07 14:57:00 -0400

    Add files via upload

---
+++ Program's_Contributed_By_Contributors/Python_Programs/lag.py
@@ -0,0 +1,10 @@
+import time
+
+print('youre going to want to kill this task in a bit')
+time.sleep(1)
+loop = 1
+while loop > 0:
+    print('hi')
+    time.sleep(.0000000001)
+    print('hello')
+    time.sleep(.0000000001)

80a066193cad18f901f2db64b9d066e74a2623cd
Akash Singhal <singhlakash1234@gmail.com>
2022-10-07 20:57:56 +0530

    add akash

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1977,4 +1977,9 @@ contributors = [
     fullname: "Benjamin Norman",
     username: "https://github.com/BenjaminN117",
   },
+  {
+    id: 393,
+    fullname: "Akash Singhal",
+    username: "https://github.com/Akashsinghal1234",
+  }
 ];

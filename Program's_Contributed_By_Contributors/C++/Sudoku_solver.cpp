98ee2367ec640e454e0a2cbf75885bf6a1260b43
Yash Kumar <70716232+yash5015@users.noreply.github.com>
2022-10-08 23:51:34 +0530

    added Sudoku algorithm

---
+++ Program's_Contributed_By_Contributors/C++/Sudoku_solver.cpp
@@ -0,0 +1,77 @@
+// Contributed by - yash Kumar <yashrathor5015@gmail.com>
+
+// Sudokku solver
+
+// Time Complexity: O(9(n ^ 2)), in the worst case, for each cell in the n2 board, we have 9 possible numbers.
+
+// Space Complexity: O(1), since we are refilling the given board itself, there is no extra space required, so constant space complexity.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isValid(vector<vector<char>> &board, int row, int col, char c)
+{
+    for (int i = 0; i < 9; i++)
+    {
+        if (board[i][col] == c)
+            return false;
+
+        if (board[row][i] == c)
+            return false;
+
+        if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+            return false;
+    }
+    return true;
+}
+
+bool solveSudoku(vector<vector<char>> &board)
+{
+    for (int i = 0; i < board.size(); i++)
+    {
+        for (int j = 0; j < board[0].size(); j++)
+        {
+            if (board[i][j] == '.')
+            {
+                for (char c = '1'; c <= '9'; c++)
+                {
+                    if (isValid(board, i, j, c))
+                    {
+                        board[i][j] = c;
+
+                        if (solveSudoku(board))
+                            return true;
+                        else
+                            board[i][j] = '.';
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+    return true;
+}
+int main()
+{
+    vector<vector<char>> board{
+        {'9', '5', '7', '.', '1', '3', '.', '8', '4'},
+        {'4', '8', '3', '.', '5', '7', '1', '.', '6'},
+        {'.', '1', '2', '.', '4', '9', '5', '3', '7'},
+        {'1', '7', '.', '3', '.', '4', '9', '.', '2'},
+        {'5', '.', '4', '9', '7', '.', '3', '6', '.'},
+        {'3', '.', '9', '5', '.', '8', '7', '.', '1'},
+        {'8', '4', '5', '7', '9', '.', '6', '1', '3'},
+        {'.', '9', '1', '.', '3', '6', '.', '7', '5'},
+        {'7', '.', '6', '1', '8', '5', '4', '.', '9'}};
+
+    solveSudoku(board);
+
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+            cout << board[i][j] << " ";
+        cout << "\n";
+    }
+    return 0;
+}

c344e042fb1628a89a81c5eabc998602eb6934ac
aniket-2002 <ac745273@gmail.com>
2022-10-08 19:24:31 +0530

    rockpapergame

---
+++ rockpapergame.py
@@ -0,0 +1,39 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+
+options = ["rock", "paper", "scissors"]
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break
+
+    if user_input not in options:
+        continue
+
+    random_number = random.randint(0, 2)
+    # rock: 0, paper: 1, scissors: 2
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+print("You won", user_wins, "times.")
+print("The computer won", computer_wins, "times.")
+print("Goodbye!")

24190b6c42a1fc153a7d9318374d79fc1fc7afff
Yannick Fuchs <yannick.fuchs@seitenbau.com>
2022-10-08 18:27:17 +0200

    Add name to contributorslist

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1968,4 +1968,9 @@ contributors = [
     fullname: "Isnani",
     username: "https://github.com/Isnani25",
   },
+  {
+    id: 411,
+    fullname: "Yannick Fuchs",
+    username: "https://github.com/Fuchsi94",
+  },
 ];

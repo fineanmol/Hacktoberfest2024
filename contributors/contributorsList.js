e2dd2d690903e0effb984a0b08e9fae2305631e6
rohitkoh123 <kohlirohit10@gmail.com>
2022-10-09 01:27:05 +0530

    name added in contributors

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1968,4 +1968,9 @@ contributors = [
     fullname: "Isnani",
     username: "https://github.com/Isnani25",
   },
+  {
+    id: 411,
+    fullname: "Rohit Kohli",
+    username: "https://github.com/rohitkoh123",
+  },
 ];

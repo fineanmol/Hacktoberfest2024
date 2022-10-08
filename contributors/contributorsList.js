04b29f5fa5a2fcd105487a24721760761d927e08
NAEEMA ZIYAD <75922224+naeema-19@users.noreply.github.com>
2022-10-08 23:21:04 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1968,4 +1968,9 @@ contributors = [
     fullname: "Isnani",
     username: "https://github.com/Isnani25",
   },
+  {
+    id: 411,
+    fullname: "Naeema Ziyad",
+    username: "https://github.com/naeema-19",
+  },
 ];

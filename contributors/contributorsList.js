f55c737526803be75602c68c397b3652e4ceb799
Vivek jha <vivekjha3080@gmail.com>
2022-10-08 23:04:58 +0530

    Load more button

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1968,4 +1968,9 @@ contributors = [
     fullname: "Isnani",
     username: "https://github.com/Isnani25",
   },
+  {
+    id: 411,
+    fullname: "Vivek Jha",
+    username: "https://github.com/duckling69",
+  }
 ];

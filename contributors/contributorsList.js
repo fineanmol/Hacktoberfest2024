3c7ce1731d64da4f28cc362458f3028639e6c5a3
lvfei <lvfei@kuaishou.com>
2022-10-08 13:12:51 +0800

    Relevant message

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1985,4 +1985,9 @@ contributors = [
     fullname: "Matvey Romanov",
     username: "https://github.com/ra1nbow1",
   },
+  {
+    id: 394,
+    fullname: "sablea",
+    username: "https://github.com/sablea",
+  },
 ];

3e62f39e4c49111870c6ca0c92e5cd76dbca492b
YASH-VATS-7 <114814047+YASH-VATS-7@users.noreply.github.com>
2022-10-08 07:37:49 +0530

    Create Squaare.java
    Hacktoberfest event

---
+++ Squaare.java
@@ -0,0 +1,12 @@
+// AREA OF THE SQUARE
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,area;
+    cout<<"Enter the value of a\n";
+    cin>>a;
+    area=a*a;
+cout<<"Area of the square is : "<<area;
+}

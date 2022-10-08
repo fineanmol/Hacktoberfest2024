966890726d8f3fadc301dd7ee3c6a796f3d63c4d
Mohit Panthri <99413629+mohitpanthri@users.noreply.github.com>
2022-10-08 22:12:05 +0530

    Added Mohit Panthri to Contributers.md

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1968,4 +1968,9 @@ contributors = [
     fullname: "Isnani",
     username: "https://github.com/Isnani25",
   },
+  {
+    id: 411,
+    fullname: "Mohit Panthri",
+    username: "https://github.com/mohitpanthri",
+  },
 ];

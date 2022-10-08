c391766edffcd38766d58108e37da32b7eac2dab
Robino <aashu.kumar2021@vitstudent.ac.in>
2022-10-08 13:01:56 +0530

    Added name to contributor's list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1985,4 +1985,9 @@ contributors = [
     fullname: "Matvey Romanov",
     username: "https://github.com/ra1nbow1",
   },
+  {
+    id: 395,
+    fullname: "Aashu Kumar",
+    username: "https://github.com/Robino-Aashu",
+  },
 ];

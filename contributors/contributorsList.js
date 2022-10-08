f97727efba9f90d63a1064893e41ff04d94d5587
justinmraj1984 <justinmraj.1984@gmail.com>
2022-10-08 15:13:41 +0530

    Adding my name to the contributor list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1989,5 +1989,10 @@ contributors = [
     id: 395,
     fullname: "Dan Bradt",
     username: "https://github.com/denschiro"
+  },
+   {
+    id: 396,
+    fullname: "Justin Michael Raj",
+    username: "https://github.com/justinmraj1984"
   }
 ];

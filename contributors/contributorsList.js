c60d3640f149a56cb1a611bba51703c3b28afede
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2022-10-08 14:12:50 +0000

    feat: added details to contributors list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2066,4 +2066,9 @@ contributors = [
     fullname: "Shubham Rawat",
     username: "https://github.com/shubhamrawat090",
   },
+  {
+    id: 410,
+    fullname: "Adeoluwa Agbakosi",
+    username: "https://github.com/Dev-180Memes",
+  },
 ];

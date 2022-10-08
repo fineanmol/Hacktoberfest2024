987607481e4d0e2f16803be8c91b5dfb1f383392
dvcone <dani2014riau@gmail.com>
2022-10-08 16:59:00 +0700

    I add myself to contributors

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1994,5 +1994,10 @@ contributors = [
     id: 396,
     fullname: "Shruti Mishra",
     username: "https://github.com/ShrutiMishra-2002"
+  },
+  {
+    id: 397,
+    fullname: "Hakim Madani",
+    username: "https://github.com/DVCone"
   }
 ];

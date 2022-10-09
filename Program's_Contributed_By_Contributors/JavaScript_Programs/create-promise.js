f01e8a0739a29cc62b5d7bc3d5e83d66dd222765
tbacon781 <tylerbacon781@gmail.com>
2022-10-09 09:38:51 -0400

    Fixed syntax

--- Program's_Contributed_By_Contributors/JavaScript_Programs/create-promise.js
+++ Program's_Contributed_By_Contributors/JavaScript_Programs/create-promise.js
@@ -1,13 +1,12 @@
 export function createPromise(secret) {
-    const delay = (0.5 + Math.random() * 2) * 1000;
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (secret === "hacktoberfest") {
-          resolve();
-        } else {
-          reject(new Error('Wrong secret!'));
-        }
-      }, delay);
-    });
-  }
-  
+  const delay = (0.5 + Math.random() * 2) * 1000;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (secret === "Hacktoberfest") {
+        resolve();
+      } else {
+        reject(new Error("Wrong secret!"));
+      }
+    }, delay);
+  });
+}

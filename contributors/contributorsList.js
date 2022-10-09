af7a006a56e49dc8c02eaa53493dba75716c077d
TheViralClovers <theviralclovers@gmail.com>
2022-10-09 18:40:51 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1976,5 +1976,9 @@ id: 411,
     fullname: "Isnani",
     username: "https://github.com/Isnani25",
   },
-
+  {
+    id:412,
+    fullname: "Dhruva S Kashyap",
+    useername: "TheViralClovers",
+  },
 ];

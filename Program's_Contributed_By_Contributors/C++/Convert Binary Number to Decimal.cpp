9fbf336fd527b42ebf1ea4dd1682cb02fe34c08a
Amar <96937608+Amarnath-Rao@users.noreply.github.com>
2022-10-09 11:15:50 +0530

    Create Convert Binary Number to Decimal.cpp

---
+++ Program's_Contributed_By_Contributors/C++/Convert Binary Number to Decimal.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int convert(long long);
+
+int main() {
+  long long n;
+  printf("Enter a binary number: ");
+  scanf("%lld", &n);
+  printf("%lld in binary = %d in decimal", n, convert(n));
+  return 0;
+}
+
+int convert(long long n) {
+  int dec = 0, i = 0, rem;
+
+  while (n!=0) {
+    rem = n % 10;
+    n /= 10;
+    dec += rem * pow(2, i);
+    ++i;
+  }
+  return dec;
+}

b398b17882740edaba7ef4e1bf99c832c9431254
kavya98527 <imkavya1998@gmail.com>
2022-10-09 19:32:36 +0530

    Adding python Open Port Scanner code

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1976,5 +1976,10 @@ id: 411,
     fullname: "Isnani",
     username: "https://github.com/Isnani25",
   },
+  {
+    id: 412,
+    fullname: "Kavya",
+    username: "https://github.com/kavya98527",
+  },
 
 ];

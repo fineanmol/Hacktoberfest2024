1169b66c5b3b7010244a9543be93572910d29781
Thusitha karunathilaka <56513558+Thusiya05@users.noreply.github.com>
2022-10-10 02:12:30 +0530

    Update add_2_numbers.java

--- Program's_Contributed_By_Contributors/Java_Programs/add_2_numbers.java
+++ Program's_Contributed_By_Contributors/Java_Programs/add_2_numbers.java
@@ -2,8 +2,8 @@ public class SumOfNumbers1
 {  
 public static void main(String args[])   
 {  
-int n1 = 225, n2 = 115, sum;  
-sum = n1 + n2;  
+int x = 225, y = 115, sum;  
+sum = x + y;  
 System.out.println("The sum of numbers is: "+sum);  
 }  
 }  

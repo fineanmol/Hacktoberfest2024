d01c51085eb97b0f6506374abff4af1d8df30d7d
Harshavardhan Adepu <adepu@wisc.edu>
2022-10-08 20:06:11 -0500

    added harshavar1 to contributersList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1975,6 +1975,11 @@ contributors = [
 id: 411,
     fullname: "Isnani",
     username: "https://github.com/Isnani25",
+  },{
+id: 412,
+    fullname: "Harsha vardhan",
+    username: "https://github.com/harshavar1",
   },
+
 
 ];

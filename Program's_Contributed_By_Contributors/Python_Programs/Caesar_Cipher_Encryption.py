3dfaf076ddb3cb04635c3fbcb3ec1a288120192a
Bishal Aryal <70318933+C15C01337@users.noreply.github.com>
2022-10-09 10:51:12 +0545

    Create Caesar_Cipher_Encryption.py

---
+++ Program's_Contributed_By_Contributors/Python_Programs/Caesar_Cipher_Encryption.py
@@ -0,0 +1,18 @@
+#caesar cipher encryption | Twitter @C15C01337
+
+message = input('Enter a message: ')
+key = int(input('Enter a key: '))
+def encrypt_char(char, key):
+    return chr(ord('A') + (ord(char) - ord('A') + key) % 26)
+
+def encrypt_message(message, key):
+    message = message.upper()
+    cipher = ''
+    for char in message:
+        if char not in ' ,.':
+            cipher += encrypt_char(char, key)
+        else:
+            cipher += char
+    print(cipher)
+    return cipher
+encrypt_message(message, key)

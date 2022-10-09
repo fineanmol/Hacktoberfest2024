de1c731befff0088546e6ab88a27fa513ca5c4d5
codemastershrey <79367994+codemastershrey@users.noreply.github.com>
2022-10-09 10:36:31 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1976,5 +1976,11 @@ id: 411,
     fullname: "Isnani",
     username: "https://github.com/Isnani25",
   },
+    {
+id: 412,
+    fullname: "Shrey",
+    username: "https://github.com/codemastershrey",
+  },
+    
 
 ];

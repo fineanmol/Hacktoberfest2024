ccfc78d0f2d3d36f073a10c080e467bed1d1315b
Harsh Raj Jha <84229424+Hrjha30@users.noreply.github.com>
2022-10-09 14:39:58 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1976,5 +1976,9 @@ id: 411,
     fullname: "Isnani",
     username: "https://github.com/Isnani25",
   },
-
+{
+    id: 412
+    fullname: "Harsh Raj Jha",
+    username: "https://github.com/Hrjha30",
+  },
 ];

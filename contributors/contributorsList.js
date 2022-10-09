135964d99c00178519bc27bc9729e13e9ae15838
Kashyap Shah <kashyapshah9100@gmail.com>
2022-10-09 17:32:31 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1840,4 +1840,9 @@ contributors = [
     fullname: "Sabin Thapa",
     username: "https://github.com/sabin-thapa",
   },
+  {
+    id:368,
+    fullname: "Kashyap Shah",
+    username: "https://github.com/kashyap-shah",
+  },
 ];

ee0f14bc5da4764000121123de42a550ab5319c8
santosh <sahsantosh257@gmail.com>
2022-10-10 18:57:09 +0545

    Indentation error solved

--- star.py
+++ star.py
@@ -4,6 +4,6 @@ num_of_sides = 5
 length_of_side = 50
 each_angle = 720.0 / num_of_sides
 for i in range(num_of_sides):
-star.forward(length_of_side)
-star.right(each_angle)
+    star.forward(length_of_side)
+    star.right(each_angle)
 turtle.done()

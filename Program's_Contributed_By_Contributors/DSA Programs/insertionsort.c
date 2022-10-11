f184e3389e0a56dbcc306e99830786d728b84c1c
jacobs1242 <115448394+jacobs1242@users.noreply.github.com>
2022-10-11 22:28:40 +0530

    Create insertionsort.c

---
+++ Program's_Contributed_By_Contributors/DSA Programs/insertionsort.c
@@ -0,0 +1,36 @@
+// Insertion sort in C
+
+#include <stdio.h>
+
+// Function to print an array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+}
+
+void insertionSort(int array[], int size) {
+  for (int step = 1; step < size; step++) {
+    int key = array[step];
+    int j = step - 1;
+
+    // Compare key with each element on the left of it until an element smaller than
+    // it is found.
+    // For descending order, change key<array[j] to key>array[j].
+    while (key < array[j] && j >= 0) {
+      array[j + 1] = array[j];
+      --j;
+    }
+    array[j + 1] = key;
+  }
+}
+
+// Driver code
+int main() {
+  int data[] = {9, 5, 1, 4, 3};
+  int size = sizeof(data) / sizeof(data[0]);
+  insertionSort(data, size);
+  printf("Sorted array in ascending order:\n");
+  printArray(data, size);
+}

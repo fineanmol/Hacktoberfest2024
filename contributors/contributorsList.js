714d6ad8070fed47f23044ed62f8990d49ff8d8a
Akshat323 <107712242+Akshat323@users.noreply.github.com>
2022-10-12 01:15:50 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2029,6 +2029,11 @@ contributors = [
     fullname: "Arshpreet Singh Bedi",
     username: "https://github.com/avibedi1768",
   },
-
+    
+    {
+      id: 419,
+      fullname: "Akshat Jain",
+      username:"https://github.com/Akshat323"  
+    },
 
 ];

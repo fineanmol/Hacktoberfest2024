2ba09bb5338134ac146f8e237c9905225671ee7e
Ayush Sharma <62910168+ayushtenguriya@users.noreply.github.com>
2022-10-11 19:04:14 +0530

    class extend thread class

---
+++ Program's_Contributed_By_Contributors/Java_Programs/Java Thread ex extend class thread.java
@@ -0,0 +1,9 @@
+class Multi extends Thread{  
+public void run(){  
+System.out.println("thread is running...");  
+}  
+public static void main(String args[]){  
+Multi t1=new Multi();  
+t1.start();  
+ }  
+}

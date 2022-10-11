9f6356f37c8414f5ef72b6697a17d2e874ea7b3d
Skub123 <48567636+Skub123@users.noreply.github.com>
2022-10-12 00:01:20 +0530

    Create SumAndAverageArray.cpp
    Sum and Average of Array

---
+++ Program's_Contributed_By_Contributors/C++ Programs/Array/SumAndAverageArray.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+  // initialize an array without specifying size
+  double numbers[] = {7, 5, 6, 12, 35, 27};
+
+  double sum = 0;
+  double count = 0;
+  double average;
+
+  cout << "The numbers are: ";
+
+  //  print array elements
+  // use of range-based for loop
+  for (const double &n : numbers) {
+    cout << n << "  ";
+
+    //  calculate the sum
+    sum += n;
+
+    // count the no. of array elements
+    ++count;
+  }
+
+  // print the sum
+  cout << "\nTheir Sum = " << sum << endl;
+
+  // find the average
+  average = sum / count;
+  cout << "Their Average = " << average << endl;
+
+  return 0;
+}

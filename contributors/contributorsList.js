ffb0ddc6f99cfc94544f8437f19090d324079c0c
VARISH GAUTAM <48176176+Variiiest@users.noreply.github.com>
2022-10-13 03:56:30 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2029,6 +2029,12 @@ contributors = [
     fullname: "Arshpreet Singh Bedi",
     username: "https://github.com/avibedi1768",
   },
+  
+  {
+    id: 419,
+    fullname: "Varish Gautam",
+    username: "https://github.com/variiiest",
+  },
 
 
 ];

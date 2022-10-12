97c050881e2a2b1d799e2bea3f27b6a048cf0dcd
Akash verma <akash.verma2580@gmail.com>
2022-10-13 02:56:35 +0530

    Added Lusifer65

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1999,9 +1999,14 @@ contributors = [
     fullname: "Alejandro Armenta",
     username: "https://github.com/alexanderNWO",
   },
-];
- id: 415,
+  {
+    id: 415,
     fullname: "Krish Katyal",
     username: "https://github.com/krishkatyal",
+  },
+  {
+    id: 416,
+    fullname: "Akash verma",
+    username: "https://github.com/lusifer65",
   },
 ];

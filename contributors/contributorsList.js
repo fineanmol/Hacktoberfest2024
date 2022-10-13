4278352ae95d2dd1dda8f04afd355917c74c4d19
Manav Ukani <manavukani@yahoo.com>
2022-10-14 02:58:30 +0530

    Updated Contributor list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2039,6 +2039,11 @@ contributors = [
     id:420,
     fullname:"Auro S.",
     username:"https://github.com/aurocodes",
-  }
+  },
+  {
+    id: 421,
+    fullname: "Manav Ukani",
+    username: "https://github.com/manavukani",
+  },
 
 ];

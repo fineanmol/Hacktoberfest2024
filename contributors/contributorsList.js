c4f90a5ad816f8510193e2cf4aa7fbbbb6f51c8b
Ali Raza <92033640+bhaisahib122@users.noreply.github.com>
2022-10-13 13:20:36 +0500

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2029,6 +2029,11 @@ contributors = [
     fullname: "Arshpreet Singh Bedi",
     username: "https://github.com/avibedi1768",
   },
+  {
+    id: 419,
+    fullname: "Ali Raza",
+    username: "https://github.com/bhaisahib122",
+  },
 
 
 ];

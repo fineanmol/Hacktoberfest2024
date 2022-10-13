9881fc1bf28ecd54dc2dc01f52a06f935c150284
Christian Durazo <ctdurazo@users.noreply.github.com>
2022-10-13 09:48:51 -0700

    adding contributor

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2034,5 +2034,10 @@ contributors = [
     fullname: "Amit Kumar",
     username: "https://github.com/amit-kumar",
   },
+  {
+    id: 420,
+    fullname: "Christian Durazo",
+    username: "https://github.com/ctdurazo",
+  },
 
 ];

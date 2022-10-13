fdded58c6b92062f6a2bb781655d78ba7840f10e
Ankit Akash <75488501+ankit-akash@users.noreply.github.com>
2022-10-14 05:20:39 +0530

    Create quicksort.c

---
+++ C/quicksort.c
@@ -0,0 +1,46 @@
+//C program to sort the elements by using the QUICKSORT technique
+
+#include<stdio.h>
+void quicksort(int number[25],int first,int last){
+   int i, j, pivot, temp;
+   if(first<last){
+      pivot=first;
+      i=first;
+      j=last;
+      while(i<j){
+         while(number[i]<=number[pivot]&&i<last)
+         i++;
+         while(number[j]>number[pivot])
+           j--;
+         if(i<j){
+            temp=number[i];
+            number[i]=number[j];
+            number[j]=temp;
+         }
+      }
+      temp=number[pivot];
+      number[pivot]=number[j];
+      number[j]=temp;
+      quicksort(number,first,j-1);
+      quicksort(number,j+1,last);
+   }
+}
+int main(){
+   int i, count, number[25];
+   printf("How many elements are u going to enter?: ");
+   scanf("%d",&count);
+   printf("Enter %d elements: ", count);
+   for(i=0;i<count;i++)
+   scanf("%d",&number[i]);
+   quicksort(number,0,count-1);
+   printf("Order of Sorted elements: ");
+   for(i=0;i<count;i++)
+   printf(" %d",number[i]);
+   return 0;
+}
+
+
+//OUTPUT:
+//How many elements are u going to enter?: 5
+//Enter 10 elements: 2 7 4 1 9
+//Order of Sorted elements: 1 2 4 7 9

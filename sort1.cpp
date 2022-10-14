1d2c6bdc106b79e0d1b2d6ab8522a135e0125bdb
Ritikraja07 <Ritik.rock121@gmail.com>
2022-10-14 19:53:36 +0530

    Please merge my contribution

---
+++ sort1.cpp
@@ -0,0 +1,56 @@
+// C++ program for implementation of
+// selection sort
+#include <bits/stdc++.h>
+using namespace std;
+
+//Swap function
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+	int i, j, min_idx;
+
+	// One by one move boundary of
+	// unsorted subarray
+	for (i = 0; i < n-1; i++)
+	{
+	
+		// Find the minimum element in
+		// unsorted array
+		min_idx = i;
+		for (j = i+1; j < n; j++)
+		if (arr[j] < arr[min_idx])
+			min_idx = j;
+
+		// Swap the found minimum element
+		// with the first element
+		if(min_idx!=i)
+			swap(&arr[min_idx], &arr[i]);
+	}
+}
+
+//Function to print an array
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i=0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+// Driver program to test above functions
+int main()
+{
+	int arr[] = {64, 25, 12, 22, 11};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	selectionSort(arr, n);
+	cout << "Sorted array: \n";
+	printArray(arr, n);
+	return 0;
+}
+// This is code is contributed by rathbhupendra

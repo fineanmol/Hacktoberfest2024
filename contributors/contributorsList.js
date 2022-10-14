5e535bcf9545e9a0abad538ae466477c74d59218
Himalaya Pal <45174860+palhimalaya@users.noreply.github.com>
2022-10-14 11:16:00 +0545

    added new automatic-birthday wisher program project

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2040,5 +2040,10 @@ contributors = [
     fullname:"Auro S.",
     username:"https://github.com/aurocodes",
   }
+  {
+    id: 421,
+    fullname: "Himalaya Pal",
+    username: "https://github.com/palhimalaya",
+  }
 
 ];

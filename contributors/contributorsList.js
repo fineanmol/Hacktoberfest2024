6b06c1ca03ed4ba71f0dcc4dccf046f59adc1b71
unknown <chowdhurysoham007@gmail.com>
2022-10-15 19:53:10 +0530

    Add Soham Chowdhury to Contributors List

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2040,5 +2040,10 @@ contributors = [
     fullname:"Auro S.",
     username:"https://github.com/aurocodes",
   }
+  {
+    id:421,
+    fullname:"Soham Chowdhury"
+    username:"https://github.com/Mesoham",
+  }
 
 ];

52c378c29ff25e95188a3cfef2a0128241f04d24
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2022-10-15 10:59:52 +0530

    contributor

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2039,6 +2039,11 @@ contributors = [
     id:420,
     fullname:"Auro S.",
     username:"https://github.com/aurocodes",
+  },
+  {
+    id:421,
+    fullname:"Debajyoti Bhattacharjee",
+    username:"https://github.com/debajyoti2050",
   }
 
 ];

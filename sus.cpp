e175b6529d76c73045aa3b4ff9a47cd5ca391442
rajan4705 <75534410+rajan4705@users.noreply.github.com>
2022-10-15 20:21:01 +0530

    Create sus.cpp

---
+++ sus.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        long long int a,b;
+        cin>>a>>b;
+        if((b-a) % 3 == 2){
+            cout<<"NO"<<endl;}
+        else {cout<<"YES"<<endl;}    
+        
+    }
+return 0;
+}

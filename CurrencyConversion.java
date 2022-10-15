ffd3e7bce4af600b84deccf27055d9c7fb4aa5c1
ShrutikaKo <115891203+ShrutikaKo@users.noreply.github.com>
2022-10-15 22:41:43 +0530

    Create CurrencyConversion.java

---
+++ CurrencyConversion.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class CurrencyConversion
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int usd = 80;
+    int inr = n * usd;//100*80
+    System.out.println(inr);
+    
+	}
+}

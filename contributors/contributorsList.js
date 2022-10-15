5f168330d8106b1e8323a65520989a24e7a0baef
Joyjeet Mukherjee <96014361+joyjeetcoding@users.noreply.github.com>
2022-10-15 17:53:23 +0530

    updated the contributors list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2039,6 +2039,11 @@ contributors = [
     id:420,
     fullname:"Auro S.",
     username:"https://github.com/aurocodes",
+  },
+  {
+    id:421,
+    fullname:"Joyjeet Mukherjee",
+    username:"https://github.com/joyjeetcoding",
   }
 
 ];

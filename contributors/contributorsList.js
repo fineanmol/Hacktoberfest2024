6c46ca7b6f99936916a4ceaa54be14b505c879eb
domogami <domogami@gmail.com>
2022-10-15 16:46:14 -0700

    Added myself as a contributor

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2039,6 +2039,10 @@ contributors = [
     id:420,
     fullname:"Auro S.",
     username:"https://github.com/aurocodes",
-  }
-
+  },
+  {
+    id: 421,
+    fullname: "Dominick Lee",
+    username: "https://github.com/domogami/",
+  },
 ];

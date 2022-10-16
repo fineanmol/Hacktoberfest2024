d99055facd8cc79527136ed69e73afe53c7d5d9c
Atul Rajput <92659293+AtulRajput01@users.noreply.github.com>
2022-10-16 22:22:58 +0530

    Update selection.cpp

--- selection.cpp
+++ selection.cpp
@@ -1,44 +1,39 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-
-void display(vector<int> A, int n)
-{
-    cout << endl;
-    for (auto i : A)
-        cout << i << " ";
+void swapping(int &a, int &b) {         //swap the content of a and b
+   int temp;
+   temp = a;
+   a = b;
+   b = temp;
 }
-
-void S_S(vector<int> &A, int n)
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-        int min = A[i];
-        int pos = i;
-        for (int j = i + 1; j < n; j++)
-        {
-            if (A[j] < min)
-            {
-                min = A[j];
-                pos = j;
-            }
-        }
-        swap(A[i], A[pos]);
-    }
+void display(int *array, int size) {
+   for(int i = 0; i<size; i++)
+      cout << array[i] << " ";
+   cout << endl;
 }
-
-int main()
-{
-    vector<int> V;
-    int ch, n;
-    cout << "size: ";
-    cin >> n;
-    cout << "ENTER ARRY: ";
-    for (int i = 0; i < n; i++)
-    {
-        cin >> ch;
-        V.push_back(ch);
-    }
-
-    S_S(V, n);
-    display(V, n);
+void selectionSort(int *array, int size) {
+   int i, j, imin;
+   for(i = 0; i<size-1; i++) {
+      imin = i;   //get index of minimum data
+      for(j = i+1; j<size; j++)
+         if(array[j] < array[imin])
+            imin = j;
+         //placing in correct position
+         swap(array[i], array[imin]);
+   }
+}
+int main() {
+   int n;
+   cout << "Enter the number of elements: ";
+   cin >> n;
+   int arr[n];           //create an array with given number of elements
+   cout << "Enter elements:" << endl;
+   for(int i = 0; i<n; i++) {
+      cin >> arr[i];
+   }
+   cout << "Array before Sorting: ";
+   display(arr, n);
+   selectionSort(arr, n);
+   cout << "Array after Sorting: ";
+   display(arr, n);
 }

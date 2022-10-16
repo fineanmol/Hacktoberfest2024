a2c457a2f813a0d56f306ff1704fe2d5c41f9720
SohamDesai32 <202003021.sohamdak@student.xavier.ac.in>
2022-10-16 23:32:35 +0530

    Calculator in ReactJS for Hacktoberfest 2022

---
+++ Program's_Contributed_By_Contributors/Calculator/src/App.js
@@ -0,0 +1,12 @@
+import Calculator from "./components/Calculator";
+
+// import react from "react";
+function App() {
+  return (
+    <div>
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;

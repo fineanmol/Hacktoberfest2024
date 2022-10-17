1fc917382e11228a00c265eeed0c2db8f915454e
Anurag Roy <89125578+binarybeast880@users.noreply.github.com>
2022-10-17 18:27:43 +0530

    Updated contributor list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2039,6 +2039,10 @@ contributors = [
     id:420,
     fullname:"Auro S.",
     username:"https://github.com/aurocodes",
+  },
+  {
+    id:421,
+    fullname:"Anurag Roy",
+    username:"https://github.com/binarybeast880",
   }
-
 ];

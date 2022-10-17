caef8305bd95115a0e729072ec395f24e8fab82c
mannukeim <116020722+mannukeim@users.noreply.github.com>
2022-10-17 21:58:56 +0530

    Create SwapTwoNumbers.java

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,24 @@
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.20f, second = 2.45f;
+
+        System.out.println("--Before swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+
+        // Value of first is assigned to temporary
+        float temporary = first;
+
+        // Value of second is assigned to first
+        first = second;
+
+        // Value of temporary (which contains the initial value of first) is assigned to second
+        second = temporary;
+
+        System.out.println("--After swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+    }
+}

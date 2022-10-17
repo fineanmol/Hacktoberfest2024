f9630539fa2a14d880649cdd6e5bd4760e82a024
ShiviDev <12shivi.ani@gmail.com>
2022-10-17 22:33:46 +0530

    added swing calculator app

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2039,6 +2039,10 @@ contributors = [
     id:420,
     fullname:"Auro S.",
     username:"https://github.com/aurocodes",
+  },
+  {
+    id:421,
+    fullname:"Shivani",
+    username:"https://github.com/ShiviDev",
   }
-
 ];

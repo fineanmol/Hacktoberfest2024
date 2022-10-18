5adbdbc9e97f850dd301f226881c27bc93e68fc5
Uday Kharatmol <100768172+UdayKharatmol@users.noreply.github.com>
2022-10-18 14:10:45 +0530

    Update contributorsList.js
     automerge

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2046,5 +2046,11 @@ contributors = [
     fullname:"Gautami SInha",
     username:"https://github.com/amnotcreative",
   }
+    {
+    id: 1,
+    fullname: "Uday Kharatmol",
+    username: "https://github.com/UdayKharatmol",
+  },
+  
 
 ];

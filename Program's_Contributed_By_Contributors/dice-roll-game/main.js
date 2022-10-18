b88dae34eea51ae469dd59aaf2071db6405304bb
Shivam-Singh <sd.shivam.00@gmail.com>
2022-10-18 15:09:31 +0530

    Delete main.js

--- Program's_Contributed_By_Contributors/dice-roll-game/main.js
+++
@@ -1,9 +0,0 @@
-function result(){
-  var result=dice.roll()
-}
-dice = {
-        sides: 6,
-        roll: function () {
-         var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-         return randomNumber; 
-        }}

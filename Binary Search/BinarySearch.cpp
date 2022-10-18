0ed17f1a52f1ec5cbfa00adff024e5771203265e
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2022-10-18 12:51:39 +0530

    Create BinarySearch.cpp

---
+++ Binary Search/BinarySearch.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int binarySearch(int arr[], int l, int r, int x)
+{
+    if (r >= l) {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+        return binarySearch(arr, mid + 1, r, x);
+    }
+    return -1;
+}

e40984239dbae054b45b4ebc5a682a8918e7fbb8
Andrey Balbino <andreymelo98@gmail.com>
2022-10-18 17:07:23 -0300

    Adjust file

--- Program's_Contributed_By_Contributors/Java_Programs/ValidatePassword.java
+++ Program's_Contributed_By_Contributors/Java_Programs/ValidatePassword.java
@@ -1,5 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ValidatePassword {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String senha;
         while ((senha = br.readLine()) !=null) {
@@ -33,5 +37,49 @@ public class ValidatePassword {
         }
         return false;
     }
+
+    private static boolean temMinuscula(String senha) {
+        for (int i = 0; i < senha.length(); i++) {
+            if (senha.charAt(i) >= 97 && senha.charAt(i) <= 122) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean temTamanhoadq(String senha) {
+        int tamanho = senha.length();
+        if (tamanho >= 6 && tamanho <= 32) {
+            return true;
+        }
+        return false;
+
+    }
+    private static boolean semEspacos(String senha) {
+        for (int i = 0; i < senha.length(); i++) {
+            char letra = senha.charAt(i);
+            if(letra==' '){
+                return true;
+            }
+        }
+        return false;
+    }
+    private static boolean semSimbol(String senha){
+        for (int i = 0; i < senha.length(); i++) {
+            if(!(senha.charAt(i)>=48 && senha.charAt(i)<=57) &&
+                    !(senha.charAt(i)>=65 && senha.charAt(i)<=90) &&
+                    !(senha.charAt(i)>=97 && senha.charAt(i)<=122)){
+                return false;
+            }
+        }
+        return true;
+    }
+    private static boolean semAcentos(String senha){
+//				for (int i = 0; i < senha.length(); i++) {
+//					if(senha.charAt(i)>=91 && senha.charAt(i)<=96){
+//						return false;
+//					}
+//				}
+        return true;
     }
 }

19df1735a6cf1fd2c480ac2c0f403987988e2c54
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2022-10-18 12:52:53 +0530

    Create BinarySearch.py

---
+++ Binary Search/BinarySearch.py
@@ -0,0 +1,13 @@
+def binarySearch(arr, l, r, x):
+    if r >= l:
+        mid = l + (r - l) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return binarySearch(arr, l, mid-1, x)
+        else:
+            return binarySearch(arr, mid + 1, r, x)
+ 
+    else:
+        return -1
+ 

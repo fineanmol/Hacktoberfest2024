4deff9c775a509f7d13ceb084b2eae95e3d37fca
PRIYANSHU TRIVEDI <33357862+PRIYANSHU-AMBITION@users.noreply.github.com>
2022-10-21 01:32:45 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2040,11 +2040,16 @@ contributors = [
     fullname:"Auro S.",
     username:"https://github.com/aurocodes",
   },
-  
-   {
+
+  {
     id:421,
     fullname:"Gautami SInha",
     username:"https://github.com/amnotcreative",
+  },
+  
+  {
+    id:422,
+    fullname:"Priyanshu Trivedi",
+    username:"https://github.com/PRIYANSHU-AMBITION",
   }
-
 ];

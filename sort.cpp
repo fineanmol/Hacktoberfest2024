4fb3b72d7989af7e73621032fc7723a9a4c1705f
Omkar Arora <57015056+omkarxx@users.noreply.github.com>
2022-10-21 02:51:09 +0530

    Create sort.cpp

---
+++ sort.cpp
@@ -0,0 +1,22 @@
+// C++ program to demonstrate default behaviour of
+// sort() in STL.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int arr[] = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	/*Here we take two parameters, the beginning of the
+	array and the length n upto which we want the array to
+	be sorted*/
+	sort(arr, arr + n);
+
+	cout << "\nArray after sorting using "
+			"default sort is : \n";
+	for (int i = 0; i < n; ++i)
+		cout << arr[i] << " ";
+
+	return 0;
+}

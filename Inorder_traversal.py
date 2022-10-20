3b226858b682f23415c422721a4fe5a7c41cf3b7
Devanshu Srivastava <42748021+devanshusrivastava25@users.noreply.github.com>
2022-10-20 17:04:33 +0530

    Create Inorder_traversal.py

---
+++ Inorder_traversal.py
@@ -0,0 +1,39 @@
+# Python3 program to for tree traversals
+ 
+# A class that represents an individual node in a
+# Binary Tree
+ 
+ 
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+ 
+ 
+# A function to do inorder tree traversal
+def printInorder(root):
+ 
+    if root:
+ 
+        # First recur on left child
+        printInorder(root.left)
+ 
+        # then print the data of node
+        print(root.val),
+ 
+        # now recur on right child
+        printInorder(root.right)
+ 
+ 
+# Driver code
+if __name__ == "__main__":
+  root = Node(1)
+  root.left = Node(2)
+  root.right = Node(3)
+  root.left.left = Node(4)
+  root.left.right = Node(5)
+ 
+  # Function call
+  print "\nInorder traversal of binary tree is"
+  printInorder(root)

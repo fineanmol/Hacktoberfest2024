4612a54c158dbef0a159747a9d8539b219441f5f
muskangupta00 <116362714+muskangupta00@users.noreply.github.com>
2022-10-21 19:01:12 +0530

    addoftwo.java
    This contains the addition of sum in Java language.

---
+++ addoftwo.java
@@ -0,0 +1,9 @@
+public class SumOfNumbers1  
+{  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+}  

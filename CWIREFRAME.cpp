d855ff562d4fe286df239f2a553c1e342a5a32f3
Bhavik Prajapati <109121443+Bhavik-prajapati@users.noreply.github.com>
2022-10-21 16:51:09 +0530

    Create CWIREFRAME.cpp

---
+++ CWIREFRAME.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+   int t;
+   cin>>t;
+   while (t--)
+   {
+      long long int n,m,x;
+      cin>>n>>m>>x;
+      cout<<(((2*n)+(2*m))*x)<<endl;
+
+   }
+   
+
+}

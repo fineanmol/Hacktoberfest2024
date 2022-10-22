2e25dd2967015cd08e9a41650b0f73784b139c59
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-10-22 17:13:53 -0300

    add find the duplicate number problema

---
+++ Program's_Contributed_By_Contributors/Python_Programs/findDuplicate.py
@@ -0,0 +1,46 @@
+"""
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+ 
+
+Constraints:
+
+1 <= n <= 105
+nums.length == n + 1
+1 <= nums[i] <= n
+All the integers in nums appear only once except for precisely one integer which appears two or more times.
+"""
+
+def findDuplicate(nums: list[int]) -> int:
+    tortoise = nums[0]
+    hare = nums[0]
+
+    while True:
+        tortoise = nums[tortoise]
+        hare = nums[nums[hare]]
+        if tortoise == hare:
+            break
+    ptr1 = nums[0]
+    ptr2 = tortoise
+    while ptr1 != ptr2:
+        ptr1 = nums[ptr1]
+        ptr2 = nums[ptr2]
+    return ptr1
+
+if __name__ == "__main__":
+    nums = [3,1,3,4,2]
+    print(findDuplicate(nums))

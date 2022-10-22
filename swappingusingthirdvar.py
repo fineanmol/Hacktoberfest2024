a14ce81c2c8b94e04759b03405e50117b50e95a7
muskangupta00 <116362714+muskangupta00@users.noreply.github.com>
2022-10-22 21:58:30 +0530

    swappingusingthirdvar.py

---
+++ swappingusingthirdvar.py
@@ -0,0 +1,8 @@
+a = int(input("please give first number a: "))
+b = int(input("please give second number b: "))
+tempvar=a
+a=b
+b=tempvar
+print("After swapping")
+print("value of a is : ", a);
+print("value of b is : ", b); 

41f969a853a24811b46e3387e4014af1341386b1
FelipeJer <felipe.jeronimo.silva@ccc.ufcg.edu.br>
2022-10-22 00:48:35 -0300

    added binary search

---
+++ Program's_Contributed_By_Contributors/Python_Programs/binary_search.py
@@ -0,0 +1,17 @@
+def binary_search(arr, low, high, x):
+ 
+    if high >= low:
+ 
+        mid = (high + low) // 2
+ 
+        if arr[mid] == x:
+            return mid
+ 
+        elif arr[mid] > x:
+            return binary_search(arr, low, mid - 1, x)
+ 
+        else:
+            return binary_search(arr, mid + 1, high, x)
+ 
+    else:
+        return -1

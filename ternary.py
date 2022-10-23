0d48fb3543cc46826eb869b68fe87b1102755d4d
sanjit1314 <99742716+sanjit1314@users.noreply.github.com>
2022-10-23 20:49:56 +0530

    Create ternary.py

---
+++ ternary.py
@@ -0,0 +1,11 @@
+# Python program to find the
+# maximum of two numbers
+	
+# Driver code
+a = 2
+b = 4
+
+# Use of ternary operator
+print(a if a >= b else b)
+
+# This code is contributed by AnkThon

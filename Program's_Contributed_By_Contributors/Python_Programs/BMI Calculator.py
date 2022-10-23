faf200e3b98b77edab6069de5678ab6d5e39a3b2
Amit Singh <amitjordanthegr8@gmail.com>
2022-10-23 20:53:25 +0530

    Update BMI Calculator.py

--- Program's_Contributed_By_Contributors/Python_Programs/BMI Calculator.py
+++ Program's_Contributed_By_Contributors/Python_Programs/BMI Calculator.py
@@ -1,19 +1,18 @@
-Height = float(input("Enter your height in centimeters: "))
+Height = float(input("Enter your height in centimeters: "))/100
 Weight = float(input("Enter your Weoght in Kg: "))
-Height = Height/100
 BMI = Weight/(Height*Height)
 print(f"Your Body Mass Index is: {BMI}")
 
 if(BMI>0):
     if(BMI<=16):
-        print("ypu are severaly underweigth")
+        print("You are severaly underweigth")
     elif(BMI<=18.5):
-        print("you are underweight")
+        print("You are underweight")
     elif (BMI <= 25):
-        print("ypu are Healthy")
+        print("You are Healthy")
     elif (BMI <= 30):
-        print("you are overwight")
+        print("You are overwight")
     else:17
         print("You are severaly overweight")
 else:
-    print("enter valid details")
+    print("Enter valid details")

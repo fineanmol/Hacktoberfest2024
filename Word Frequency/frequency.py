bc04b7bade100d69e0a297657fe69ea2b2c71a28
Yashwanth chandolu <yashwanthchandolu11@gmail.com>
2022-10-24 00:55:07 +0530

    Word frquency in a file

---
+++ Word Frequency/frequency.py
@@ -0,0 +1,6 @@
+from collections import Counter
+def word_count(fname):
+        with open(fname) as f:
+                return Counter(f.read().split())
+
+print("Number of words in the file :",word_count("test.txt"))

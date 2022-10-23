ccee252c60695da6971747b59f6902968d767cf1
Bishal7890 <82110413+Bishal7890@users.noreply.github.com>
2022-10-23 09:48:09 +0530

    Create Number swapping.C

---
+++ Number swapping.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+  double first, second, temp;
+  printf("Enter first number: ");
+  scanf("%lf", &first);
+  printf("Enter second number: ");
+  scanf("%lf", &second);
+
+  // value of first is assigned to temp
+  temp = first;
+
+  // value of second is assigned to first
+  first = second;
+
+  // value of temp (initial value of first) is assigned to second
+  second = temp;
+
+  // %.2lf displays number up to 2 decimal points
+  printf("\nAfter swapping, first number = %.2lf\n", first);
+  printf("After swapping, second number = %.2lf", second);
+  return 0;
+}

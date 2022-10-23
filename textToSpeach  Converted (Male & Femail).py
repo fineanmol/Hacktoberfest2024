b2ff03f6f5f49c218fc0e136e0d46b0a7eaa6c4b
Aditi <116505937+Foxtrot31@users.noreply.github.com>
2022-10-23 21:30:56 +0530

    textToSpeach Converted (Male & Femail)
    Python code to convert text to speech and availability to choose between male and female voice.

---
+++ textToSpeach  Converted (Male & Femail).py
@@ -0,0 +1,26 @@
+import pyttsx3
+
+
+assistantVoice = int(input('''Select you assistants Voice 
+  [1] Male 
+  [2] Female : '''))
+
+
+if assistantVoice == 1:
+    engine = pyttsx3.init("sapi5")
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice',voices[0].id)
+elif assistantVoice == 2:
+    engine = pyttsx3.init("sapi5")
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice',voices[1].id)
+else:
+    print("Invalid Input")
+
+
+x = 'Hello Everyone I am Contributing in Hacktoberfest'
+
+
+engine.say(x)
+
+engine.runAndWait()

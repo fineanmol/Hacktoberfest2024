cd43387be9b2589180e622284b5192815379b67c
Haiku32 <114604444+Haiku32@users.noreply.github.com>
2022-10-23 22:54:14 -0700

    creating heap sort in python
    heap sort but in python!

---
+++ heap sort.py
@@ -0,0 +1,47 @@
+def heapify(arr, n, i):
+    largest = i  
+    l = 2 * i + 1  
+    r = 2 * i + 2  
+ 
+ 
+    if l < n and arr[i] < arr[l]:
+        largest = l
+ 
+
+ 
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+ 
+ 
+    if largest != i:
+        (arr[i], arr[largest]) = (arr[largest], arr[i])  # swap
+ 
+ 
+        heapify(arr, n, largest)
+ 
+ 
+
+ 
+def heapSort(arr):
+    n = len(arr)
+ 
+
+ 
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+ 
+
+ 
+    for i in range(n - 1, 0, -1):
+        (arr[i], arr[0]) = (arr[0], arr[i])  # swap
+        heapify(arr, i, 0)
+ 
+ 
+
+ 
+arr = [12, 11, 13, 5, 6, 7, ]
+heapSort(arr)
+n = len(arr)
+print('Sorted array is')
+for i in range(n):
+    print(arr[i])

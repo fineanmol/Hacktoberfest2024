88ffc0b49d9a3f43a6aee4477b0105cf9e99c0eb
Sneha2319 <snehamurugesh2019@gmail.com>
2022-10-24 13:42:29 +0530

    Added java programs

---
+++ Program's_Contributed_By_Contributors/Java_Programs/Data_structure/Arraylist.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.ArrayList;
+class Arraylist{
+ public static void main(String[] args){
+ArrayList<Integer> al = new ArrayList<>();
+al.add(1);
+al.add(2);
+al.add(3);
+System.out.println(al);
+}}

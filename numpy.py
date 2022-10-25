1ef33e6c0fe9562d9908433bdc30112855370ad4
BRJ304 <116664404+BRJ304@users.noreply.github.com>
2022-10-25 21:44:09 +0530

    Create numpy.py

---
+++ numpy.py
@@ -0,0 +1,12 @@
+# Python program explaining
+# bitwise_and() function
+ 
+import numpy as geek
+in_num1 = 10
+in_num2 = 11
+ 
+print ("Input  number1 : ", in_num1)
+print ("Input  number2 : ", in_num2) 
+   
+out_num = geek.bitwise_and(in_num1, in_num2) 
+print ("bitwise_and of 10 and 11 : ", out_num) 

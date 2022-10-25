b13e9d8b5ccdd71345ddd28c93f69fcdb038d33c
Anas Raza <anasraza6096@gmail.com>
2022-10-26 00:28:12 +0500

    updated contributers list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2045,6 +2045,10 @@ contributors = [
     id:421,
     fullname:"Gautami SInha",
     username:"https://github.com/amnotcreative",
+  },
+  {
+    id:422,
+    fullname:"M Anas Raza",
+    username:"https://github.com/anasgee",
   }
-
 ];

f0d84cfe01f69598e3f5b20ad6d9710a72f6965e
SOS TECH Inc <88025671+Divjot121@users.noreply.github.com>
2022-10-25 16:42:22 +0530

    ADDED

---
+++ SOL4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SOL4 {
+    public static void main(String[] args) {
+        int num1;
+        Scanner scanner = new Scanner(System.in);
+        num1= scanner.nextInt();
+        int cube=num1*num1*num1;
+        System.out.println(cube);
+
+    }
+}

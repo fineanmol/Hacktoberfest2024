8b3eb3fe1e9ed391e00ea95c2ed78dbf81ca7ea6
pizzalord22 <markjoling3@gmail.com>
2022-10-26 19:09:23 +0200

    added comment to function

--- Program's_Contributed_By_Contributors/Golang Programs/countdown_timer.go
+++ Program's_Contributed_By_Contributors/Golang Programs/countdown_timer.go
@@ -1,62 +1,63 @@
 package main
 
 import (
-	"flag"
-	"os"
-	"fmt"
-	"time"
+    "flag"
+    "fmt"
+    "os"
+    "time"
 )
 
 type countdown struct {
-	t int
-	d int
-	h int
-	m int
-	s int
+    t int
+    d int
+    h int
+    m int
+    s int
 }
 
+// get the remaining time
 func getTimeRemaining(t time.Time) countdown {
-	currentTime := time.Now()
-	difference := t.Sub(currentTime)
+    currentTime := time.Now()
+    difference := t.Sub(currentTime)
 
-	total := int(difference.Seconds())
-	days := int(total / (60 * 60 * 24))
-	hours := int(total / (60 * 60) % 24)
-	minutes := int(total/60) % 60
-	seconds := int(total % 60)
+    total := int(difference.Seconds())
+    days := int(total / (60 * 60 * 24))
+    hours := int(total / (60 * 60) % 24)
+    minutes := int(total/60) % 60
+    seconds := int(total % 60)
 
-	return countdown{
-		t: total,
-		d: days,
-		h: hours,
-		m: minutes,
-		s: seconds,
-	}
+    return countdown{
+        t: total,
+        d: days,
+        h: hours,
+        m: minutes,
+        s: seconds,
+    }
 }
 
 func main() {
-	deadline := flag.String("deadline", "", "The deadline for the countdown timer in RFC3339 format (e.g. 2019-12-25T15:00:00+01:00)")
-	flag.Parse()
+    deadline := flag.String("deadline", "", "The deadline for the countdown timer in RFC3339 format (e.g. 2019-12-25T15:00:00+01:00)")
+    flag.Parse()
 
-	if *deadline == "" {
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
+    if *deadline == "" {
+        flag.PrintDefaults()
+        os.Exit(1)
+    }
 
-	v, err := time.Parse(time.RFC3339, *deadline)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+    v, err := time.Parse(time.RFC3339, *deadline)
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
 
-	for range time.Tick(1 * time.Second) {
-		timeRemaining := getTimeRemaining(v)
+    for range time.Tick(1 * time.Second) {
+        timeRemaining := getTimeRemaining(v)
 
-		if timeRemaining.t <= 0 {
-			fmt.Println("Countdown reached!")
-			break
-		}
+        if timeRemaining.t <= 0 {
+            fmt.Println("Countdown reached!")
+            break
+        }
 
-		fmt.Printf("Days: %d Hours: %d Minutes: %d Seconds: %d\n", timeRemaining.d, timeRemaining.h, timeRemaining.m, timeRemaining.s)
-	}
+        fmt.Printf("Days: %d Hours: %d Minutes: %d Seconds: %d\n", timeRemaining.d, timeRemaining.h, timeRemaining.m, timeRemaining.s)
+    }
 }

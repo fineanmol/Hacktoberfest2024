f25ddf5943f47e4942a02d5aab5819e0eae0ad9c
pizzalord22 <markjoling3@gmail.com>
2022-10-26 19:06:21 +0200

    the add function now returns the value created by adding to numbers

--- Program's_Contributed_By_Contributors/Golang Programs/addition.go
+++ Program's_Contributed_By_Contributors/Golang Programs/addition.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 // Function accepting arguments
-func add(x int, y int) {
+func add(x int, y int) int {
 	total := 0
 	total = x + y
 	fmt.Println(total)
+	return total
 }
 
 func main() {

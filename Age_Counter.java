94fe2c29d65b0e8aa6ee8ae6abe122656922cca1
Rahul Kumar Das <98277636+rahuldas-404@users.noreply.github.com>
2022-10-26 15:22:59 +0530

    Create Age_Counter.java
    age count program using java

---
+++ Age_Counter.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner bday=new Scanner(System.in);
+
+        //Get Inputs
+        System.out.println("Enter Your Birth Year");
+        int year=bday.nextInt();
+
+        System.out.println("Enter Your Birth Month");
+        int month=bday.nextInt();
+
+        System.out.println("Enter Your Birth Date");
+        int day=bday.nextInt();
+
+
+
+        LocalDate pres = LocalDate.now();
+        LocalDate bdate = LocalDate.of(year,month,day);
+
+        int age = Period.between(bdate , pres).getYears();
+
+        System.out.println("Current Date");
+        System.out.println(pres);
+        System.out.println("Birth Date");
+        System.out.println(bdate);
+        System.out.println("Your Age");
+        System.out.println(age);
+    }
+
+)

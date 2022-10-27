418ee127aeb388d319b67fa5b2bb278b34f42505
DexterHimZ <himangshu.shekhar03@gmail.com>
2022-10-27 11:12:43 +0530

    Contibute to hacktoberfest2022

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2045,6 +2045,11 @@ contributors = [
     id:421,
     fullname:"Gautami SInha",
     username:"https://github.com/amnotcreative",
+  },
+  {
+    id:422,
+    fullname:"Himangshu Shekhar",
+    username:"https://github.com/DexterHimZ",
   }
 
 ];

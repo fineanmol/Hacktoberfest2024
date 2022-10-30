a5bc40c97e72188b8ca645a995fdbbf4276ae44a
Syeda Nowshin Ibnat <47534922+Nowshin1077@users.noreply.github.com>
2022-10-30 18:33:06 +0600

    Create Birthday_wish.py

---
+++ Birthday_wish.py
@@ -0,0 +1,34 @@
+# Happy Birthday in Python Code 
+import time
+from random import randint
+
+
+for i in range(1,85):
+    print('')
+
+space = ''
+
+
+for i in range(1,1000):
+    count = randint(1, 100)
+    while(count > 0):
+        space += ' '
+        count -= 1
+
+    if(i%10==0):
+        print(space + 'Happy Birthday Python!💖')
+    elif(i%9 == 0):
+        print(space + "🎂")
+    elif(i%5==0):
+        print(space +"💛")
+    elif(i%8==0):
+        print(space + "🎉")
+    elif(i%7==0):
+        print(space + "🐍")
+    elif(i%6==0):
+        print(space + "🔸")
+    else:
+        print(space + "🔸")
+
+    space = ''
+    time.sleep(0.2)

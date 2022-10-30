d53b837c1ea79f9b4947cb90fc60d9921d1f5de9
AbhisekhJha <101344810+AbhisekhJha@users.noreply.github.com>
2022-10-30 14:00:16 +0545

    Create TwoSum.java
    Name:Abhisekh Kumar Jha

---
+++ TwoSum.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Map;
+
+class TwoSum {
+    
+    // Time complexity: O(n)
+    private static int[] findTwoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] { numMap.get(complement), i };
+            } else {
+                numMap.put(nums[i], i);
+            }
+        }
+        return new int[] {};
+    }
+}

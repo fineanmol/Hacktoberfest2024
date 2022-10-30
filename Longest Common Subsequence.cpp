8b343b4a67926bb54447d4852712ae03a2eb3639
Hk <71863477+Himanshukwal@users.noreply.github.com>
2022-10-30 12:57:50 +0530

    Create Longest Common Subsequence.cpp

---
+++ Longest Common Subsequence.cpp
@@ -0,0 +1,34 @@
+/* A Naive recursive implementation of LCS problem */
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+/* Returns length of LCS for X[0..m-1], Y[0..n-1] */
+int lcs( char *X, char *Y, int m, int n )
+{
+	if (m == 0 || n == 0)
+		return 0;
+	if (X[m-1] == Y[n-1])
+		return 1 + lcs(X, Y, m-1, n-1);
+	else
+		return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));
+}
+
+
+
+/* Driver code */
+int main()
+{
+	char X[] = "AGGTAB";
+	char Y[] = "GXTXAYB";
+	
+	int m = strlen(X);
+	int n = strlen(Y);
+	
+	cout<<"Length of LCS is "<< lcs( X, Y, m, n ) ;
+	
+	return 0;
+}
+
+// This code is contributed by rathbhupendra

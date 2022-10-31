efe8b3ef7572ae9d821e2974f5a5352d8874ad99
IT18231724 <116820814+vidushanit@users.noreply.github.com>
2022-10-31 22:44:06 +0530

    Quicksort

--- quick-sort.java
+++ quick-sort.java
@@ -3,7 +3,6 @@
 
 class GFG {
 
-	// A utility function to swap two elements
 	static void swap(int[] arr, int i, int j)
 	{
 		int temp = arr[i];
@@ -11,30 +10,18 @@ class GFG {
 		arr[j] = temp;
 	}
 
-	/* This function takes last element as pivot, places
-	the pivot element at its correct position in sorted
-	array, and places all smaller (smaller than pivot)
-	to left of pivot and all greater elements to right
-	of pivot */
 	static int partition(int[] arr, int low, int high)
 	{
 
 		// pivot
 		int pivot = arr[high];
 
-		// Index of smaller element and
-		// indicates the right position
-		// of pivot found so far
 		int i = (low - 1);
 
 		for (int j = low; j <= high - 1; j++) {
 
-			// If current element is smaller
-			// than the pivot
 			if (arr[j] < pivot) {
 
-				// Increment index of
-				// smaller element
 				i++;
 				swap(arr, i, j);
 			}
@@ -43,21 +30,12 @@ class GFG {
 		return (i + 1);
 	}
 
-	/* The main function that implements QuickSort
-			arr[] --> Array to be sorted,
-			low --> Starting index,
-			high --> Ending index
-	*/
 	static void quickSort(int[] arr, int low, int high)
 	{
 		if (low < high) {
 
-			// pi is partitioning index, arr[p]
-			// is now at right place
 			int pi = partition(arr, low, high);
 
-			// Separately sort elements before
-			// partition and after partition
 			quickSort(arr, low, pi - 1);
 			quickSort(arr, pi + 1, high);
 		}

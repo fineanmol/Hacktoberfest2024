0529be04cc341af58886ce7511c646e957fc1081
Tharindu Yehan <85949077+TYehan@users.noreply.github.com>
2022-11-07 19:48:45 +0530

    C++ Menu Driven Program
    A little c++ program which imitates a bank.

---
+++ Program's_Contributed_By_Contributors/Bank Account/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    string name, gender, account_no, NIC;
+    float balance;
+    int phone_no, age;z     q   4
+    const string country_code = "(+94)";
+    cin >> phone_no;
+    cout << contr << bla;
+    cout << fixed << setprcision(2) << balance ;
+}
+

d7757211116c13b1cade29e9107db375b2804e76
Jayesh Gaba <111695826+JayGaba@users.noreply.github.com>
2023-10-01 01:11:45 +0530

    Create portscanner.py
    Added a basic Port Scanner

---
+++ Program's_Contributed_By_Contributors/Python_Programs/portscanner.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+import sys
+import socket
+from datetime import datetime
+
+#Define our target
+if len(sys.argv) == 2:
+	target = socket.gethostbyname(sys.argv[1]) 
+else:
+	print("Invalid amount of arguments.")
+	print("Syntax: python3 scanner.py")
+
+print("-" * 50)
+print("Scanning target "+target)
+print("Time started: "+str(datetime.now()))
+print("-" * 50)
+
+try:
+	for port in range(50,85):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1)
+		result = s.connect_ex((target,port)) 
+		if result == 0:
+			print("Port {} is open".format(port))
+		s.close()
+
+except KeyboardInterrupt:
+	print("\nExiting program.")
+	sys.exit()
+	
+except socket.gaierror:
+	print("Hostname could not be resolved.")
+	sys.exit()
+
+except socket.error:
+	print("Could not connect to server.")
+	sys.exit()

d4e6b28a3134cf79651efa4e19a91389d0ac474e
VIKRAM KUMAR BHARTI <457ayan123@gmail.com>
2023-09-30 20:54:07 +0530

    this is my contribution

---
+++ Program's_Contributed_By_Contributors/LogIn/script.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form"),
+        nextBtn = form.querySelector(".nextBtn"),
+        backBtn = form.querySelector(".backBtn"),
+        allInput = form.querySelectorAll(".first input");
+
+
+nextBtn.addEventListener("click", ()=> {
+    allInput.forEach(input => {
+        if(input.value != ""){
+            form.classList.add('secActive');
+        }else{
+            form.classList.remove('secActive');
+        }
+    })
+})
+
+backBtn.addEventListener("click", () => form.classList.remove('secActive'));

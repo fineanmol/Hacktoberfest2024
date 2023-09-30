d09169e1e89abecf8ef0e6165db4eb30318bb11d
Berat Osman Demiralay <bdemiralay7@gmail.com>
2023-09-30 22:39:54 +0300

    "Upgrade weight conversion feature in Tkinter app
    - Added labels for result fields for better user clarity.
    - Used consistent grid layout for improved alignment.
    - Organized code within a class for better readability."

--- Program's_Contributed_By_Contributors/Python_Programs/weight_convertor_gui.py
+++ Program's_Contributed_By_Contributors/Python_Programs/weight_convertor_gui.py
@@ -1,57 +1,49 @@
-from tkinter import *
+import tkinter as tk
 
-# Creating a GUI Window
-window = Tk()
+class WeightConverterApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Weight Converter")
 
+        self.create_widgets()
 
-def from_kg():
-    gram = float(e2_value.get())*1000
-    pound = float(e2_value.get())*2.20462
-    ounce = float(e2_value.get())*35.274
-    stone = float(e2_value.get())*0.157473
-    uston = float(e2_value.get())*0.00110231
+    def create_widgets(self):
+        # Input Label and Entry
+        self.input_label = tk.Label(self.root, text="Input the weight in KG")
+        self.input_label.grid(row=0, column=0)
 
-    t1.delete("1.0", END)
-    t1.insert(END, gram)
-    t2.delete("1.0", END)
-    t2.insert(END, pound)
-    t3.delete("1.0", END)
-    t3.insert(END, ounce)
-    t4.delete("1.0", END)
-    t4.insert(END, stone)
-    t5.delete("1.0", END)
-    t5.insert(END, uston)
+        self.e2_value = tk.StringVar()
+        self.input_entry = tk.Entry(self.root, textvariable=self.e2_value)
+        self.input_entry.grid(row=0, column=1)
 
+        # Conversion Labels
+        conversion_units = ["Gram", "Pound", "Ounce", "Stone", "US Ton"]
+        for i, unit in enumerate(conversion_units):
+            tk.Label(self.root, text=unit).grid(row=1, column=i)
 
-e1 = Label(window, text="Input the weight in KG")
-e2_value = StringVar()
-e2 = Entry(window, textvariable=e2_value)
-e3 = Label(window, text="Gram")
-e4 = Label(window, text="Pound")
-e5 = Label(window, text="Ounce")
-e6 = Label(window, text="Stone")
-e7 = Label(window, text="US Ton")
+        # Result Text Boxes
+        self.result_texts = []
+        for i in range(len(conversion_units)):
+            t = tk.Text(self.root, height=5, width=30)
+            t.grid(row=2, column=i)
+            self.result_texts.append(t)
 
-t1 = Text(window, height=5, width=30)
-t2 = Text(window, height=5, width=30)
-t3 = Text(window, height=5, width=30)
-t4 = Text(window, height=5, width=30)
-t5 = Text(window, height=5, width=30)
+        # Convert Button
+        self.convert_button = tk.Button(self.root, text="Convert", command=self.from_kg)
+        self.convert_button.grid(row=0, column=2)
 
-b1 = Button(window, text="Convert", command=from_kg)
+    def from_kg(self):
+        kg_value = float(self.e2_value.get())
+        conversions = [kg_value * factor for factor in [1000, 2.20462, 35.274, 0.157473, 0.00110231]]
 
-e1.grid(row=0, column=0)
-e2.grid(row=0, column=1)
-e3.grid(row=1, column=0)
-e4.grid(row=1, column=1)
-e5.grid(row=1, column=2)
-e6.grid(row=1, column=3)
-e7.grid(row=1, column=4)
-t1.grid(row=2, column=0)
-t2.grid(row=2, column=1)
-t3.grid(row=2, column=2)
-t4.grid(row=2, column=3)
-t5.grid(row=2, column=4)
-b1.grid(row=0, column=2)
+        for i, t in enumerate(self.result_texts):
+            t.delete("1.0", tk.END)
+            t.insert(tk.END, conversions[i])
 
-window.mainloop()
+def main():
+    root = tk.Tk()
+    app = WeightConverterApp(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()

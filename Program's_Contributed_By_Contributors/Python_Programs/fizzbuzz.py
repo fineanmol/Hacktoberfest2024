353be318478738fb28122ec596e23483b6a398b9
jmoua123 <nraugnaagmouaa@gmail.com>
2023-09-30 15:19:29 -0700

    fizzbuzz script

---
+++ Program's_Contributed_By_Contributors/Python_Programs/fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# Example usage:
+fizzbuzz(20)
--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2050,6 +2050,12 @@ contributors = [
     id:422,
     fullname:"Evgeny Skorlov",
     username:"https://github.com/ru-asdx",
+  },
+
+  {
+    id:423,
+    fullname: "Jonah Moua",
+    username: "https://github.com/JonahMoua"
   }
 
 

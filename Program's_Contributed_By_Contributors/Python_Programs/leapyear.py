b05ba5fc6d9d3e55e4d4c31dc5d86a529b074c33
farzeencode <145785539+farzeencode@users.noreply.github.com>
2023-10-01 16:57:27 +0530

    Create leapyear.py

---
+++ Program's_Contributed_By_Contributors/Python_Programs/leapyear.py
@@ -0,0 +1,8 @@
+# Input: Year
+year = int(input("Enter a year: "))
+
+# Check if it's a leap year
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year.")
+else:
+    print(year, "is not a leap year.")

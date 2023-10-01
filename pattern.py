307df3efefeafce9bc38ad331e909bab49798443
Subhajit Chowdhury <61339528+Subhajit-Chowdhury@users.noreply.github.com>
2023-10-01 19:37:03 +0530

    Create pattern.py

---
+++ pattern.py
@@ -0,0 +1,29 @@
+def triangle(n):
+	
+	# number of spaces
+	k = n - 1
+
+	# outer loop to handle number of rows
+	for i in range(0, n):
+	
+		# inner loop to handle number spaces
+		# values changing acc. to requirement
+		for j in range(0, k):
+			print(end=" ")
+	
+		# decrementing k after each loop
+		k = k - 1
+	
+		# inner loop to handle number of columns
+		# values changing acc. to outer loop
+		for j in range(0, i+1):
+		
+			# printing stars
+			print("* ", end="")
+	
+		# ending line after each row
+		print("\r")
+
+# Driver Code
+n = 5
+triangle(n)

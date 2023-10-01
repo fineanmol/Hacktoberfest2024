26923b46a6350b6c63f0b9909ef7099fede9eea3
Sushant Kumar <mail.sushantthakur@gmail.com>
2023-10-01 15:14:49 +0530

    first commit

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2050,7 +2050,13 @@ contributors = [
     id:422,
     fullname:"Evgeny Skorlov",
     username:"https://github.com/ru-asdx",
+  },
+  {
+    id: 423,
+    fullname: "Sushant Kumar",
+    username: "https://github.com/Sushant0753",
   }
+  
 
 
 

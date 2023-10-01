cb3ab9521ab3adf09aae179f4f5f43096cb9c60e
Pamood <pamoodjayaratne2001@gmail.com>
2023-10-02 01:23:21 +0530

    added my name to the contibution list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2054,4 +2054,9 @@ contributors = [
     fullname: "Mohit Yadav",
     username: "https://github.com/ymohit1603",
   },
+  {
+    id: 423,
+    fullname: "Pamood Jayaratne",
+    username: "https://github.com/pamood",
+  },
 ];

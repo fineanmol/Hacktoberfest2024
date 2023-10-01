1c64b064669c937679c27e5d2661f7c5300985ab
USMAN-FAIZYAB-KHAN <110056579+USMAN-FAIZYAB-KHAN@users.noreply.github.com>
2023-10-01 13:09:20 +0500

    Added a calculator project

---
+++ Program's_Contributed_By_Contributors/JavaScript_Programs/calculator/index.ts
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+import inquirer from "inquirer"
+// import gradient from "gradient-string";
+// import chalkAnimation from "chalk-animation";
+// import figlet from "figlet";
+import { createSpinner } from "nanospinner";
+console.log("helo")

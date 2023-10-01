ed6da872db27ad2583be45755addc06249ed8280
electrify-7 <145887059+electrify-7@users.noreply.github.com>
2023-10-02 00:00:16 +0530

    Update buyandsell.cpp
    Added a little description and Improved formatting for better readability.

--- Program's_Contributed_By_Contributors/C++ Programs/Array/buyandsell.cpp
+++ Program's_Contributed_By_Contributors/C++ Programs/Array/buyandsell.cpp
@@ -3,14 +3,18 @@ using namespace std;
 
 int main()
 {
-	vector<int> price = {7,1,5,3,6,4};
-	int mn =INT_MAX;
+	vector<int> price={7,1,5,3,6,4};
+	
+	int mn = INT_MAX;
 	int profit = 0;
-	for(int i=0;i<price.size();i++)
-	{
-		mn = min(mn, price[i]);
-		profit = max(profit, price[i]-mn);
+	
+	for(int i=0; i<price.size() ;i++)
+	{      
+        //Comparing the prices and calculating profit accordingly.
+	  mn = min( mn , price[i]);
+	  profit = max( profit , price[i]- mn); 
 	}
+	
 	cout<<profit<<endl;
 
 	return 0;

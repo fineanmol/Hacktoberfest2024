7a82a02dca6e8a61ed41cd1228b4ea56f0235342
Devashish Roy Chowdhury <100063820+devashishroychowdhury@users.noreply.github.com>
2023-10-01 11:35:20 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2051,7 +2051,11 @@ contributors = [
     fullname:"Evgeny Skorlov",
     username:"https://github.com/ru-asdx",
   }
-
+   {
+    id: 423,
+    fullname: "Devashish Roy Chowdhury",
+    username: "https://github.com/devashishroychowdhury",
+  }
 
 
 

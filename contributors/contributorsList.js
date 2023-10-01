869686300070c65d7366ca582dfccdc6c17d65e7
rajmishra-47 <81914386+rajmishra-47@users.noreply.github.com>
2023-10-01 11:18:09 +0530

    First Commit

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2050,9 +2050,12 @@ contributors = [
     id:422,
     fullname:"Evgeny Skorlov",
     username:"https://github.com/ru-asdx",
+  },
+  {
+    id:423,
+    fullname:"Rajnandan Mishra",
+    username:"https://github.com/rajmishra-47",
   }
-
-
 
 
 ];

8bb29e2899153613912d45516bd481efd4aa6bb0
USMAN-FAIZYAB-KHAN <110056579+USMAN-FAIZYAB-KHAN@users.noreply.github.com>
2023-10-01 13:11:35 +0500

    Delete Program's_Contributed_By_Contributors/JavaScript_Programs/calculator directory
    deleted my project as it was not correct

--- Program's_Contributed_By_Contributors/JavaScript_Programs/calculator/index.ts
+++
@@ -1,7 +0,0 @@
-import chalk from "chalk";
-import inquirer from "inquirer"
-// import gradient from "gradient-string";
-// import chalkAnimation from "chalk-animation";
-// import figlet from "figlet";
-import { createSpinner } from "nanospinner";
-console.log("helo")

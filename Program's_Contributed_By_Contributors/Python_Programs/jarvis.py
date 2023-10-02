73a8781c243a53a53efc009936c7196d563013e5
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2023-10-02 22:08:47 +0530

    Update jarvis.py

--- Program's_Contributed_By_Contributors/Python_Programs/jarvis.py
+++ Program's_Contributed_By_Contributors/Python_Programs/jarvis.py
@@ -1,346 +1,212 @@
-#Structure is as follows
+# IMPORTING BUILT IN LIBRARIES
 
-# importing, defining, audio, Microphone, EMAIL, other commands, dialogues
-
-
-
-#Importing
-from urllib.parse import quote_from_bytes
-import pyttsx3 #pip install pyttsx3
-import speech_recognition as sr #pip install speechRecognition
-import datetime
-import wikipedia #pip install wikipedia
-import webbrowser
-import pywhatkit as kit
 import os
-import smtplib
-import random
-import pyautogui
-import psutil
-import pyjokes
-# import playsound # to play an audio file
-# from gtts import gTTS # google text to speech
-# import ssl
-# import certifi
-# from wikipedia.wikipedia import search
-# import pyPDF2
+import time
+import subprocess
+import json
 
+# IMPORTING LIBRARIES INSTALLED FROM ANACONDA PROMT
+
+import wolframalpha
+import requests
+import webbrowser
+import wikipedia
+import datetime
+import instaloader
+
+# LIBRARIES FOR NATURAL LANGUAGE PROCESSING & SPEACH TO TEXT COVERSION
+
+import speech_recognition as sr
+import pyttsx3
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-# print(voices[1].id)
-engine.setProperty('voice', voices[0].id)
+engine.setProperty("voice",'voices[1].id')
 
-#For opening the results in Chrome
-url1 = 'youtube.com'
-url2 = 'google.com'
-url3 = 'amazon.in'
-url4 = 'code-projects.org'
-url5 = 'youtube.com/channel/UCeVMnSShP_Iviwkknt83cww'
-url6 = 'stackoverflow.com'
-url7 = 'mcs.edunexttechnologies.com/StudentDashboardApp/StudentHome'
+# Defining a function for speaking a sentence
 
-chrome_path = r'C:\Program Files\Google\Chrome\Application\chrome.exe'
-webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
-
-# For Random Greeting
-
-greetings = ['Hello! I am Jarvis Sir. How may I help you?', 'Sup!', 'Hey', 'Bonjour', 'Everything great..sir?', 'Hope you are great']
-
-value = random.choice(greetings)
-
-#For random songs
-songs = ['on my way' , 'kandhon se milte hain', 'Raabta', 'Shiv Tandav Stotram']
-gaana = random.choice(songs)
-
-gaane = ['ben 10 theme song guitar' , 'kfir pirates of the caribbean', 'Jai Jai Shivshankar Song']
-othersongs = random.choice(gaane)
-
-
-
-def speak(audio):
-    engine.say(audio)
+def speak(text):
+    #speak = wincl.Dispatch("SAPI.SpVoice",pythoncom.CoInitialize())
+    #speak.Speak(text)
+    engine.say(text)
     engine.runAndWait()
 
-def time():
-    Time = datetime.datetime.now().strftime("%I:%M:%S")
-    speak("the current time is")
-    speak(Time)
-
-def date():
-    year = int(datetime.datetime.now().year)
-    month = int(datetime.datetime.now().month)
-    date = int(datetime.datetime.now().day)
-    speak("the current date is")
-    speak(date)
-    speak(month)
-    speak(year)
+# Function to greet a Human
 
 def wishMe():
-    
-    # hour = int(datetime.datetime.now().hour)
-    # if hour>=0 and hour<12:
-    #     speak("Good Morning!")
+    hour = datetime.datetime.now().hour
+    if hour >= 0 and hour < 12:
+        speak("hello, good morning Mr. ABC, i wish your day is as pleasant as you are")
+        print("hello, good morning Mr. ABC ")
+    elif hour >= 12 and hour <=18:
+        speak("hello, Good afternoon Mr. ABC, Sunny days are always bright")
+        print("hello, Good afternoon Mr. ABC")
+    else:
+        speak("hello, Good evening Mr. ABC, its time to rest and go to sleep soon after you are done with me")
+        print("hello, Good evening Mr. ABC")
 
-    # elif hour>=12 and hour<18:
-    #     speak("Good Afternoon!")   
-
-    # else:
-    #     speak("Good Evening!")  
-
-    speak(value)       
+# Function used to take commands as statements from the voice of a human using laptops in-built Microphone
 
 def takeCommand():
-    #It takes microphone input from the user and returns string output
-
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Listening...")
-        r.pause_threshold = 1
+        print("i am listening . . . .")
         audio = r.listen(source)
+        
+        try:
+            statement = r.recognize_google(audio,language='en-in')
+            print(f"user said:{statement}\n")
+            
+        except:
+            speak("pardon me , please say that again")
+            return "None"
+        return statement 
 
-    try:
-        print("Recognizing...")    
-        query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}\n")
+# A Simplified main function
+# Add your code blocks here to upgrade this personal assistant. it is 100% modifiable 
 
-    except Exception as e:
-        # print(e)    
-        print("Say that again please...")  
-        return "None"
-    return query
+speak("LOADING YOU PERSONAL A.I ASSISTANT JARVIS")
+wishMe()
 
-def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('vanshajpoonia06@gmail.com', 'your-password')
-    server.sendmail('vanshajpoonia06@gmail.com', to, content)
-    server.close()
 
-def screenShot():
-    img = pyautogui.screenshot()
-    img.save ("C:\\Users\\vansh\\OneDrive\\Pictures\\Jarvis Shots\\ss.png")
 
-def cpu():
-    usage = str(psutil.cpu_percent())
-    speak('CPU is at'+ usage)
-    battery = psutil.sensors_battery()
-    speak("Battery is at")
-    speak(battery.percent )
+if __name__ == '__main__':
 
-def jokes():
-    speak(pyjokes.get_joke())
 
-if __name__ == "__main__":
-    wishMe()
     while True:
-    # if 1:
-        query = takeCommand().lower()
+        speak("Tell me how can I help you now?")
+        statement = takeCommand().lower()
+        if statement == 0:
+            continue
 
-        # Logic for executing tasks based on query
-        if 'wikipedia' in query:
+        if "good bye" in statement or "ok bye" in statement or "stop" in statement:
+            speak('your A.I personal assistant JARVIS is shutting down,Good bye')
+            print('your A.I personal assistant JARVIS is shutting down,Good bye')
+            break
+
+
+        if 'wikipedia' in statement:
             speak('Searching Wikipedia...')
-            query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=2)
+            statement =statement.replace("wikipedia", "")
+            results = wikipedia.summary(statement, sentences=3)
             speak("According to Wikipedia")
             print(results)
             speak(results)
-        
-        # Logic for opening apps
 
-        elif 'open youtube' in query:
-            webbrowser.get('chrome').open_new_tab(url1)
-            # webbrowser.open("youtube.com")
-
-        elif 'open google' in query:
-            webbrowser.get('chrome').open_new_tab(url2)
-            # webbrowser.open("google.com")
-
-        elif 'open amazon' in query:
-            webbrowser.get('chrome').open_new_tab(url3)
-
-        elif 'code projects' in query:
-            webbrowser.get('chrome').open_new_tab(url4)
-
-        elif 'coding' in query:
-            speak('coding mat lab.. harrry bhai')
-            webbrowser.get('chrome').open_new_tab(url5)
+        if 'instagram' in statement or "profile" in statement:
+            mod = instaloader.Instaloader()
+            speak("whats the name on instagram ?")
+            a = input("Enter correct user name > > ")
+            mod.download_profile(a,profile_pic_only = True)
+            speak("Your crushs picture of instagram is saved have a look at your folder ")
             
 
-        elif 'open stackoverflow' in query:
-            webbrowser.get('chrome').open_new_tab(url6)
-            # webbrowser.open("stackoverflow.com")
-
-        elif 'online classes' in query:
-            webbrowser.get('chrome').open_new_tab(url7)
-
-        elif 'play music' in query:
-            # music_dir = 'D:\\YT Downloads\\Songs\\'
-            # songs = os.listdir(music_dir)
-            # print(songs)    
-            # os.startfile(os.path.join(music_dir, songs[0]))
-            kit.playonyt(gaana)
-
-        elif 'change' in query:
-            kit.playonyt(othersongs)
-
-        # elif 'change music' in query:
-        #     kit.playonyt(othersongs)
-
-        elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-            speak(f"Sir, the time is {strTime}")
-
-            hour = int(datetime.datetime.now().hour)
-
-        elif 'time in detail' in query:
-            print(hour)
-            year = int(datetime.datetime.now().year)
-            month = int(datetime.datetime.now().month)
-            date = int(datetime.datetime.now().day)
-            Time = datetime.datetime.now().strftime("%I:%M:%S") 
-            print(Time)
-            print(date)
-            print(month)
-            print(year)
-            speak("the current Time is")
-            speak(Time)
-            speak("the current Date is")
-            speak(date)
-            speak(month)
-            speak(year)
+            
+        elif 'open youtube' in statement:
+            webbrowser.open_new_tab("https://www.youtube.com")
+            speak("youtube is open for you")
+            time.sleep(3)
             
 
-        elif 'open code' in query:
-            codePath = "C:\\Users\\vansh\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
-            os.startfile(codePath)
+        elif 'open google' in statement:
+            webbrowser.open_new_tab("https://www.google.com")
+            speak("Google chrome is open for you")
+            time.sleep(3)
+            
 
-        elif 'screenshot' in query:
-                screenShot()
-                speak('Done!')
+        elif 'open gmail' in statement:
+            webbrowser.open_new_tab("gmail.com")
+            speak("Google Mail open for you")
+            time.sleep(3)
+            
+        elif "weather" in statement:
+            api_key="8ef61edcf1c576d65d836254e11ea420"
+            base_url="https://api.openweathermap.org/data/2.5/weather?"
+            speak("whats the city name")
+            city_name=takeCommand()
+            complete_url=base_url+"appid="+api_key+"&q="+city_name
+            response = requests.get(complete_url)
+            x=response.json()
+            if x["cod"]!="404":
+                y=x["main"]
+                current_temperature = y["temp"]
+                current_humidiy = y["humidity"]
+                z = x["weather"]
+                weather_description = z[0]["description"]
+                speak(" Temperature in kelvin unit is " +
+                      str(current_temperature) +
+                      "\n humidity in percentage is " +
+                      str(current_humidiy) +
+                      "\n description  " +
+                      str(weather_description))
+                print(" Temperature in kelvin unit = " +
+                      str(current_temperature) +
+                      "\n humidity (in percentage) = " +
+                      str(current_humidiy) +
+                      "\n description = " +
+                      str(weather_description))
 
-        elif 'cpu' in query:
-            cpu()
+            else:
+                speak(" City Not Found ")
 
-        elif 'time' in query:
-            time()
-        elif 'date' in query:
-            date()
-        
-        elif 'log out' in query:
-            os.system("shutdown -l")
+        elif 'time' in statement:
+            strTime=datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"the time is {strTime}")
 
-        elif 'shutdown' in query:
-            os.system("shutdown /s /t 1")
-
-        elif 'restart' in query:
-            os.system("shutdown /r /t 1")
-
-        elif 'offline' in query:
-            quit()
-        
-        elif 'joke' in query:
-            jokes()
-
-        elif 'to do' in query:
-            speak("")
-
-        elif 'find location' in query:
-            search_term = query.replace("search","")
-            # location=query('What is the location?')
-            url = 'https://google.com/maps/place' + search_term + '/&amp;'
-            webbrowser.get().open(url)
-            speak('Here is the location of ' + search_term)
-
-        # elif 'search' in query:
-        #     search_term = query.split("for")[-1]
-        #     url = f"https://google.com/search?q={search_term}"
-        #     webbrowser.get().open(url)
-        #     speak(f'Here is what I found for {search_term} on google')
-
-        # elif 'search' in query:
-        #     search_term = query.replace("search for","")
-        #     url = "https://google.com/search?q=" + search_term
-        #     webbrowser.get().open(url)
-        #     speak("Here is what I found for" + search_term + "on google")
-
-        elif 'on google' in query:
-            # search_term = query.replace("search google for","",)
-            search_term = query.split("for")[-1]
-            search_term = search_term.replace("on google","").replace("search","")
-            url = "https://google.com/search?q=" + search_term
-            webbrowser.get().open(url)
-            speak("Here is what I found for" + search_term + "on google")
-
-        elif 'search' in query:
-            search_term = query.replace("search for","")
-            url = "https://moogle.vercel.app/search?term=" + search_term
-            webbrowser.get().open(url)
-            speak("Here is what I found for" + search_term + "on moogle")
-
-        # elif ' youtube' in query:
-        #     search_term = query.split("for")[-1]
-        #     url = f"https://www.youtube.com/results?search_query={search_term}"
-        #     webbrowser.get().open(url)
-        #     speak(f'Here is what I found for {search_term} on youtube')
-
-        elif 'on youtube' in query:
-            search_term = query.split("for")[-1]
-            search_term = search_term.replace("on youtube","").replace("search","")
-            url = "https://www.youtube.com/results?search_query=" + search_term
-            webbrowser.get().open(url)
-            speak("Here is what I found for " + search_term + "on youtube")
-
-        elif 'on mewtube' in query:
-            search_term = query.split("for")[-1]
-            search_term = search_term.replace("on mewtube","").replace("search","")
-            url = "https://mewtube.web.app/search/" + search_term
-            webbrowser.get().open(url)
-            speak("Here is what I found for " + search_term + "on mewtube")
-
-        elif'what is my exact location' in query:
-            url = "https://www.google.com/maps/search/Where+am+I+?/"
-            webbrowser.get().open(url)
-            speak("You must be somewhere near here, as per Google maps") 
+        elif 'who are you' in statement or 'what can you do' in statement:
+            speak('I am JARVIS version 1 point O your persoanl assistant. I am programmed to minor tasks like'
+                  'opening youtube,google chrome,gmail and stackoverflow ,predict time,take a photo,search wikipedia,predict weather' 
+                  'in different cities , get top headline news from times of india and you can ask me computational or geographical questions too!')
 
 
-        elif 'email to vanshaj' in query:
-            try:
-                speak("What should I say?")
-                content = takeCommand()
-                to = "vanshajpoonia06@gmail.com"    
-                sendEmail(to, content)
-                speak("Email has been sent!")
-            except Exception as e:
-                print(e)
-                speak("Sorry my friend Vanshaj. I am not able to send this email")    
+        elif "who made you" in statement or "who created you" in statement or "who discovered you" in statement:
+            speak("I was built by ABC")
+            print("I was built by ABC")
+        elif "open stackoverflow" in statement:
+            webbrowser.open_new_tab("https://stackoverflow.com/login")
+            speak("Here is stackoverflow")
+            time.sleep(3)
 
-        
-        
-        
-        
-        
-        ############Dialogues####################################
-        ##structure##
-        #vanshaj
-        #about
-        #school
-        #views
-        elif 'who are you' in query:
-            speak('I am Jarvis')
-            print('I am Jarvis')
-        
-        elif 'who is vanshaj' in query:
-            speak('Vanshaj is the most genius person in the galaxy.. oh sorry in the universe')
-            print('Vanshaj is the most genius person in the galaxy.. oh sorry in the universe')
-        
-        elif 'you seem to be intelligent'  in query:
-            speak('Hey you.. Mind your language.. I dont seem to be so, I actually am. Dont reapeat that statement Otherwise you know what can I do.. I can freakin hack your system and then I will tell you how intelligent I am... hun seem to be')
+        elif 'news' in statement:
+            news = webbrowser.open_new_tab("https://timesofindia.indiatimes.com/home/headlines")
+            speak('Here are some headlines from the Times of India - Happy reading')
+            time.sleep(3)
+            
+        elif 'bank' in statement:
+            bank = webbrowser.open_new_tab("https://www.axisbank.com")
+            speak('Here is the details of axis bank')
+            time.sleep(3)
 
 
+        elif 'search'  in statement:
+            statement = statement.replace("search", "")
+            webbrowser.open_new_tab(statement)
+            time.sleep(3)
+            
 
-        #############Dialogues end here ##########################
+        elif 'ask' in statement:
+            speak('I can answer to computational and geographical questions and what question do you want to ask now')
+            question=takeCommand()
+            app_id="R2K75H-7ELALHR35X"
+            client = wolframalpha.Client('R2K75H-7ELALHR35X')
+            res = client.query(question)
+            answer = next(res.results).text
+            speak(answer)
+            print(answer)
+            
+        elif "love" in statement or "like" in statement:
+            speak('You are the person who made me and bring me to this wonderful world, yes i love you too')
+        elif "feel" in statement or "feelings" in statement:
+            speak('offcourse yes, i want to feel their emotions live & work with humans just to make this world a better place')
+            speak('You may add a neural network module in me, which will make me feel and understands emotions and perform my tasks better')
+            
+            
+        elif "age" in statement or "old" in statement:
+            speak('I am still sweet sixteen, and i will be that forever')
 
 
-        
+        elif "log off" in statement or "sign out" in statement:
+            speak("Ok , your pc will log off in 10 sec make sure you exit from all applications")
+            subprocess.call(["shutdown", "/l"])
+
+time.sleep(3)

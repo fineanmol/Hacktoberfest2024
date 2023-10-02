d50435ced8430806f80b51c568397dddbbccca7f
beacon45 <codewithbeacon@gmail.com>
2023-10-02 19:48:26 +0530

    Updated Contributer List

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2054,4 +2054,9 @@ contributors = [
     fullname: "Mohit Yadav",
     username: "https://github.com/ymohit1603",
   },
+  {
+    id: 424,
+    fullname: "Souvik Banik",
+    username: "https://github.com/beacon45",
+  },
 ];

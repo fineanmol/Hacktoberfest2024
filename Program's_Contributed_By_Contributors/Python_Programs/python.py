126cef900f9fe0375ad8e3f8cb69c2320205a9a3
Maksim Korolev <57666774+pkrfc@users.noreply.github.com>
2023-10-02 21:20:59 +0300

    Update python.py

--- Program's_Contributed_By_Contributors/Python_Programs/python.py
+++ Program's_Contributed_By_Contributors/Python_Programs/python.py
@@ -1,6 +1,6 @@
 import random
 
 test_list = ['black', 'white', 'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple', 'violet', 'pink', 'silver', 'gold', 'brown', 'gray']
-random_index = random.randrange(len(test_list))
+random_element = random.choice(test_list)
 
-print(test_list[random_index])
+print(random_element)

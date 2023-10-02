8d8e61fd08c7674a31e17dcbeac87ff5a2eefaae
Rohan Biswas <rohan.biswas2020@uem.edu.in>
2023-10-02 10:49:09 +0530

    contributor list ID 424 added

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2054,4 +2054,9 @@ contributors = [
     fullname: "Mohit Yadav",
     username: "https://github.com/ymohit1603",
   },
+  {
+    id: 424,
+    fullname: "Rohan Biswas",
+    username: "https://github.com/rohanpranto",
+  },
 ];

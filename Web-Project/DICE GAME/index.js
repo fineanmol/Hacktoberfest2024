d5a53ea8b56a44d7e045247c42095b149bce186b
Abhijit Sahoo <sahooabhijit0001@gmail.com>
2023-10-02 21:26:43 +0530

    Updated Contributing.js file and added a web dev project into the repository

---
+++ Web-Project/DICE GAME/index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage= "dice"+randomNumber1+".png";
+
+var randomImageSource= "images/"+ randomDiceImage;
+
+var image1= document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomDiceImage2 = "dice"+ randomNumber2+ ".png";
+var randomImageSource2 = "images/"+ randomDiceImage2;
+
+var image2= document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
+
+if(randomNumber1> randomNumber2){
+    document.querySelector("h1").innerHTML="🚩Player 1 wins!";
+}
+else if(randomNumber1< randomNumber2){
+    document.querySelector("h1").innerHTML="🚩Player 2 wins!";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!"
+}

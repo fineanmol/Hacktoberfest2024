1788bf1fe03720c25cc114af5d0818de5a473b8b
Mani Sankar Chintagunti <114079126+Mindslayer001@users.noreply.github.com>
2023-10-02 12:16:09 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2054,4 +2054,9 @@ contributors = [
     fullname: "Mohit Yadav",
     username: "https://github.com/ymohit1603",
   },
+  {
+    id: 424,
+    fullname: "Mani Sankar Chintagunti",
+    username: "https://github.com/Mindslayer001
+  }
 ];

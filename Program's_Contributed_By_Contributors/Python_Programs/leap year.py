804691ea88dc58e57bc5bf6172fae4944282cc06
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2023-10-02 23:29:31 +0530

    Update leap year.py
    Divisibility by 4: The primary rule is that a year is a leap year if it is divisible by 4. This is a simple rule that adds an extra day (February 29th) to the calendar every four years, helping to account for the fact that the Earth's orbit around the Sun takes approximately 365.2422 days.
    
    Divisibility by 100: To further refine the system, years divisible by 100 are not leap years unless they are also divisible by 400. This rule removes some leap years, correcting for the fact that the basic rule of divisibility by 4 would make too many leap years. For example, without this rule, the year 1900 would be considered a leap year (divisible by 4), but it's not because it's divisible by 100 and not by 400.
    
    Divisibility by 400: The exception to the above rule is that years divisible by 400 are still considered leap years. This rule ensures that certain years, like the year 2000, are leap years, even though they are divisible by 100.

--- Program's_Contributed_By_Contributors/Python_Programs/leap year.py
+++ Program's_Contributed_By_Contributors/Python_Programs/leap year.py
@@ -1,16 +1,8 @@
 # Check leap year
-
+#Leap year is the sequence of every 4 years so to check if  a given year is leap year or not we have to check its divisibility by 4,400, and 100.
 def leap_year_check(yr):
-    leap = False
-    if yr % 4 == 0:
-        leap = True
-        if yr % 100 ==0 and yr % 400 != 0:
-            leap = False
-        else: 
-            leap = True
-    if leap: 
-        print("{0} is a leap year".format(yr))
-    else: 
-        print("{0} is not a leap year".format(yr))
-
+    if (yr % 4 == 0 and yr % 100 != 0) or (yr % 400 == 0) == 0:
+        print("Its a leap year")
+    else:
+        print("Its not a leap year")
 leap_year_check(1700)

110c02b73fce1f739f5fe95be46f03af675fe8e3
Naymul Islam <naymul504@gmail.com>
2023-10-02 23:30:09 +0600

    Update the BMI Calculator.py file
    Transformed the codes into modular code

--- Program's_Contributed_By_Contributors/Python_Programs/BMI Calculator.py
+++ Program's_Contributed_By_Contributors/Python_Programs/BMI Calculator.py
@@ -1,49 +1,50 @@
+def get_height():
+    a = int(input("Height in 1.inches or 2. feet and inches or 3.centimeters? choose and type 1 or 2 or 3 ->"))
+    if a == 1:
+        height = float(input("Enter your height in inches: "))
+        return height * 2.54 / 100
+    elif a == 2:
+        height = str(input("Enter in this format: 5'4 or foot'inches ->"))
+        return (float(height[0]) * 12 * 2.54 + float(height[2]) * 2.54) / 100
+    elif a == 3:
+        height = float(input("Enter your height in centimeters: "))
+        return height / 100
 
-#input height
-a= int(input((" Height in 1.inches or 2. feet and inches or 3.centimeters? choose and type 1 or 2 or 3 ->")))
+def get_weight():
+    b = int(input("Weight in 1.kg or 2.pounds? choose and type 1 or 2 ->"))
+    weight = float(input("Enter your weight: "))
+    if b == 1:
+        return weight
+    elif b == 2:
+        return weight * 0.453592
 
-if a==1:
-             Height = float(input(" Enter your height: "))
-             Height*=2.54
-elif ( a==2 ):
-             Height_1 = str(input(" Enter in this format: 5'4 or foot'inches ->" ))
-             Height =  float(Height_1[0]) *12 *2.54  + float(Height_1[2]) *2.54 
-             
+def calculate_bmi(height, weight):
+    return weight / (height * height)
 
-#input weight
-b= int( input (" Weight in 1.kg or 2.pounds? choose and type 1 or 2 ->"))
-
-Weight = float( input(" Enter your Weight-> "))
-if b==2:
-             Weight*=0.453592
-
-#convert height into metres
-Height = Height / 100
-
-#calculate BMI
-BMI = Weight / (Height*Height)
-
-#printing BMI
-print(f"Your Body Mass Index is: {BMI}")
-
-#printing results
-if( BMI > 0):
-    if( BMI <= 16 ):
+def print_bmi_category(bmi):
+    if bmi <= 16:
         print("You are severely underweight")
-    elif( BMI <= 17 ):
+    elif bmi <= 17:
         print("You are moderately underweight")
-    elif( BMI <= 18.5 ):
+    elif bmi <= 18.5:
         print("You are mildly underweight")
-    elif ( BMI <= 25 ):
+    elif bmi <= 25:
         print("You are healthy")
-    elif ( BMI <= 30 ):
+    elif bmi <= 30:
         print("You are overweight")
-    elif ( BMI <= 35 ):
+    elif bmi <= 35:
         print("You belong to obesity class I")
-    elif ( BMI <= 40 ):
+    elif bmi <= 40:
         print("You belong to obesity class II")
     else:
         print("You belong to obesity class III")
-else:
-    print("Enter valid details")
-    
+
+def main():
+    height = get_height()
+    weight = get_weight()
+    bmi = calculate_bmi(height, weight)
+    print(f"Your Body Mass Index is: {bmi}")
+    print_bmi_category(bmi)
+
+if __name__ == "__main__":
+    main()

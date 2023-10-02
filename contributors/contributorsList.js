fd72a6fbfc195a787c544f4eceb821e71247c89c
iamansingh0 <amankumar7355197337@gmail.com>
2023-10-02 21:08:35 +0530

    update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2054,4 +2054,9 @@ contributors = [
     fullname: "Mohit Yadav",
     username: "https://github.com/ymohit1603",
   },
+  {
+    id: 424,
+    fullname: "Aman Kumar Singh",
+    username: "https://github.com/iamansingh0"
+  },
 ];

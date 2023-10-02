ba2ceb61420c60266a0146027042d52ccfff7a3f
Shubham Nagure <nagure.shubham@gmail.com>
2023-10-02 16:44:54 +0530

    Create game_stone_paper_ scissors.py

---
+++ Program's_Contributed_By_Contributors/Python_Programs/game_stone_paper_ scissors.py
@@ -0,0 +1,42 @@
+import random
+
+def get_user_choice():
+    while True:
+        user_choice = input("Enter your choice (rock, paper, or scissors): ").lower()
+        if user_choice in ["rock", "paper", "scissors"]:
+            return user_choice
+        else:
+            print("Invalid choice. Please choose rock, paper, or scissors.")
+
+def get_computer_choice():
+    choices = ["rock", "paper", "scissors"]
+    return random.choice(choices)
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (
+        (user_choice == "rock" and computer_choice == "scissors")
+        or (user_choice == "paper" and computer_choice == "rock")
+        or (user_choice == "scissors" and computer_choice == "paper")
+    ):
+        return "You win!"
+    else:
+        return "Computer wins!"
+
+def main():
+    print("Welcome to Rock-Paper-Scissors!")
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+        print(f"You chose: {user_choice}")
+        print(f"Computer chose: {computer_choice}")
+        result = determine_winner(user_choice, computer_choice)
+        print(result)
+        
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != "yes":
+            break
+
+if __name__ == "__main__":
+    main()

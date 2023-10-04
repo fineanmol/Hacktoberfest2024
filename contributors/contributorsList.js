8ca61bba52716b9bbcd1ca64205fc8e3c554fe39
varghese-babu <55503677+varghese-babu@users.noreply.github.com>
2023-10-04 11:55:26 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2054,4 +2054,9 @@ contributors = [
     fullname: "Mohit Yadav",
     username: "https://github.com/ymohit1603",
   },
+  {
+    id: 424,
+    fullname: "Varghese Babu",
+    username: "https://github.com/varghese-babu",
+  },
 ];

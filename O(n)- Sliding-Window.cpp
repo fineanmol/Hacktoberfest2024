46183121da2c087f6ee121d04ae8ec0a939ce1fa
NEERAJ DHAKAD <dhakadneeraj786@gmail.com>
2023-10-05 14:03:17 +0530

    Create O(n)- Sliding-Window.cpp

---
+++ O(n)- Sliding-Window.cpp
@@ -0,0 +1,41 @@
+// O(n) solution for finding maximum sum of
+// a subarray of size k
+#include <iostream>
+using namespace std;
+
+// Returns maximum sum in a subarray of size k.
+int maxSum(int arr[], int n, int k)
+{
+	// n must be greater
+	if (n < k) {
+		cout << "Invalid";
+		return -1;
+	}
+
+	// Compute sum of first window of size k
+	int max_sum = 0;
+	for (int i = 0; i < k; i++)
+		max_sum += arr[i];
+
+	// Compute sums of remaining windows by
+	// removing first element of previous
+	// window and adding last element of
+	// current window.
+	int window_sum = max_sum;
+	for (int i = k; i < n; i++) {
+		window_sum += arr[i] - arr[i - k];
+		max_sum = max(max_sum, window_sum);
+	}
+
+	return max_sum;
+}
+
+// Driver code
+int main()
+{
+	int arr[] = { 1, 4, 2, 10, 2, 3, 1, 0, 20 };
+	int k = 4;
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << maxSum(arr, n, k);
+	return 0;
+}

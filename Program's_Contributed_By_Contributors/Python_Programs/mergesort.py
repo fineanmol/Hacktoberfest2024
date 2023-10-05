8b66ec50ba57d20259a790b990cad7d7c1b19357
prokan468 <61049742+prokan468@users.noreply.github.com>
2023-10-05 15:46:13 +0530

    Create mergesort.py
    Added a DSA script that sorts using the merging technique.

---
+++ Program's_Contributed_By_Contributors/Python_Programs/mergesort.py
@@ -0,0 +1,56 @@
+def merge(arr, l, m, r):
+	n1 = m - l + 1
+	n2 = r - m
+	L = [0] * (n1)
+	R = [0] * (n2)
+
+	for i in range(0, n1):
+		L[i] = arr[l + i]
+
+	for j in range(0, n2):
+		R[j] = arr[m + 1 + j]
+
+	i = 0	 
+	j = 0	 
+	k = l	 
+
+	while i < n1 and j < n2:
+		if L[i] <= R[j]:
+			arr[k] = L[i]
+			i += 1
+		else:
+			arr[k] = R[j]
+			j += 1
+		k += 1
+	
+	while i < n1:
+		arr[k] = L[i]
+		i += 1
+		k += 1
+
+	
+	
+	while j < n2:
+		arr[k] = R[j]
+		j += 1
+		k += 1
+
+def mergeSort(arr, l, r):
+	if l < r:	
+		m = l+(r-l)//2
+		mergeSort(arr, l, m)
+		mergeSort(arr, m+1, r)
+		merge(arr, l, m, r)
+
+arr = [12, 11, 13, 5, 6, 7]
+n = len(arr)
+print("Given array is")
+for i in range(n):
+	print("%d" % arr[i],end=" ")
+
+mergeSort(arr, 0, n-1)
+print("\n\nSorted array is")
+for i in range(n):
+	print("%d" % arr[i],end=" ")
+
+

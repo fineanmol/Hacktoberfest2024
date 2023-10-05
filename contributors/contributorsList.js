903eda3b85f506402771fccc6eced8823860f018
krydelmany <giclamoraes@gmail.com>
2023-10-05 13:46:56 -0300

    turn pc off python add

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2054,4 +2054,9 @@ contributors = [
     fullname: "Mohit Yadav",
     username: "https://github.com/ymohit1603",
   },
+  {
+    id: 424,
+    fullname: "Giovani Claro Moraes",
+    username: "https://github.com/Krydelmany"
+  }
 ];

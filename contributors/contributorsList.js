5afbfafc05217531f40ec420933bbc1f8a725273
Aditya <helloadig@gmail.com>
2023-10-05 17:58:52 +0530

    filling name in contributors list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2054,4 +2054,9 @@ contributors = [
     fullname: "Mohit Yadav",
     username: "https://github.com/ymohit1603",
   },
+  {
+    id: 424,
+    fullname: "Aditya Gaikwad",
+    username: "https://github.com/aaadityaG",
+  },
 ];

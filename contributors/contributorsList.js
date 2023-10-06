49fd6ff76842a0fb02ad136a289c7767d4057b78
shanharold <146119600+shanharold@users.noreply.github.com>
2023-10-07 00:39:50 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426
+    fullname: "Shan Harold",
+    username: "https://github.com/shanharold"
+  }
 ];

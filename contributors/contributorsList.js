402f8e813f8ef8e0c3409a084c4de230302758b2
khalid <khalidcomilla58@gmail.com>
2023-10-06 22:15:19 +0600

    Graph theory concept added

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Khalid Abdullah",
+    username: "https://github.com/khalid586",
+  },
 ];

2b80a10de0f121838a9f22e485ef6de42a3ddf40
Arijit Ghosh <92109154+Rancho2002@users.noreply.github.com>
2023-10-07 00:29:56 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Arijit Ghosh",
+    username: "https://github.com/rancho2002",
+  },
 ];

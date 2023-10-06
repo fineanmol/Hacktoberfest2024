f8809ad6edc183a6afa8b73c6f22db93a9b66d00
Hritik Raj <37825309+Hritikraj8804@users.noreply.github.com>
2023-10-06 18:36:36 +0530

    add myself

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+   {
+    id: 426,
+    fullname: "Hritik Raj",
+    username: "https://github.com/Hritikraj8804",
+  },
 ];

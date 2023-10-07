ca441fb19445c9f23349b0b94dad4680d1cda162
Gaurav Saxena <111829266+Gauravsaxena816@users.noreply.github.com>
2023-10-07 21:32:44 +0530

    Create stacks.cpp

---
+++ Program's_Contributed_By_Contributors/C++ Programs/Stacks/stacks.cpp
@@ -0,0 +1,68 @@
+// Write a C++ program to implement a stack using an array with push and pop operations. Find the top element of the stack and check if the stack is empty or not.
+#include<iostream>
+#define size 1000
+using namespace std;
+class stack{
+    private :
+    int top;
+    int arr[size];
+
+    public:
+
+    stack(){
+        top = -1;
+    }
+
+    bool push(int x){
+        if(top>=size-1){
+            cout<<"stack is full"<<endl;
+            return false; 
+        }
+        arr[++top] = x;
+        return true;
+    }
+
+    int pop(){
+        if(top<1){
+            cout<<"stack is underflow";
+            return 0;
+        }
+        return arr[top--];
+    }
+
+    int peek(){
+        if (top<1){
+            cout<<"stack is underflow";
+            return 0;
+        }
+        return arr[top];   
+    }
+
+    bool isempty(){
+        return (top<1);
+    }
+
+    void print(){
+        if(top<1){
+            cout<<"stack is empty"<<endl;
+        }
+
+        for(int i = top;i>=0;i--){
+            cout<<arr[i]<<" ";
+        }
+    }
+};
+
+int main(){
+    stack s;
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        s.push(x);
+    }
+    cout<<s.isempty()<<endl;
+    cout<<"Element of the stack are :"<<endl;
+    s.print();
+}

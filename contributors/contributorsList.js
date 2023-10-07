68b9c5c8fc03fd55de7ad85c7b517e5bd50e3a27
smoll-xiao <108113313+smoll-xiao@users.noreply.github.com>
2023-10-07 14:27:00 -0700

    just periods for consistancy and a couple emojis.

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Xiaoren C.",
+    username: "https://github.com/smoll-xiao",
+  },
 ];

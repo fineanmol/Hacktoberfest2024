5d95a396bb4931d4994957b8d9e17ccea91c6c9a
waruna srinath <waruna12345@outlook.com>
2023-10-07 21:57:42 +0530

    added new figures to merge sort

--- Program's_Contributed_By_Contributors/Python_Programs/Merge sort Algorithm.py
+++ Program's_Contributed_By_Contributors/Python_Programs/Merge sort Algorithm.py
@@ -15,9 +15,10 @@ def merge(nums1, m, nums2, n):
         p2 -= 1
         p -= 1
 
-nums1 = [1, 2, 3, 0, 0, 0]
+nums1 = [4, 8, 3, 4, 9, 7]
 m = 3
 nums2 = [2, 5, 6]
 n = 3
 merge(nums1, m, nums2, n)
-print(nums1)  
+print(nums1) 
+ 

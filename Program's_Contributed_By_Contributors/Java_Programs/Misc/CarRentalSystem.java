aa37b87e7b7e8cd07e26f65fc7f5b45924385bc0
RichaBanerjee <richa2882002@gmail.com>
2023-10-07 23:42:25 +0530

    commiting again

--- Program's_Contributed_By_Contributors/Java_Programs/Misc/CarRentalSystem.java
+++ Program's_Contributed_By_Contributors/Java_Programs/Misc/CarRentalSystem.java
@@ -11,33 +11,39 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class Car {
+class Car 
+{
     private String carId;
     private String brand;
     private String model;
     private double basePricePerDay;
     private boolean isAvailable;
 
-    public Car(String carId, String brand, String model, double basePricePerDay) {
+    public Car(String carId, String brand, String model, double basePricePerDay) 
+    {
         this.carId = carId;
         this.brand = brand;
         this.model = model;
         this.basePricePerDay = basePricePerDay;
         this.isAvailable = true;
     }
-    public String getCarId() {
+    public String getCarId() 
+    {
         return carId;
     }
 
-    public String getBrand() {
+    public String getBrand() 
+    {
         return brand;
     }
 
-    public String getModel() {
+    public String getModel() 
+    {
         return model;
     }
 
-    public double calculatePrice(int rentalDays) {
+    public double calculatePrice(int rentalDays) 
+    {
         return basePricePerDay * rentalDays;
     }
 

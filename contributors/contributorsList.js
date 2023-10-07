234450cd8734edd7d5721ad805fbda1eb884d235
Kumar Aryan <kumararyanaim@gmail.com>
2023-10-08 02:12:09 +0530

    Added Myself to Contribution List

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Kumar Aryan",
+    username: "https://github.com/Kumar-Aryan-NIET",
+  },
 ];

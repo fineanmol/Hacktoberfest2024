5b58e578ac9d54db96c24c1ee8c075d0d630a1e4
Sander Sonck <sander.sonck@student.hogent.be>
2023-10-07 15:17:25 +0200

    added python-file for sorting

---
+++ Program's_Contributed_By_Contributors/Python_Programs/selection_sort_inFront.py
@@ -0,0 +1,17 @@
+def selection_sort_inFront(a):
+    n = len(a)
+    for i in range(n-1):
+        position = i
+        minimum = a[i]
+        for j in range(i+1, n):
+            if a[j] < minimum:
+                position = j
+                minimum = a[j]
+        a[position] = a[i]
+        a[i]=minimum
+        print(a)
+    
+
+numbers = "44 55 12 42 94 18 6 67"
+a = [int(i) for i in numbers.split()]
+selection_sort_inFront(a)

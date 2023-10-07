88287446a0489ba6b9c5c3f21a8633679be9cdf1
kharshita590 <83570759+kharshita590@users.noreply.github.com>
2023-10-08 00:27:27 +0530

    Created rockPaperScissor.py

---
+++ rockPaperScissor.py
@@ -0,0 +1,11 @@
+#rock paper scissors
+print("rock,paper,scissor")
+rps=input('type "rock","paper",or "scissor"')
+if rps=="paper":
+  print("scissor","you loose")
+if rps=="scissor":
+ print("stone","you loose")
+if rps=="rock":
+ print("scissor","you win")
+if rps=="stone":
+ print("paper","you loose")

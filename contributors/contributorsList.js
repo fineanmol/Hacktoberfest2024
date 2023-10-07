8a701f5fa1072cc4139bb6a73d4a831c367bb53e
Ayush Arora <ayusharora21003@gmail.com>
2023-10-08 00:04:03 +0530

    add ayush in contributors list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Ayush Arora",
+    username: "https://github.com/ayusharora21003",
+  }
 ];

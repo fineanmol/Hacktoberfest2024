456d2211079f83aa0aa588a41c2ef37c563c2c4a
camarota <joao.camarota@discente.ufg.br>
2023-10-07 13:25:13 -0300

    added myself

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "João Guihlerme",
+    username: "https://github.com/camarota-234",
+  },
 ];

baadd65d982851913ba46d70b6c2f9e9470091dc
Surya Efendi <suryaefendi72@gmail.com>
2023-10-08 11:29:21 +0700

    add contrib

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 427,
+    fullname: "Surya E.",
+    username: "https://github.com/luxfield",
+  },
 ];

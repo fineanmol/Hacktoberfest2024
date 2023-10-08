e34aef7c4945ac5011f92c16763b4f5e76544b75
Abdul Rehman <105493274+AbdulRehmanGHub@users.noreply.github.com>
2023-10-08 18:34:01 +0000

    files added

---
+++ abdulrehmanghub_files/sameTree.cpp
@@ -0,0 +1,23 @@
+/**
+    Abdul Rehman
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p && !q) return true;
+        if(!p || !q) return false;
+        bool root = (p->val == q->val);
+        bool lst = isSameTree(p->left, q->left);
+        bool rst = isSameTree(p->right, q->right);
+        return root && lst && rst;
+    }
+};

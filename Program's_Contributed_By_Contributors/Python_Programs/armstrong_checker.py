ed42406d4fbf2bb5ee4e6f45842ea349923c1539
Rohan Sardar <77870108+RohanSardar@users.noreply.github.com>
2023-10-08 11:56:49 +0530

    Armstrong number
    This is a python program to take user input and check whether the given number is an Armstrong number or not.

---
+++ Program's_Contributed_By_Contributors/Python_Programs/armstrong_checker.py
@@ -0,0 +1,28 @@
+def armstrongChecker(num:int) -> bool:
+    """
+    An Armstrong number is a number that 
+    is equal to the sum of its digits 
+    each raised to the power of the number of digits.
+
+    Usage examples:
+
+    >>> armstrongChecker(153)
+    True
+    >>> armstrongChecker(370)
+    True
+    >>> armstrongChecker(1634)
+    True
+    >>> armstrongChecker(25)
+    False
+    >>> armstrongChecker(1456)
+    False
+
+    """
+    temp = str(num)
+    result = 0
+    for i in temp:
+        result += int(i) ** len(temp)
+    return num == result
+
+number = int(input("Enter a number: "))
+print(f"\n\tGiven number is {number} \n\t{armstrongChecker(number) = }\n")

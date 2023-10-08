e2bd89e4910351acd39df9452e4d1317c6eb594f
Hardik Mittal <mhardik2003@ggmail.com>
2023-10-08 21:33:04 +0530

    Added name to list of contributors

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id:426,
+    fullname: "Hardik Mittal",
+    username: "https://github.com/mhardik003"
+  }
 ];

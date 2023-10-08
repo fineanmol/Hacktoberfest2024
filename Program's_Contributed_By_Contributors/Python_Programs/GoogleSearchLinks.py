60087182c780e57e61b444fc15dc55b5027b107b
Abhijit Mohanty <51369559+mohantyabhijit074@users.noreply.github.com>
2023-10-08 11:32:40 +0530

    Added GoogleSearchLinks.py
    Fetches the google links for passed parameter.

---
+++ Program's_Contributed_By_Contributors/Python_Programs/GoogleSearchLinks.py
@@ -0,0 +1,47 @@
+import requests
+import urllib
+import pandas as pd
+from requests_html import HTML
+from requests_html import HTMLSession
+
+def get_source(url):
+    """Return the source code for the provided URL. 
+
+    Args: 
+        url (string): URL of the page to scrape.
+
+    Returns:
+        response (object): HTTP response object from requests_html. 
+    """
+
+    try:
+        session = HTMLSession()
+        response = session.get(url)
+        return response
+
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+def scrape_google(query):
+
+    query = urllib.parse.quote_plus(query)
+    #call get_source and pass the query to get the source
+    response = get_source("https://www.google.co.uk/search?q=" + query)
+
+    links = list(response.html.absolute_links)
+    # some of the google domains
+    google_domains = ('https://www.google.', 
+                      'https://google.', 
+                      'https://webcache.googleusercontent.', 
+                      'http://webcache.googleusercontent.', 
+                      'https://policies.google.',
+                      'https://support.google.',
+                      'https://maps.google.')
+
+    for url in links[:]:
+        if url.startswith(google_domains):
+            links.remove(url)
+
+    return links
+
+scrape_google("Hacktoberfest")

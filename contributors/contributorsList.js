f060ee69cff62514106cb4405b012a89736f6457
sharonllcj <146122682+sharonllcj@users.noreply.github.com>
2023-10-08 19:05:31 -0700

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,8 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Sharon L.",
+    username: "https://github.com/sharonllcj",
 ];

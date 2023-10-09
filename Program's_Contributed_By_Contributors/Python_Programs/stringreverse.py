3bbae3e057d8395bd3af22e7c8026e052453ede9
Akbar Ahmed <70852077+Akbar-Ahmed@users.noreply.github.com>
2023-10-09 12:02:35 +0530

    String Reverse

---
+++ Program's_Contributed_By_Contributors/Python_Programs/stringreverse.py
@@ -0,0 +1,8 @@
+str="abcdefghijklMNopqrstuvwxyz"
+print(str[0:25:])
+print(str[::-1])
+# not working print(str[-1:20:])
+print(str[1:10:2])
+print (str[::-2])
+print(str[-1:-20])
+print(str[25::-1])

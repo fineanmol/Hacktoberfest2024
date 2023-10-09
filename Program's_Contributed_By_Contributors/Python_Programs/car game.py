ff7bd1990a5a4d5cb21374271b4df524c01d79d1
zedTeam21 <103349442+zedTeam21@users.noreply.github.com>
2023-10-09 13:03:34 +0530

    Update car game.py

--- Program's_Contributed_By_Contributors/Python_Programs/car game.py
+++ Program's_Contributed_By_Contributors/Python_Programs/car game.py
@@ -1,47 +1,47 @@
-"""
------------------
-Boolean Variables
------------------
-"""
+# Initialize boolean variables to track game state
 game_opened_once = False
 car_started = False
 
+# Start an infinite loop for the game
 while True:
-
-    # Checks whether the game has already been opened
-
+    # Check if the game has been opened for the first time
     if not game_opened_once:
         command = input("Enter command (type help to get help): ").lower()
         game_opened_once = True
     else:
         command = input("Enter command: ").lower()
 
-    # Start command block
+    # Process user commands
     if command == "start":
+        # Check if the car is already started
         if car_started:
             print("Car has already started")
         else:
+            # Start the car and update the state
             print("Car has started")
             car_started = True
 
-    # Stop command block
     elif command == "stop":
+        # Check if the car is already stopped
         if not car_started:
             print("Car has already stopped")
         else:
+            # Stop the car and update the state
             print("Car has stopped")
+            car_started = False
 
-    # Quit command block
     elif command == "quit":
         print("Quitting...")
-        break
+        break  # Exit the loop and end the game
 
-    # Help command block
     elif command == "help":
+        # Display a help menu with available commands
         print("""
 start - starts the car
 stop - stops the car
-quit - quits out of the game 
-
+quit - quits out of the game
 help - brings up this help menu
         """)
+    else:
+        # Handle invalid commands
+        print("Invalid command. Type 'help' for available commands.")

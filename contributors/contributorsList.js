74932988e51f323442209fd8028e4161d9e760c4
IRBR-41379 <sayandeep369@gmail.com>
2023-10-10 21:06:45 +0530

    Added jupytor directory and a Perceptron Learning program

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Sayandeep Dey",
+    username: "https://github.com/IBR-41379",
+  },
 ];

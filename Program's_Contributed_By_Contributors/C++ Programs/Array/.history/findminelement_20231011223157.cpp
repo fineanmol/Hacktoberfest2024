c1d3a2b46dca676743011ee9f7ad9b8017b90385
Ayansaxena24 <ayan.saxena24@gmail.com>
2023-10-11 22:35:38 +0530

    Added Info and Code. Please accept for HacktoberFest 2023

---
+++ Program's_Contributed_By_Contributors/C++ Programs/Array/.history/findminelement_20231011223157.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int t;
+  cin>>t;
+  while (t--)
+  {
+     int a,b,c,i,sum1,sum2;
+     cin>>a>>b>>c;
+  }
+}
---
+++ Program's_Contributed_By_Contributors/C++ Programs/Array/.history/findminelement_20231011223330.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int t;
+  cin>>t;
+  while (t--)
+  {
+     int a,b,c,i,n;
+     cin>>n;
+     int arr[n];
+     for(i=0;i<n;i++)
+     {
+       cin>>arr[i];
+     }
+     sort(arr,arr+n);
+     cout<<arr[0]<<endl;
+  }
+  
+}

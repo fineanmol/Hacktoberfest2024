0e306759bf29eca281339856db9372b7b11bb902
Aditya Shrimali <shrimaliaditya013@gmail.com>
2023-10-12 12:56:38 +0530

    Changed a cpp buyandsell code to take user input

--- Program's_Contributed_By_Contributors/C++ Programs/Array/buyandsell.cpp
+++ Program's_Contributed_By_Contributors/C++ Programs/Array/buyandsell.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 int main()
 {
-	vector<int> price = {7,1,5,3,6,4};
+	int n;
+	cin>>;
+	vector<int> price(n);
+	for(int i=0;i<n;i++){
+		cin>>price[i];
+	}
 	int mn =INT_MAX;
 	int profit = 0;
 	for(int i=0;i<price.size();i++)

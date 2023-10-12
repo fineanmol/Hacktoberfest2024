f480b6353d406e49d84677fc02269073d610962e
Rishu Raj <133753604+rishuraj1708@users.noreply.github.com>
2023-10-12 18:54:57 +0530

    Creating Rock-Papper-Scissor-game.py

---
+++ Program's_Contributed_By_Contributors/Python_Programs/Rock-Papper-Scissor-game.py
@@ -0,0 +1,39 @@
+import random
+
+def get_user_choice():
+    while True:
+        user_choice = input("Choose Rock, Paper, or Scissors: ").lower()
+        if user_choice in ["rock", "paper", "scissors"]:
+            return user_choice
+        else:
+            print("Invalid choice. Please choose Rock, Paper, or Scissors.")
+
+def get_computer_choice():
+    choices = ["rock", "paper", "scissors"]
+    return random.choice(choices)
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (
+        (user_choice == "rock" and computer_choice == "scissors")
+        or (user_choice == "paper" and computer_choice == "rock")
+        or (user_choice == "scissors" and computer_choice == "paper")
+    ):
+        return "You win!"
+    else:
+        return "Computer wins!"
+
+while True:
+    user_choice = get_user_choice()
+    computer_choice = get_computer_choice()
+
+    print(f"You chose {user_choice}")
+    print(f"Computer chose {computer_choice}")
+
+    result = determine_winner(user_choice, computer_choice)
+    print(result)
+
+    play_again = input("Play again? (yes/no): ").lower()
+    if play_again != "yes":
+        break

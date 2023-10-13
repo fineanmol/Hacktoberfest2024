f6d967f0424ea301a667c42d85840947ec0a58e2
solo-coder13 <113525473+solo-coder13@users.noreply.github.com>
2023-10-13 20:16:09 +0530

    Create FindLeftRecursion
    c program to find the left recursion in production rules for compiler

---
+++ FindLeftRecursion.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+void main()  {
+    char input[100],l[50],r[50],temp[10],tempprod[20],productions[25][50];
+    int i=0,j=0,flag=0,consumed=0;
+    printf("Enter the productions: ");
+    scanf("%1s->%s",l,r);
+    printf("%s",r);
+    while(sscanf(r+consumed,"%[^|]s",temp) == 1 && consumed <= strlen(r))  {
+        if(temp[0] == l[0])  {
+            flag = 1;
+            sprintf(productions[i++],"%s->%s%s'\0",l,temp+1,l);
+        }
+        else
+            sprintf(productions[i++],"%s'->%s%s'\0",l,temp,l);
+        consumed += strlen(temp)+1;
+    }
+    if(flag == 1)  {
+        sprintf(productions[i++],"%s->ε\0",l);
+        printf("The productions after eliminating Left Recursion are:\n");
+        for(j=0;j<i;j++)
+            printf("%s\n",productions[j]);
+    }
+    else
+        printf("The Given Grammar has no Left Recursion");
+}

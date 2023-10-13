2edf498a0a9b8244fd6f7446530fb2650a145d34
Adwaith Sajeev C <adwaithsajeev64@gmail.com>
2023-10-13 21:36:08 +0530

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,10 +1,19 @@
+# Define the quicksort function that sorts a list using the quicksort algorithm
 def quicksort(arr):
+    # Base case: If the list has 1 or fewer elements, it's already sorted
     if len(arr) <= 1:
         return arr
     else:
+        # Choose the first element as the pivot
         pivot = arr[0]
+        
+        # Create two sublists: one for elements less than or equal to the pivot,
+        # and one for elements greater than the pivot
         less = [x for x in arr[1:] if x <= pivot]
         greater = [x for x in arr[1:] if x > pivot]
+        
+        # Recursively call quicksort on the two sublists, and concatenate them
+        # with the pivot to return the sorted list
         return quicksort(less) + [pivot] + quicksort(greater)
 
 # Take user input as a comma-separated list of integers

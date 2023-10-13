fe71efdf3d659ff4e5afc03d4beb5a28724a2ca3
Prince Kumar Raj <66669293+Princekumaraj@users.noreply.github.com>
2023-10-13 11:50:47 +0530

    Create Mergesort.java

---
+++ Program's_Contributed_By_Contributors/Java_Programs/Sorting/Mergesort.java
@@ -0,0 +1,47 @@
+public class MergeSort {
+
+    public static void mergeSort(int[] arr) {
+        if (arr.length <= 1) return;
+
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+
+        System.arraycopy(arr, 0, left, 0, mid);
+        System.arraycopy(arr, mid, right, 0, arr.length - mid);
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(arr, left, right);
+    }
+
+    public static void merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            arr[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            arr[k++] = right[j++];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 6, 8, 10, 1, 2, 1};
+        mergeSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}

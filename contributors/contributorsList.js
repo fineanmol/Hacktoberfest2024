6fd7727c1112b0ff0b86374e539e8a59923e4005
Iskandar Sholeh <iskandar.sholeh22@gmail.com>
2023-10-13 21:41:22 +0700

    change the color to pink

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Iskandar Sholeh",
+    username: "https://github.com/iskandarsh",
+  },
 ];

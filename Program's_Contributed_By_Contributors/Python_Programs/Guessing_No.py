97db1529c9863d2d6f1512da46a31b4beb3cddd5
vVIBHORj <137258447+vVIBHORj@users.noreply.github.com>
2023-10-14 11:53:25 +0530

    Update Guessing_No.py
    new change

--- Program's_Contributed_By_Contributors/Python_Programs/Guessing_No.py
+++ Program's_Contributed_By_Contributors/Python_Programs/Guessing_No.py
@@ -1,18 +1,18 @@
 # A python program for fun enjoyment where you need to guess a no using hints.
 
 import random
-randNumber = random.randint(1,100)
+random_number = random.randint(1,100)
 userGuess = None
 guesses = 0
-while(userGuess!=randNumber):
+while(userGuess!=random_number):
     userGuess = int(input("Enter your guess between(1 to 100):"))
     if userGuess>100 or userGuess<1:
         print("Enter Valid Number")
     guesses+=1
-    if(userGuess == randNumber):
+    if(userGuess == random_number):
         print("You Guessed it right!!")
     else:
-        if(userGuess>randNumber):
+        if(userGuess>random_number):
             print("You Guessed it worng!! Enter a smaller number")
         else:
             print("You Guessed it wrong!! Enter a larger number")

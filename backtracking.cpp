b9e7347fe16a1b1f990318e0119e7773cea22980
jayrathod2003 <133162811+jayrathod2003@users.noreply.github.com>
2023-10-15 00:23:17 +0530

    Create backtracking.cpp
    This Program is very helpful when your going to solve N Queens Backtracking Problems in Dynamic programing

---
+++ backtracking.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isSafe(vector<vector<int>>& board, int row, int col, int N) {
+    // Check if there is a queen in the same column
+    for (int i = 0; i < row; i++) {
+        if (board[i][col] == 1) {
+            return false;
+        }
+    }
+
+    // Check upper left diagonal
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+        if (board[i][j] == 1) {
+            return false;
+        }
+    }
+
+    // Check upper right diagonal
+    for (int i = row, j = col; i >= 0 && j < N; i--, j++) {
+        if (board[i][j] == 1) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool solveNQueensUtil(vector<vector<int>>& board, int row, int N) {
+    if (row == N) {
+        // All queens are placed, solution found
+        return true;
+    }
+
+    for (int col = 0; col < N; col++) {
+        if (isSafe(board, row, col, N)) {
+            board[row][col] = 1; // Place the queen
+
+            if (solveNQueensUtil(board, row + 1, N)) {
+                return true;
+            }
+
+            board[row][col] = 0; // Backtrack if no solution found
+        }
+    }
+
+    return false;
+}
+
+void solveNQueens(int N) {
+    vector<vector<int>> board(N, vector<int>(N, 0));
+
+    if (solveNQueensUtil(board, 0, N)) {
+        // Print the solution
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                cout << board[i][j] << " ";
+            }
+            cout << endl;
+        }
+    } else {
+        cout << "No solution exists." << endl;
+    }
+}
+
+int main() {
+    int N;
+    cout << "Enter the number of queens (N): ";
+    cin >> N;
+    solveNQueens(N);
+    return 0;
+}

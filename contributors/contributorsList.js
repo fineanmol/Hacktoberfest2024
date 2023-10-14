cead8d4d08cdefe3b0c3e6df50e9a748b20c0463
Aditya Harsh <114324933+aditya13jun@users.noreply.github.com>
2023-10-14 11:02:39 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Aditya Harsh",
+    username: "https://github.com/aditya13jun",
+  },
 ];

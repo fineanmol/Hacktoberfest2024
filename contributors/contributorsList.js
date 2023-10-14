7122663836941a36fdd07cde18c5ee09c6f73c79
Ratul Roy <ratul.arya.roy@gmail.com>
2023-10-15 04:20:13 +0530

    Edited Binary Search

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Ratl Roy",
+    username: "https://github.com/royal-programmer",
+  },
 ];

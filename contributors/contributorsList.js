ae0b96883266b5453fe410ab7c909375f6f02670
FebrianS-putra <gokil.febrian45@gmail.com>
2023-10-14 17:37:23 +0700

    update contributorsList.js and footer.css

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Febrians Putra",
+    username: "https://github.com/FebrianS-putra",
+  },
 ];

9f22e1241d5fcea931c995aeb0f74834923a0603
PranshuBasak <pranshubasak@gmail.com>
2023-10-15 00:11:33 +0530

    Update Javascript folder & contributorsList

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Pranshu Basak",
+    username: "https://github.com/PranshuBasak",
+  },
 ];

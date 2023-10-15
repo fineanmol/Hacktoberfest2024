e9967a65e5cdc4c2b8c9520c46eec75a90cfc192
DR34M14 <89645994+DR34M14@users.noreply.github.com>
2023-10-15 02:23:05 -0400

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "DR34M14",
+    username: "https://github.com/DR34M14",
+  }
 ];

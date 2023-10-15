9828ea3aaec8bffb3d92b85c540c24cc0c040f16
Tharakadhanushka <55351120+Tharakadhanushka@users.noreply.github.com>
2023-10-15 10:13:52 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Tharaka Dhanushka",
+    username: "https://github.com/tharakadhanushka",
+  },
 ];

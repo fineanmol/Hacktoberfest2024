e1204aa739ef26722852a54f2e56adf3e3ede9fc
Atheke <jothharrison@gmail.com>
2023-10-15 22:31:19 +0530

    added profile to contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Jonan Puro",
+    username: "https://github.com/Atheke",
+  },
 ];

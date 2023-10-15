b8265f2d5c47d4ba18b75e61a9e1940f61f5ed76
ohohoh456 <97571395+ohohoh456@users.noreply.github.com>
2023-10-15 00:19:22 -0700

    Create Multiplication_program.py
    Program multiplies 2 numbers that the user gives

---
+++ Program's_Contributed_By_Contributors/Python_Programs/Multiplication_program.py
@@ -0,0 +1,10 @@
+print("-Multiplication Calulator-")
+def product(a, b):
+    return (a * b)
+
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'product of {a} and {b} is {product(a, b)}')
+print("--------------------------")

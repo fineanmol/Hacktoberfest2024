bf8d6e9a5c5c0d717b3a79f4d6e549e9f714d878
Harsh Tripathi <108024082+alpharsh@users.noreply.github.com>
2023-10-15 23:11:36 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Harsh Tripathi",
+    username: "https://github.com/alpharsh",
+  },
 ];

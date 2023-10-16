d814adf2657855f9f30f1c469886fd23224a02a7
Ahmed Raza <ahmed.raza1054@gmail.com>
2023-10-17 04:28:40 +0500

    Hactoberfest2023

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Ahmed raza",
+    username: "https://github.com/ahmedrazabaloch",
+  },
 ];

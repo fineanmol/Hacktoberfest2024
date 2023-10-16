7dcd46c344e1fe627b3934daa6d3c9a72c2668b0
YashKesh <89805020+YashKesh@users.noreply.github.com>
2023-10-16 23:41:49 +0530

    added the code of best time to buy and sell stock

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Yash Kesharwani",
+    username: "https://github.com/YashKesh",
+  },
 ];

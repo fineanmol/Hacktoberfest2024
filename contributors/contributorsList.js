7285175952870931987966781f5a2499025e233a
SAHIL HOSSAIN <111786720+Sahil1786@users.noreply.github.com>
2023-10-16 22:06:03 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+    {
+    id: 426,
+    fullname: "Sahil Hoosain.",
+    username: "https://github.com/Sahil1786",
+  },
 ];

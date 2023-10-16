6a1c0b52e2e413866c7d8547bb361a519f91f910
Rudreshwar Baranwal <baranwalrudreshwar@gmail.com>
2023-10-16 19:02:16 +0530

    added names

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Rudreshwar Baranwal",
+    username: "https://github.com/devrudra9",
+  },
 ];

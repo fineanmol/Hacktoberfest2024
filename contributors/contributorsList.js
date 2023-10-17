3d2fcb1b2ed549b221c8bd445ffb1e4897cb872a
daulatojha17 <117494705+daulatojha17@users.noreply.github.com>
2023-10-17 21:47:47 +0530

    Update contributorsList.js
    Updated Contributors

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Daulat Ojha",
+    username: "https://github.com/daulatojha17",
+  },
 ];

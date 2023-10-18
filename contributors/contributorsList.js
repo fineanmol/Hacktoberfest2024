b729b8037fd2f2e26cd8e20901188468337e9f38
harshv147 <barveharsh18@gmail.com>
2023-10-18 11:11:29 +0530

    Updated contributors list and added solution of asteroid collision problem from leetcode in c++

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Harsh Barve",
+    username: "https://github.com/harshv147",
+  },
 ];

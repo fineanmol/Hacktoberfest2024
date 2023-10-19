8888fbb0e44095dc6767de08cc8ee2a2d3d5a7a0
KarenB <karenabourgeois@gmail.com>
2023-10-19 15:44:41 -0300

    Adding my name to contributors

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Karen Bourgeois",
+    username: "https://github.com/kzefram",
+  },
 ];

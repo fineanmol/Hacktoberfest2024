77c0d4d2c1ea71fd00e4b5f51570dec55d1ca7a3
TANISHA <115376761+tanisha10101@users.noreply.github.com>
2023-10-20 17:31:00 +0530

    Create stacks_using_arrays.cpp

---
+++ stacks_using_arrays.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#define N 5
+using namespace std; 
+
+//STACK IMPLEMENTATION USING ARRAYS (STATIC IMPLEMENTATION)
+
+int stack[N]; 
+int top = -1; //top of the stack 
+
+
+
+//PUSH
+
+void Push(){
+
+    int x; 
+    cout << "enter the number that u want to push" << endl; 
+    cin >> x; 
+
+    if(top == N-1){
+        cout << "stack is full that means overflow" <<endl; 
+
+    } else {
+        
+        top ++; 
+        stack[top] = x; 
+        cout << "current stack: " << stack[top] << endl; 
+        
+    }
+
+
+}
+
+
+
+//POP
+
+void Pop(){
+    if(top == -1){
+        cout << "stack is empty or it is underflow" << endl; 
+
+    }
+    else {
+        cout << "Now, current stack is: " <<endl; 
+        int item = stack[top];  // to store your deleted value
+        top --;
+        cout << "deleted item: " <<item << endl;  
+        cout << "current stack: " << stack[top] <<endl;
+    }
+
+
+}
+
+
+
+//PEEK
+
+void Peek(){
+    if(top == -1){
+        cout << "stack is empty or it is underflow" << endl; 
+    }
+
+    else {
+        cout << stack[top] << endl; 
+  
+    }
+}
+
+
+
+//DISPLAY
+
+void display(){
+    for(int i = top; i>=0; i--){
+        cout << "here is ur stack: " << stack[i] << endl;
+    }
+}
+
+
+
+//SWITCH STATEMENTS 
+
+int main(){
+
+    int number; 
+
+    cout << "enter number" <<endl; 
+    cin >> number; 
+
+    switch(number){
+
+        case 1: Push(); 
+        break; 
+
+        case 2: 
+        { 
+            int num; 
+            cout << "enter another number" <<endl; 
+            cin >> num; 
+
+            switch(num){
+                case 1: Push();
+                Pop(); 
+                break;
+
+                case 2: Pop(); 
+                break; 
+
+                default: 
+                break; 
+
+            }
+            
+        }
+        break;
+        
+        case 3: Peek(); 
+        break;
+
+        case 4: display();
+        break; 
+
+        default: 
+        cout << "wrong no. pls enter valid no. " <<endl; 
+        break; 
+
+
+    }
+
+    return 0; 
+
+   }

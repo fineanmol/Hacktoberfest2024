a6f85de1a29aae03c72455c2b5d64b9c6d72f5a5
root <Syariful Mujaddiq>
2023-10-21 03:30:05 +0800

    Add Syariful Mujaddiq to Contributors list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Syariful Mujaddiq",
+    username: "https://github.com/syarifulmujaddiq",
+  },
 ];

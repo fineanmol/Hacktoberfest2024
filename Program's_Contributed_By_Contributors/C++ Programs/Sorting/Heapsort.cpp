9fa7db1d276afd09b9dd07748382ef2b213278ff
priyanka-deshmukh8 <priyankadeshmukh.h@gmail.com>
2023-10-21 11:51:00 +0530

    Added Heapsort

---
+++ Program's_Contributed_By_Contributors/C++ Programs/Sorting/Heapsort.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+// Function to heapify a subtree rooted with node i, where n is the size of the heap
+void heapify(int arr[], int n, int i) {
+    int largest = i; // Initialize largest as the root
+    int left = 2 * i + 1; // Left child
+    int right = 2 * i + 2; // Right child
+
+    // If the left child is larger than the root
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    // If the right child is larger than the largest so far
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    // If the largest is not the root
+    if (largest != i) {
+        std::swap(arr[i], arr[largest]);
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+// The main function to perform Heap Sort
+void heapSort(int arr[], int n) {
+    // Build a max heap
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    // Extract elements from the heap one by one
+    for (int i = n - 1; i > 0; i--) {
+        // Move the current root to the end
+        std::swap(arr[0], arr[i]);
+
+        // Call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Original array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    heapSort(arr, n);
+
+    std::cout << "\nSorted array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+}

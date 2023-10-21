1e74c05a8efe4df9fbce17c15855efb7b58c0bce
abdrrahmenz <abdrrahmenz@gmail.com>
2023-10-21 08:34:51 +0700

    contribute hacktoberfest

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Abdurrahman",
+    username: "https://github.com/abdrrahmenz",
+  },
 ];

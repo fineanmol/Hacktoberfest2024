82cee44c5030269a2a034906c9c6149b692fa4b2
Karthick Thoppe <karthick.thoppe@bearingpoint.com>
2023-10-22 20:42:11 +0100

    Contrib update

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Karthick Thoppe",
+    username: "https://github.com/karthicktv",
+  },
 ];

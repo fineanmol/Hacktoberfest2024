0379044dda70cbeb7eb2bbce92a50da8eb9b51f2
Raksha703 <85754170+Raksha703@users.noreply.github.com>
2023-10-22 22:38:34 +0530

    Create QuickSort.cpp

---
+++ Program's_Contributed_By_Contributors/C++ Programs/QuickSort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quicksort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    vector<int> arr = {10, 7, 8, 9, 1, 5};
+    int n = arr.size();
+    quicksort(arr, 0, n - 1);
+    cout << "Sorted array is: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}

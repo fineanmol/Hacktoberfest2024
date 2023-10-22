d0ab2c9cc5024989037c27cb572f6aec0bf31394
sak-90 <sakrox90@gmail.com>
2023-10-22 13:15:02 +0530

    Added a new standard greedy problem known as josephus problem, fix

--- Program's_Contributed_By_Contributors/C++ Programs/greedy/josephusProblem.cpp
+++ Program's_Contributed_By_Contributors/C++ Programs/greedy/josephusProblem.cpp
@@ -1,8 +1,3 @@
-/*
-Problem statement: There are N people standing in a circle waiting to be executed. The counting out begins at some point in the circle and proceeds around the circle in a fixed direction. In each step, a certain number of people are skipped and the next person is executed. The elimination proceeds around the circle (which is becoming smaller and smaller as the executed people are removed), until only the last person remains, who is given freedom.
-Given the total number of persons N and a number k which indicates that k-1 persons are skipped and the kth person is killed in a circle. The task is to choose the person in the initial circle that survives.
-*/
-
 #include <bits/stdc++.h>
 using namespace std;
 

953fb0ef8040f8800e774797849b202a5b458208
Himanshi Rana <himanshirana2403@gmail.com>
2023-10-23 15:26:29 +0530

    Contribution Himanshi Rana

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Himanshi Rana",
+    username: "https://github.com/himanshirana2403",
+  },
 ];

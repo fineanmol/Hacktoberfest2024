6ef64cb7ec9760d97cb053d3e0187f27b9cec7f2
runtimeerror11 <suparshwapatil28@gmail.com>
2023-10-24 02:01:12 +0530

    Updated the Contributors list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Suparshwa Patil",
+    username: "https://github.com/runtimeerror11",
+  }
 ];

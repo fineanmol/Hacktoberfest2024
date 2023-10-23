a8f44b29c57f928f143d7ebf40e7d2afa0b7830d
Nikunj Khinchi <115391420+Nikunj-Khinchi@users.noreply.github.com>
2023-10-23 15:41:37 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Nikunj Khinchi",
+    username: "https://github.com/Nikunj-Khinchi"
+  },
 ];

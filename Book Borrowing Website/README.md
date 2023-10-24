c4d08822b3b4abc5636b234c8e7b1b1845d3241e
Amrita Kesh <74363370+amritakesh@users.noreply.github.com>
2023-10-24 17:36:17 +0530

    Add files via upload
    Added a book borrowing website and a mediation app

---
+++ Book Borrowing Website/README.md
@@ -0,0 +1,18 @@
+# SHEHACKS_TEAM-101
+SHARE-MY-BOOK 
+(CREATED IN SHE HACKS HACKATHON 2021 HELD ON 13-14 MARCH)[Presentation 1.pdf](https://github.com/amritakesh/SHEHACKS_TEAM-101/files/6136346/Presentation.1.pdf)
+
+![Screenshot from 2021-03-14 08-16-46](https://user-images.githubusercontent.com/74363370/111333569-d1ec3000-8698-11eb-8773-adb4e149d4e2.png)
+
+![Screenshot from 2021-03-14 08-17-17](https://user-images.githubusercontent.com/74363370/111333602-dadd0180-8698-11eb-9226-aa08b12a6fd1.png)
+
+![Screenshot from 2021-03-14 08-17-00](https://user-images.githubusercontent.com/74363370/111333630-df091f00-8698-11eb-964e-f9535ea48763.png)
+
+
+
+An online platform created by students for students. It provides readers with books they can rent for a stipulated period of time, Buy pre-owned books and even donate their own books.
+
+This Front-end webpage was created using
+1. HTML
+2. CSS
+3. JAVASCRIPT
---
+++ Book Borrowing Website/app.js
@@ -0,0 +1,82 @@
+const song = document.querySelector(".song");
+const play = document.querySelector(".play");
+const replay = document.querySelector(".replay");
+const outline = document.querySelector(".moving-outline circle");
+const video = document.querySelector(".vid-container video");
+//Sounds
+const sounds = document.querySelectorAll(".sound-picker button");
+//Time Display
+const timeDisplay = document.querySelector(".time-display");
+const outlineLength = outline.getTotalLength();
+//Duration
+const timeSelect = document.querySelectorAll(".time-select button");
+let fakeDuration = 600;
+
+outline.style.strokeDashoffset = outlineLength;
+outline.style.strokeDasharray = outlineLength;
+timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+  fakeDuration % 60
+)}`;
+
+sounds.forEach(sound => {
+  sound.addEventListener("click", function() {
+    song.src = this.getAttribute("data-sound");
+    video.src = this.getAttribute("data-video");
+    checkPlaying(song);
+  });
+});
+
+play.addEventListener("click", function() {
+  checkPlaying(song);
+});
+
+replay.addEventListener("click", function() {
+    restartSong(song);
+    
+  });
+
+
+const restartSong = song =>{
+    let currentTime = song.currentTime;
+    song.currentTime = 0;
+    console.log("ciao")
+
+}
+
+timeSelect.forEach(option => {
+  option.addEventListener("click", function() {
+    fakeDuration = this.getAttribute("data-time");
+    timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+      fakeDuration % 60
+    )}`;
+  });
+});
+
+const checkPlaying = song => {
+  if (song.paused) {
+    song.play();
+    video.play();
+    play.src = "./svg/pause.svg";
+  } else {
+    song.pause();
+    video.pause();
+    play.src = "./svg/play.svg";
+  }
+};
+
+song.ontimeupdate = function() {
+  let currentTime = song.currentTime;
+  let elapsed = fakeDuration - currentTime;
+  let seconds = Math.floor(elapsed % 60);
+  let minutes = Math.floor(elapsed / 60);
+  timeDisplay.textContent = `${minutes}:${seconds}`;
+  let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+  outline.style.strokeDashoffset = progress;
+
+  if (currentTime >= fakeDuration) {
+    song.pause();
+    song.currentTime = 0;
+    play.src = "./svg/play.svg";
+    video.pause();
+  }
+};

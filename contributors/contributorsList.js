42a57da785c7cb79a4a8f4e471679ca901746d64
Silvan Ferreira <sjrmiranda@gmail.com>
2023-10-24 09:48:19 -0300

    Add contributor

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 425,
+    fullname: "Silvan F.",
+    username: "https://github.com/silvaan",
+  },
 ];

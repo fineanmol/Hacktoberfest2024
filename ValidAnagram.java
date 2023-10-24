c29d965ed01a7e45f2801fcc891a081ab6604086
kalaSriSaiRaksha <103526228+kalaSriSaiRaksha@users.noreply.github.com>
2023-10-24 13:26:42 +0530

    Create ValidAnagram.java

---
+++ ValidAnagram.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        return Arrays.equals(sArray, tArray);
+    }
+    
+    public static void main(String[] args) {
+        String s1 = "listen";
+        String s2 = "silent";
+        
+        if (isAnagram(s1, s2)) {
+            System.out.println(s1 + " and " + s2 + " are anagrams.");
+        } else {
+            System.out.println(s1 + " and " + s2 + " are not anagrams.");
+        }
+    }
+}

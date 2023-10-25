c6f1af2cc8614dca2e4e4b3d2e454778bef84884
Anushthan Saxena <anushthans@gmail.com>
2023-10-25 14:20:31 +0530

    hacktoberfest 2023 contribution

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Anushthan Saxena",
+    username: "https://github.com/AnushthanS"
+  },
 ];

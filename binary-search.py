9d34f6aca2b7e318bad1bd441d6b327895ab1132
Sanket Nikam <sanketn734@gmail.com>
2023-10-25 17:03:16 +0530

    Improve Compatibility, Format, Test, Output

--- binary-search.py
+++ binary-search.py
@@ -1,39 +1,34 @@
-# Python 3 program for recursive binary search.
-# Modifications needed for the older Python 2 are found in comments.
+# Python 2/3 program for recursive binary search.
+
+# Importing division module from future to make it compatible with Python 2 and 3
+from __future__ import division
 
 # Returns index of x in arr if present, else -1
 def binary_search(arr, low, high, x):
-
-	# Check base case
-	if high >= low:
-
-		mid = (high + low) // 2
-
-		# If element is present at the middle itself
-		if arr[mid] == x:
-			return mid
-
-		# If element is smaller than mid, then it can only
-		# be present in left subarray
-		elif arr[mid] > x:
-			return binary_search(arr, low, mid - 1, x)
-
-		# Else the element can only be present in right subarray
-		else:
-			return binary_search(arr, mid + 1, high, x)
-
-	else:
-		# Element is not present in the array
-		return -1
+    # Check base case
+    if high >= low:
+        mid = (high + low) // 2
+        # If element is present at the middle itself
+        if arr[mid] == x:
+            return mid
+        # If element is smaller than mid, then it can only be present in the left subarray
+        elif arr[mid] > x:
+            return binary_search(arr, low, mid - 1, x)
+        # Else the element can only be present in the right subarray
+        else:
+            return binary_search(arr, mid + 1, high, x)
+    else:
+        # Element is not present in the array
+        return -1
 
 # Test array
-arr = [ 2, 3, 4, 10, 40 ]
+arr = [2, 3, 4, 10, 40]
 x = 10
 
 # Function call
-result = binary_search(arr, 0, len(arr)-1, x)
+result = binary_search(arr, 0, len(arr) - 1, x)
 
 if result != -1:
-	print("Element is present at index", str(result))
+    print("Element is present at index", str(result))
 else:
-	print("Element is not present in array")
+    print("Element is not present in the array")

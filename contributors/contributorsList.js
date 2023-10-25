eb8482a8deb34085b806736c16970b936fc107b0
Alok Kumar Verma <as1492@srmist.edu.in>
2023-10-25 23:29:06 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+    {
+    id: 426,
+    fullname: "Alok Verma",
+    username: "https://github.com/alokverma18",
+  },
 ];

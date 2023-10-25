41ab5b87cb6d7d39cc6350fc7929086be8a34a11
Abhishek Anand <76611051+abhianand0110@users.noreply.github.com>
2023-10-26 01:35:35 +0530

    Add files via upload

---
+++ quiz app/end.js
@@ -0,0 +1,25 @@
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+const finalScore = document.getElementById("finalScore");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener("keyup", () => {
+  saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = (e) => {
+  e.preventDefault();
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("index.html");
+};
---
+++ quiz app/highscores.js
@@ -0,0 +1,8 @@
+const highScoresList = document.getElementById("highScoresList");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+highScoresList.innerHTML = highScores
+  .map((score) => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  .join("");

3a1beef7343bef5beb9ef6f526ea3545b67f543d
Ayush Morbar <108860844+ayushmorbar@users.noreply.github.com>
2023-10-26 21:11:27 -0700

    Update contributorsList.js
    Added myself as Contributor

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Ayush Morbar",
+    username: "https://github.com/ayushmorbar/",
+  },
 ];

d354ce024926d0a82f6eadcf00db0966ddc8006b
Ananay <73402363+FTS18@users.noreply.github.com>
2023-10-27 18:56:04 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Ananay Dubey",
+    username: "https://github.com/fts18",
+  },
 ];

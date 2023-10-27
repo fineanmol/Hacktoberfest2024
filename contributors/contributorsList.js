8a4ad9a9018c2ddac65c94154ce3ae8428db8afe
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2023-10-27 21:17:50 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Abhishek Vedant",
+    username: "https://github.com/Webby1015",
+  },
 ];

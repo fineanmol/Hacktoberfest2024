71b871741e0f067893fb51fce6a9947c3081ee3f
Luis A.Tavarez <51054204+uppy19d0@users.noreply.github.com>
2023-10-28 01:12:33 -0400

    Update scroll.js
    The improved code enhances maintainability and flexibility by using function parameters for scrollTrigger and scrollDuration, allowing for easy reuse with different elements. It includes comments for clarity, organizes the code better, and avoids global variables. This refactoring improves code quality and usability.

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,45 +1,56 @@
-window.onscroll = scrollFunc;
-var scrollButton = document.getElementById('backToTop')
+// Global variables or default values
+const scrollButton = document.getElementById('backToTop') || window;
+const scrollTrigger = 150; // Default value
+const scrollDuration = 1250; // Default value
+
+scrollButton.onscroll = scrollFunc;
 
 function scrollFunc() {
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-        scrollButton.style.display = "block"
+    if (scrollButton.scrollTop > scrollTrigger) {
+        scrollButton.style.display = "block";
     } else {
-        scrollButton.style.display = "none"
+        scrollButton.style.display = "none";
     }
 }
-scrollButton.onclick = function() {
-    scrollButton.classList.add('startscrolling')
-    scrollTo(document.documentElement, 0, 1250, function() {
-        scrollButton.classList.remove('startscrolling')
+
+scrollButton.onclick = function () {
+    scrollButton.classList.add('startscrolling');
+    scrollTo(scrollButton, 0, scrollDuration, function () {
+        scrollButton.classList.remove('startscrolling');
     });
-}
-scrollButton.onmousedown = function() {
-    scrollButton.classList.add("mousedown")
-}
-scrollButton.onmouseup = function(){
-    scrollButton.classList.remove("mousedown")
-}
+};
+
+scrollButton.onmousedown = function () {
+    scrollButton.classList.add("mousedown");
+};
+
+scrollButton.onmouseup = function () {
+    scrollButton.classList.remove("mousedown");
+};
+
 function scrollTo(element, to, duration, callback) {
-    var start = element.scrollTop,
+    const start = element.scrollTop,
         change = (to - start),
         currentTime = 0,
-        increment = 20
-    var animateScroll = function() {
-        currentTime += increment
-        var val = Math.easeInOutQuad(currentTime, start, change, duration)
-        element.scrollTop = val
+        increment = 20;
+
+    const animateScroll = function () {
+        currentTime += increment;
+        const val = Math.easeInOutQuad(currentTime, start, change, duration);
+        element.scrollTop = val;
         if (currentTime < duration) {
-            setTimeout(animateScroll,increment)
-        } else if(callback && typeof callback === 'function'){
-            callback()
+            setTimeout(animateScroll, increment);
+        } else if (callback && typeof callback === 'function') {
+            callback();
         }
-    }
-    animateScroll()
+    };
+
+    animateScroll();
 }
-Math.easeInOutQuad = function(t, b, c, d) {
-    t /= d/2
-    if (t < 1) return (c/2*t*t) + b
-    t--
-    return -c/2 * (t*(t-2) - 1) + b
-}
+
+Math.easeInOutQuad = function (t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2 * t * t) + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+};

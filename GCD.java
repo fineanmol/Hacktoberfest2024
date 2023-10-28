24143008ed63f67b250a182c6c07b8908b40f3a2
Rishabh Raj Kumar <108469021+Rishabh-raj-kumar@users.noreply.github.com>
2023-10-28 17:26:20 +0530

    Create GCD.java

---
+++ GCD.java
@@ -0,0 +1,39 @@
+// Java program to compute GCD of two numbers using general approach.
+import java.io.*;
+
+class GCD {
+
+	// gcd() method, returns the GCD of a and b
+	static int gcd(int a, int b)
+	{
+		// stores minimum(a, b)
+		int i;
+		if (a < b)
+			i = a;
+		else
+			i = b;
+
+		// take a loop iterating through smaller number to 1
+		for (i = i; i > 1; i--) {
+
+			// check if the current value of i divides both
+			// numbers with remainder 0 if yes, then i is
+			// the GCD of a and b
+			if (a % i == 0 && b % i == 0)
+				return i;
+		}
+
+		// if there are no common factors for a and b other
+		// than 1, then GCD of a and b is 1
+		return 1;
+	}
+	// Driver method
+	public static void main(String[] args)
+	{
+		int a = 30, b = 20;
+
+		// calling gcd() method over
+		// the integers 30 and 20
+		System.out.println("GCD = " + gcd(b, a));
+	}
+}

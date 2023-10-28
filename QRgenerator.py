e9cf31253a0f521b7d788687f2a3f07d89369892
himanshugupta11002 <72141497+himanshugupta11002@users.noreply.github.com>
2023-10-29 03:05:29 +0530

    Update QRgenerator.py

--- QRgenerator.py
+++ QRgenerator.py
@@ -1,11 +1,10 @@
 import pyqrcode 
 from pyqrcode import QRCode 
   
-# String which represent the QR code 
+
 s = "https://www.youtube.com/channel/UCeO9hPCfRzqb2yTuAn713Mg"
   
-# Generate QR code 
 url = pyqrcode.create(s) 
   
-# Create and save the png file naming "myqr.png" 
+# Hacktoberfest-accepted
 url.svg("myyoutube.svg", scale = 8)

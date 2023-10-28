d36da839f3253a5dc06b5719efeb1b5d1b89f944
himanshugupta11002 <72141497+himanshugupta11002@users.noreply.github.com>
2023-10-29 02:53:14 +0530

    Create QRgenerator.py

---
+++ QRgenerator.py
@@ -0,0 +1,11 @@
+import pyqrcode 
+from pyqrcode import QRCode 
+  
+# String which represent the QR code 
+s = "https://www.youtube.com/channel/UCeO9hPCfRzqb2yTuAn713Mg"
+  
+# Generate QR code 
+url = pyqrcode.create(s) 
+  
+# Create and save the png file naming "myqr.png" 
+url.svg("myyoutube.svg", scale = 8)

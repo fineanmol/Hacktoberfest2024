bc6e117bdc843c7647127fda71958192c736a8ae
ABay23 <alejandrobay23@gmail.com>
2023-10-29 13:23:06 -0400

    Added classes card and deck

---
+++ Program's_Contributed_By_Contributors/Python_Programs/double_card_game.py
@@ -0,0 +1,63 @@
+from random import shuffle
+
+
+class Card:
+    suits = ["spades",
+             "hearts",
+             "diamonds",
+             "clubs"]
+
+    values = [None, None, "2", "3",
+              "4", "5", "6", "7",
+              "8", "9", "10",
+              "Jack", "Queen",
+              "King", "Ace"]
+
+    def __init__(self, v, s):
+        """suit + value are ints"""
+        self.value = v
+        self.suit = s
+
+    def __lt__(self, c2):
+        if self.value < c2.value:
+            return True
+        if self.value == c2.value:
+            if self.suit < c2.suit:
+                return True
+            else:
+                return False
+        return False
+
+    def __gt__(self, c2):
+        if self.value > c2.value:
+            return True
+        if self.value == c2.value:
+            if self.suit > c2.suit:
+                return True
+            else:
+                return False
+        return False
+
+    def __repr__(self):
+        v = self.values[self.value] +\
+            " of " + \
+            self.suits[self.suit]
+        return v
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for i in range(2, 15):
+            for j in range(4):
+                self.cards\
+                    .append(Card(i,
+                                 j))
+        shuffle(self.cards)
+
+    def rm_card(self):
+        if len(self.cards) == 0:
+            return
+        return self.cards.pop()
+
+

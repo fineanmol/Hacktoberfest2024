c16d613d93a5958844d30fc6c28e439495c0a50c
Aum Patel <73774338+patel-aum@users.noreply.github.com>
2023-10-30 11:29:09 +0530

    Update two-pointer.cpp

--- Program's_Contributed_By_Contributors/C++ Programs/2pointer/two-pointer.cpp
+++ Program's_Contributed_By_Contributors/C++ Programs/2pointer/two-pointer.cpp
@@ -5,7 +5,7 @@ int main() {
   int x = 10;
   int* p1 = &x; // p1 points to x
 
-  int* p2 = p1; // p2 also points to x
+  int* p2 = p1; // p2 also points to the same x
 
   std::cout << "x = " << x << std::endl; 
   std::cout << "*p1 = " << *p1 << std::endl;

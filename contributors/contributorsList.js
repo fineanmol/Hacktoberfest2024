17ae18bde7bcc07af8ada2072ac53ffc796aacf1
Shivansh Dwivedi <55807745+shvuuuu@users.noreply.github.com>
2023-10-31 03:16:09 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Shivansh Dwivedi",
+    username: "https://github.com/shvuuuu",
+  },
 ];

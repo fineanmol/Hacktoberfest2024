7896fc67e6bd334c5c429973d867a24cc6f5ff89
Shivansh Dwivedi <55807745+shvuuuu@users.noreply.github.com>
2023-10-31 15:22:47 +0530

    Create autotyper.py

---
+++ Program's_Contributed_By_Contributors/Python_Programs/autotyper.py
@@ -0,0 +1,11 @@
+# Python Based Auto Typer
+
+# Importing Libraries 
+import pyautogui
+import time
+
+# Setting Sleep Timer
+sleeptime=time.sleep(10)
+
+# Inserting text
+pyautogui.typewrite(""" Place Your Content Here """)

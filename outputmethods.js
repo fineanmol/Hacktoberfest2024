889e1ca20105c37665d477414b79fbfd34251494
Abdul Rehman Riaz <abdulrehmanrk@gmail.com>
2023-10-31 18:46:48 +0000

    outputmethods.js

---
+++ outputmethods.js
@@ -0,0 +1,12 @@
+//Javascript outputmethods using function
+
+function printUsingAllMethods(data) {
+    console.log("Console Log: " + data); // Print to the browser console using console.log()
+  
+   document.write("<p>Document Write: " + data + "</p>");  // Print to the web page using document.write()
+  
+    alert("Alert: " + data); // Display an alert to the user
+  }
+  
+  printUsingAllMethods("Hello, World!"); // Call the function with some data
+  

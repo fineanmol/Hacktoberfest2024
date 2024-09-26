e22f0bbc38328af34fdd1aeddb89918087649c3b
harry pattani <harrypattani15@gmail.com>
2024-09-26 20:08:41 +0530

    mobile style changes with header wrapper

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Harry Pattani",
+    username: "https://github.com/pattaniharry",
+  },
   
 ];

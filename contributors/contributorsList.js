a78ce147113467abceb9a9478877d2103ce79f35
Rajana Vara Rahul <rajanarahul93@gmail.com>
2024-09-27 15:44:28 +0530

    added name to contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,9 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+  {
+    id: 428,
+    fullname: "Vara Rahul Rajana",
+    username: "https://github.com/rajanarahul93",
+  }
 ];

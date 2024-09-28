229e997f29d2eb5ff626ce0615469915282201e1
Pallavi Kathait <kathaitpallavi164@gmail.com>
2024-09-28 21:01:16 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,9 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+    {
+    id: 428,
+    fullname: "Pallavi Kathait",
+    username: "https://github.com/iceybubble",
+  },
 ];

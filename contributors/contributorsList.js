db4d2e3cb2c894778fd7a568b3eeb6a3cc8bb766
Hathaipach <hathaipach@hotmail.com>
2024-09-28 19:36:56 +0700

    Update contributorsList.js
    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,9 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+  {
+    id: 428,
+    fullname: "Hathaipach Iamnetr",
+    username: "https://github.com/Hathaipach",
+  },
 ];

239e2d1a8946df011147bc8a0ffd5ab811512d08
vongrossi <vongrossi@gmail.com>
2024-09-30 06:22:14 -0300

    added me to contributors

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Angelo Grossi",
+    username: "https://github.com/vongrossi",
+  },
   
 ];

704c5ca79b9bda08da312d09880d71d36a6301fd
harshit1142 <harshitpandey98765@gmail.com>
2024-09-30 23:38:19 +0530

    updated contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,11 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Harshit Pandey",
+    username: "https://github.com/harshit1142",
+  },
+
   
 ];

3d4d5b934d5185aa2b125c9ceaf0a03e603ae64c
Dhanush S <95139774+Fandroid745@users.noreply.github.com>
2024-10-01 20:49:41 +0530

    Updated contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,9 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+  {
+  id:428,
+  fullname:"Dhanush S",
+  username: "https://github.com/Fandroid745",
+  },  
 ];

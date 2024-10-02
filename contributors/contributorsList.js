78ea9c731d48d5a87b301ab48be7642a98c0e67f
Mandipthakur <89298449+Mandipthakur@users.noreply.github.com>
2024-10-02 18:54:47 +0545

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,11 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+   {
+    id: 428,
+    fullname: "Mandip Thakur",
+    username: "https://github.com/Mandipthakur",
+  },
+  
   
 ];

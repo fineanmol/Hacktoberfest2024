25551b410285de841c4423d0a992d83754e71c8b
Ansh Dwivedi <anshdhardwivediad@gmail.com>
2024-10-02 12:59:48 +0530

    2sum code

---
+++ Program's_Contributed_By_Contributors/Python_Programs/2sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums, target):
+        num_map = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+
+# Example usage
+if __name__ == "__main__":
+    solution = Solution()
+    nums = [2, 7, 11, 15]
+    target = 9
+    result = solution.twoSum(nums, target)
+    print(f"Indices: {result[0]}, {result[1]}")

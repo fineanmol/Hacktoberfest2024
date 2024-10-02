88dcbe8c07333b4d89ac4b6dc2a6f424f1b6d253
parv nema <115030244+PARVNEMA@users.noreply.github.com>
2024-10-02 08:03:57 +0000

    image added to footer and name added to contributors list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "parv nema",
+    username: "https://github.com/PARVNEMA",
+  },
   
 ];

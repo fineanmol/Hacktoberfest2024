d80e78a2c9446e0a76d2e44cc937e77d5e2fde50
zziyouzhang18 <ziyouzhang719@gmail.com>
2024-10-03 16:25:34 -0700

    Create mad_libs.py
    Mad libs generator for fun in python

---
+++ Program's_Contributed_By_Contributors/mad_libs.py
@@ -0,0 +1,36 @@
+'''
+Mad Libs Generator
+-------------------------------------------------------------
+'''
+
+# Questions for the user to answer
+
+noun = input('Choose a noun: ')
+
+p_noun = input('Choose a plural noun: ')
+
+noun2 = input('Choose a noun: ')
+
+place = input('Name a place: ')
+
+adjective = input('Choose an adjective (Describing word): ')
+
+noun3 = input('Choose a noun: ')
+
+# Print a story from the user input
+
+print('------------------------------------------')
+
+print('Be kind to your', noun, '- footed', p_noun)
+
+print('For a duck may be somebody\'s', noun2, ',')
+
+print('Be kind to your', p_noun, 'in', place)
+
+print('Where the weather is always', adjective, '. \n')
+
+print('You may think that is this the', noun3, ',')
+
+print('Well it is.')
+
+print('------------------------------------------')

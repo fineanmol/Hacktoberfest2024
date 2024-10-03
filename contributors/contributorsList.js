fc62fded31cca3810262d8fe02d44ff1a443c2f6
Elisabeth <elise.doisneau@gmail.com>
2024-10-03 20:06:44 +0200

    Added my github details

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,11 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+
+  {
+    id: 429,
+    fullname: "Elisabeth Erkekoglou",
+    username: "https://github.com/elic4vet",
+  },
   
 ];

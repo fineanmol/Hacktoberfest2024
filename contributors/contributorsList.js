56de2d69f891c44d6ffa22e5a99c20491ac71349
Mayank Kumar Karn <karnmayank07@gmail.com>
2024-10-03 19:40:46 +0530

    added-my-name

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,11 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+
+  {
+    id: 428,
+    fullname: "Mayank Kumar Karn",
+    username: "https://github.com/karnmayank07",
+  },
   
 ];

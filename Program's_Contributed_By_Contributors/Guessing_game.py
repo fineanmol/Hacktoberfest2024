6dacf05afb10451a344a85c5fef3b438aa80c64a
zziyouzhang18 <ziyouzhang719@gmail.com>
2024-10-03 16:36:51 -0700

    Create Guessing_game.py
    simple guessing game, from siddhartha

---
+++ Program's_Contributed_By_Contributors/Guessing_game.py
@@ -0,0 +1,20 @@
+import random
+number = random.randint(1, 10)
+
+player_name = input("Hello, What's your name?")
+number_of_guesses = 0
+print('okay! '+ player_name+ ' I am Guessing a number between 1 and 10:')
+
+while number_of_guesses < 5:
+    guess = int(input())
+    number_of_guesses += 1
+    if guess < number:
+        print('Your guess is too low')
+    if guess > number:
+        print('Your guess is too high')
+    if guess == number:
+        break
+if guess == number:
+    print('You guessed the number in ' + str(number_of_guesses) + ' tries!')
+else:
+    print('You did not guess the number, The number was ' + str(number))

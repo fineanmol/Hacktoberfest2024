d91880849360de4eac14d52f24e83a31d9d4315d
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-10-04 13:29:36 +0700

    added myself as a contributor

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Aung Kaung Myint",
+    username: "https://github.com/q1akm",
+  },
   
 ];

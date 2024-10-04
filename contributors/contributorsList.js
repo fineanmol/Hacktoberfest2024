f485f988e6def2dc614df7f8d1cbeb45f4d970bf
lilpacy <revivedtomorrow@gmail.com>
2024-10-04 21:47:51 +0900

    add contributor

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,9 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+  {
+    id: 427,
+    fullname: "Lil Pacy",
+    username: "https://github.com/lilpacy",
+  },
 ];

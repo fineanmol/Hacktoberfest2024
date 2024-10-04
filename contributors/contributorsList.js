d31d14b05fa4f1ed89495f7f00faeaff6afb3c59
Sonali Kumari <90141326+sonalikari@users.noreply.github.com>
2024-10-04 19:59:54 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,9 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+   {
+    id: 428,
+    fullname: "Sonali Kumari",
+    username: "https://github.com/sonalikari",
+  },
 ];

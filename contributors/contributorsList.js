2d3d155d719cb9f6ac664c1db00b060f692cb25b
Adeel Javed <93484102+adeel-015@users.noreply.github.com>
2024-10-04 18:48:09 +0530

    Updated contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Adeel Javed",
+    username: "https://github.com/adeel-015",
+  },
   
 ];

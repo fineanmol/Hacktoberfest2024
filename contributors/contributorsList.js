1aa4a8c429879c49f78580544df4cf24178402a9
Shovit Nepal <65544518+sobit-nep@users.noreply.github.com>
2024-10-04 19:48:32 +0545

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Shovit Nepal",
+    username: "https://github.com/sobit-nep",
+  },
   
 ];

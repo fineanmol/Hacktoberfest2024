3107c3cb411938884877d7959c878d59acb2d313
Asmi1108 <asmisaner08@gmail.com>
2024-10-06 22:23:28 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,9 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+  {
+    id: 428,
+    fullname: "Asmi S.",
+    username: "https://github.com/Asmi1108",
+  },
 ];

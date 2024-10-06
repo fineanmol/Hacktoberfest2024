3d564c34f9bf753055d130734b01cf30ebaf70c7
Subash Paudel Chhetri <69474140+iamsubashchhetri@users.noreply.github.com>
2024-10-05 23:47:54 -0300

    Update scroll.js

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,50 +1,32 @@
-window.onscroll = () => handleScroll();
-const scrollButton = document.getElementById("backToTop");
-
-function handleScroll() {
-  const scrollTop =
-    document.body.scrollTop || document.documentElement.scrollTop;
+window.onscroll = () => {
+  const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
   scrollButton.style.display = scrollTop > 150 ? "block" : "none";
-}
+};
+
+const scrollButton = document.getElementById("backToTop");
 
 scrollButton.addEventListener("click", () => {
   scrollButton.classList.add("startscrolling");
-  smoothScrollTo(0, 1250, () => {
-    scrollButton.classList.remove("startscrolling");
-  });
+  smoothScrollTo(0, 1250, () => scrollButton.classList.remove("startscrolling"));
 });
 
-scrollButton.addEventListener("mousedown", () => {
-  scrollButton.classList.add("mousedown");
-});
-
-scrollButton.addEventListener("mouseup", () => {
-  scrollButton.classList.remove("mousedown");
-});
+scrollButton.addEventListener("mousedown", () => scrollButton.classList.add("mousedown"));
+scrollButton.addEventListener("mouseup", () => scrollButton.classList.remove("mousedown"));
 
 function smoothScrollTo(to, duration, callback) {
-  const start = document.documentElement.scrollTop,
-    change = to - start,
-    increment = 20;
+  const start = document.documentElement.scrollTop, change = to - start, increment = 20;
   let currentTime = 0;
 
-  function animateScroll() {
+  (function animateScroll() {
     currentTime += increment;
     const val = Math.easeInOutQuad(currentTime, start, change, duration);
     document.documentElement.scrollTop = val;
-
-    if (currentTime < duration) {
-      setTimeout(animateScroll, increment);
-    } else if (callback && typeof callback === "function") {
-      callback();
-    }
-  }
-  animateScroll();
+    if (currentTime < duration) setTimeout(animateScroll, increment);
+    else callback?.();
+  })();
 }
 
 Math.easeInOutQuad = (t, b, c, d) => {
   t /= d / 2;
-  if (t < 1) return (c / 2) * t * t + b;
-  t--;
-  return (-c / 2) * (t * (t - 2) - 1) + b;
+  return t < 1 ? (c / 2) * t * t + b : (-c / 2) * (--t * (t - 2) - 1) + b;
 };

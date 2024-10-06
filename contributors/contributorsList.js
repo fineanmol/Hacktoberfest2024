dee34c3a98c777c49d7701a182009eff5d5b6430
Kushal Bansal <321kushalbansal.kb@gmail.com>
2024-10-06 18:36:59 +0000

    added my name in the list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Kushal Bansal",
+    username: "https://github.com/kushal7201",
+  },
   
 ];

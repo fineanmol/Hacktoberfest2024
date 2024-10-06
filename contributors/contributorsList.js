f107b5ee9f793c728c1131da6883a2af215e5a9b
ayushi pal <ayushipal06@gmail.com>
2024-10-06 21:16:16 +0530

    Update welcome message and description in index.html

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,9 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+  {
+    id:428,
+    fullname: "Ayushi Pal",
+    username: "https://github.com/ayushii06",
+  },
 ];

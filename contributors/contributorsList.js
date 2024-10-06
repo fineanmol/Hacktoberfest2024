e3d6bf26eec2f2e3df8b7a3d3d2a06becb4020c3
Anamuddin Ahmad <anamuddinahmad0786@gmail.com>
2024-10-07 00:31:37 +0530

    Update contributorsList.js
    adding new user.

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2073,6 +2073,11 @@ contributors = [
     id: 427,
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
+  }, 
+  {
+    id: 428,
+    fullname: "Anamuddin Ahmad",
+    username: "https://github.com/anamuddinahmad",
   },
   
 ];

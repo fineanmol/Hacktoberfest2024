7be0aad6d5800cdd8036eb9250c380e9ee82c9d7
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2024-10-06 22:32:40 +0530

    Update contributorsList.js
    Added my name in Contributors list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+    {
+    id: 1,
+    fullname: "Ayush Patel",
+    username: "https://github.com/ayush-patel1",
+  },
   
 ];

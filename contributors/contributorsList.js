93159a5a71791968cfd19c81f2ce75bdbd5c913e
Oleksandr Yizchak Sanin <alexaaander.sanin@gmail.com>
2024-10-07 01:31:50 +0300

    added myself as a contributor

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Oleksandr Yizchak Sanin",
+    username: "https://github.com/AlexanderSanin",
+  },
   
 ];

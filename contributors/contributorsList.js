5344f25b64be37356c538db92990fe578a653180
Mahipal Choudhary <48117150+choudharymahipal@users.noreply.github.com>
2024-10-07 12:49:33 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,9 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+  {
+    id: 428,
+    fullname: "Mahipal Choudhary",
+    username: "https://github.com/choudharymahipal",
+  },
 ];

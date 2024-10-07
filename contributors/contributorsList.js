61ed971f7a4b1495cb12c4fe30f94f08b12e9da3
rushabh <rushabhwagh125@gmail.com>
2024-10-07 13:39:59 +0530

    new_msg

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Rushabh Wagh",
+    username: "https://github.com/wrexrus",
+  },
   
 ];

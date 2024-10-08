23d3dfdb986af6deb4a9ce2b5c7b09f62adb5811
Satya22051 <satyaranjan22051@gmail.com>
2024-10-09 00:09:27 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 429,
+    fullname: "Test",
+    username: "https://github.com/Test",
+  },
   
 ];

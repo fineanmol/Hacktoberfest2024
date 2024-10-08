a8f3a63dcf931d619245b52c3ae77a18d5cc2aa7
Avish Kamra <147503373+Avi-kamra01@users.noreply.github.com>
2024-10-08 09:51:35 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id:428,
+    fullname: "Avish Kamra",
+    username: "https://github.com/Avi-kamra01/",
+  }
   
 ];

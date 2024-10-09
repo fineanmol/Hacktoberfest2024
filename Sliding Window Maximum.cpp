5c91901b2bc2824f8cd52b6e51e35592c9f1e243
simrrrran1 <91774565+simrrrran1@users.noreply.github.com>
2024-10-09 20:02:56 +0530

    Update and rename Sliding Window Maximum to Sliding Window Maximum.cpp

--- Sliding Window Maximum.cpp
+++ Sliding Window Maximum.cpp
@@ -1,4 +1,4 @@
-QUESTION:
+/*QUESTION:
 You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
 
 Return the max sliding window.
@@ -30,7 +30,7 @@ Constraints:
 -104 <= nums[i] <= 104
 1 <= k <= nums.length
 
-CODE:
+CODE:*/
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {

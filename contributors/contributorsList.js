6572d50f5ff7ee3f0e42278e8d6ffc37c43d7fc3
Om6214 <omnathganapure9981@gmail.com>
2024-10-09 12:49:54 +0530

    added myself to the contributers

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Omnath Ganapure",
+    username: "https://github.com/Om6214"
+  }
   
 ];

ae57911d1f1e2d558b3fc545520a254f18ab7d34
Tushar Mishra <118966111+tusharmishra069@users.noreply.github.com>
2024-10-10 01:45:10 +0530

    added my self as a contributor

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Tushar Mishra",
+    username: "https://github.com/tusharmishra069", 
+  },
   
 ];

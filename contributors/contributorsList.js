03187a751587f7c06ea60e591617799c9e347b86
Md Sadique <119618428+sadique-2004@users.noreply.github.com>
2024-10-10 09:20:05 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Md Sadique",
+    username: "https://github.com/sadique-2004",
+  },
   
 ];

5522a98cf4dc470e65bfd4b93bdb7e8a75c88309
Adam <adam.astamir@hotmail.de>
2024-10-10 21:23:48 +0200

    added myself to contributors

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,9 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+  {
+    id: 428,
+    fullname: "Adam Astamir",
+    username: "https://github.com/IamAdamm",
+  },
 ];

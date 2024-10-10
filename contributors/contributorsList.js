7a1d0acd4e589b7183c76067457b3713fa39db50
Jheelam Hossain <jheelamhossain147@gmail.com>
2024-10-11 02:38:08 +0530

    Update contributorsList.js
    Added myself to the contributer's list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,9 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+  {
+    id: 428,
+    fullname: "Jheelam Hossain",
+    username: "https://github.com/jheelamH",
+  },
 ];

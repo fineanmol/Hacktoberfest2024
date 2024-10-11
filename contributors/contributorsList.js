f27cde135ec7dbc33b88dc41a1b6f9004565a856
Aryan <aryanpratap2004@gmail.com>
2024-10-11 12:35:43 +0530

    First Contribution

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,11 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+
+  {
+    id: 428,
+    fullname: "Aryan Pratap Singh",
+    username: "https://github.com/Aryanpratap01",
+  },
   
 ];

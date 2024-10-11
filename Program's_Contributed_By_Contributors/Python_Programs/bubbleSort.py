ac0440e639768718c58633e82c83d8e16e37e390
Vihan Gammanpila <65854449+VihanPamudya@users.noreply.github.com>
2024-10-11 13:56:44 +0530

    Create bubbleSort.py

---
+++ Program's_Contributed_By_Contributors/Python_Programs/bubbleSort.py
@@ -0,0 +1,22 @@
+def bubbleSort(arr):
+	n = len(arr)
+	
+	for i in range(n):
+		swapped = False
+
+		for j in range(0, n-i-1):
+
+			if arr[j] > arr[j+1]:
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				swapped = True
+		if (swapped == False):
+			break
+
+if __name__ == "__main__":
+	arr = [64, 34, 25, 12, 22, 11, 90]
+
+	bubbleSort(arr)
+
+	print("Sorted array:")
+	for i in range(len(arr)):
+		print("%d" % arr[i], end=" ")

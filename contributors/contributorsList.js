8dadc0f3a5f1c103247dd3f61632a139ea8a7d74
PRATYUSH MISHRA <154694565+PRATYUSH-MISH@users.noreply.github.com>
2024-10-11 14:05:20 +0530

    Update contributorsList.js
    Added my name

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id:428,
+    fullname:"Pratyush Mishra",
+    username:"https://github.com/PRATYUSH-MISH",
+  },
   
 ];

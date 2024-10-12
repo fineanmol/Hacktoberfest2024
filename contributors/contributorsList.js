875a8026d767df355fb7bd5c905272128cc20f9e
Crypto-Matrix <rahuldhawan0010@gmail.com>
2024-10-13 03:12:04 +0530

    Update Contributor.js
    Update Contributor.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Crypto Matrix",
+    username: "https://github.com/Crypto-Matrix",
+  },
   
 ];

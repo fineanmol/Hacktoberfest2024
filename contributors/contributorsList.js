13e48b0a5ebf8fd8c799eb4806de09bda08938cf
Cliptooth <abhi.p.swamy@gmail.com>
2024-10-13 21:07:01 +0530

    Added my name to the contributors list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,9 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+  {
+    id: 428,
+    fullname: "Abhishek P",
+    username: "https://github.com/Abhishekp1130",
+  }
 ];

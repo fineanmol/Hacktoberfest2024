50b5751bf39ae55e0e44b0f45db40db1c88a8091
unknown <tgoel5884@gmail.com>
2024-10-15 18:50:01 +0530

    Added my name for HacktoberFest 24

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Tanmay Goel",
+    username: "https://github.com/tgoel5884",
+  }
   
 ];

f727fd89e46aa23cf5cbd94d27fa869ade0df37e
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2024-10-16 02:41:54 +0530

    Update contributorsList.js
    added my name as open-source contributor

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Smit PATEL",
+    username: "https://github.com/jayambe36",
+  },
   
 ];

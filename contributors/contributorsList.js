6fbf4865cb162c26769d4f1e0bf297f8e6d40b15
AvinashKumar0923 <avinash0923kmr@gmail.com>
2024-10-16 21:18:27 +0530

    Added my info

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Avinash Kumar",
+    username: "https://github.com/AvinashKumar0923",
+  },
   
 ];

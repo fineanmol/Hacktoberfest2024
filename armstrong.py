1c776cf4538db26be6503ddc8f7ae5a079738cb0
ShivangJSS <150658885+ShivangJSS@users.noreply.github.com>
2024-10-17 16:03:24 +0530

    Create armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,36 @@
+def power(x, y):
+     
+    if y == 0:
+        return 1
+    if y % 2 == 0:
+        return power(x, y // 2) * power(x, y // 2)
+         
+    return x * power(x, y // 2) * power(x, y // 2)
+
+def order(x):
+ 
+   
+    n = 0
+    while (x != 0):
+        n = n + 1
+        x = x // 10
+         
+    return n
+def isArmstrong(x):
+     
+    n = order(x)
+    temp = x
+    sum1 = 0
+     
+    while (temp != 0):
+        r = temp % 10
+        sum1 = sum1 + power(r, n)
+        temp = temp // 10
+
+    return (sum1 == x)
+
+x = 153
+print(isArmstrong(x))
+ 
+x = 1253
+print(isArmstrong(x))

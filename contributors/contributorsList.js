70d0c506916f09235071be1b8574c7763c13eed3
Teja Srinivas <112886635+TEJA-2005@users.noreply.github.com>
2024-10-18 21:50:29 +0530

    Update contributorsList.js
    Added my details to the file

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id:428,
+    fullname:"Teja Srinivas",
+    username:"https://github.com/TEJA-2005"
+  },
   
 ];

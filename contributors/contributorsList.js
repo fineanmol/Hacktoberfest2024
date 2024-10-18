393d752da8b994098eaf5b8dabf031235ceba5eb
Izhaan Raza <66744194+Izhaan-Raza@users.noreply.github.com>
2024-10-18 22:36:15 +0530

    Description of your changes

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,11 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Izhaan Raza",
+    username: "https://github.com/Izhaan-Raza",
+  },
+
   
 ];

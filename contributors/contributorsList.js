46c929968ba2d15f73737172295d4d8f98550463
Kunal NayaK <165077439+NayaKunal30@users.noreply.github.com>
2024-10-19 19:37:55 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,11 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Kunal Nayak",
+    username: "https://github.com/NayaKunal30",
+  },
+
   
 ];

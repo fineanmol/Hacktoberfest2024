dc538e80549d45bd8f2ac82a704d8724414e865b
Debashis Paul <119507001+Debashis-11101-srijib@users.noreply.github.com>
2024-10-19 18:05:09 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Debashis Paul",
+    username: "https://github.com/Debashis-11101-srijib",
+  },
   
 ];

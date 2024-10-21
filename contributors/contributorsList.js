c2b1a13f7ea835756d8d471c092e5d957cc83e19
ArpitaAgg <167079982+ArpitaAgg@users.noreply.github.com>
2024-10-21 10:33:42 +0530

    Updated contributorsList.js
    Updated my name to contibutor list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Arpita Aggarwal",
+    username: "https://github.com/ArpitaAgg",
+  },
   
 ];

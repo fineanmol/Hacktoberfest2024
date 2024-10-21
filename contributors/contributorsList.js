f480213b19c264ca6d7d058c90ba578441e12057
Navya Jain <119936633+navyaajainnn@users.noreply.github.com>
2024-10-21 18:17:10 +0530

    Added myself to the contributors list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Navya Jain",
+    username: "https://github.com/navyaajainnn",
+  },
   
 ];

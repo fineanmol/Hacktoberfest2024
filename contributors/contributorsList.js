45001f225b3952c0540b3dca230cbb8f4084632f
gaetan basset <51761442+gbasset@users.noreply.github.com>
2024-10-22 16:25:21 +0200

    Add contributor: add myself

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 42,
+    fullname: "Basset Gaëtan",
+    username: "https://github.com/gbasset",
+  },
   
 ];

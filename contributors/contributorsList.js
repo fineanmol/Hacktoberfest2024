38dcaeddf8b5d9644d55b82a16fa063c8fe5fd8d
UJJWAL SRIVASTAVA <119431039+ujjwalsri1@users.noreply.github.com>
2024-10-23 00:01:43 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,9 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+  {
+    id: 428,
+    fullname: "Ujjwal Srivastava",
+    username: "https://github.com/ujjwalsri1",
+  },
 ];

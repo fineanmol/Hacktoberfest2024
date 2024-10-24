a15e4143c1dd36a897bbaac8c8c0b5cf5401f1ce
AnubhabSarkar180 <anubhabsarkar023@gmail.com>
2024-10-24 20:56:17 +0530

    Added new colours to social icons

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname:AnubhabSarkar,
+    username:"https://github.com/AnubhabSarkar180",
+  },
   
 ];

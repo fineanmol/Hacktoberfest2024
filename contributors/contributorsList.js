fb390ba44665ba5692b7ae3b79277ffe8eff1571
bibekjoshi01 <bibekjoshi34@gmail.com>
2024-10-24 11:33:40 +0545

    Added myself to contributors

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,9 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+  {
+    id: 428,
+    fullname: "Bibek Joshi",
+    username: "https://github.com/bibekjoshi01",
+  },
 ];

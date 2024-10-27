6f4a0d7fa7e8005b8108bbce51caacb9db2ceb5d
Sachin Kumar <66481063+Sharmaji513@users.noreply.github.com>
2024-10-27 20:25:08 +0530

    Update contributorsList.js
    i have updated contributors List

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2089,5 +2089,10 @@ contributors = [
     fullname: "Parth Panjwani",
     username: "https://github.com/Parth-Panjwani",
   },
+      {
+    id: 442,
+    fullname: "Sachin kumar",
+    username: "https://github.com/Sharmaji513",
+  },
 
 ];

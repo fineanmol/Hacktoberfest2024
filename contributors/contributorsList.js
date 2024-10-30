b210dd0dceee1aed891e11f25fb78d63608b26a4
VinayK8866 <vinay.kondabattula@gmail.com>
2024-10-30 14:59:38 +0530

    Added my name to the list

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Vinay Kondabathula",
+    username: "https://github.com/VinayK8866",
+  },
   
 ];

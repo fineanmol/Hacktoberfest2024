40b5fe8b8e94693baa1e5bd4ff7bed7a5758b94e
Khushi <153130183+KhushiPandey8@users.noreply.github.com>
2025-02-17 01:14:08 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2089,5 +2089,10 @@ contributors = [
     fullname: "Parth Panjwani",
     username: "https://github.com/Parth-Panjwani",
   },
+   {
+    id:432,
+    fullname: "Khushi Pandey",
+    username: "https://github.com/KhushiPandey8",
+  },
 
 ];

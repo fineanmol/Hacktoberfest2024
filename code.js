309275dd65e3ff191b869dea76857922521d3d78
fineanmol-sears <anmol.agarwal2004@yahoo.com>
2025-06-05 12:43:37 +0530

    Remove Extra Files

--- code.js
+++
@@ -1,8 +0,0 @@
-var userId = 'sys_id_of_user'; // Replace with the user's sys_id
-var gr = new GlideRecord('incident');
-gr.addQuery('assigned_to', userId);
-gr.query();
-
-while (gr.next()) {
-    gs.info('Incident: ' + gr.number + ' - ' + gr.short_description);
-}
